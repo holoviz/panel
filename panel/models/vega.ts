60978996df1e1f7759d7246159113b841138bfa4
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-09-05 15:18:05 +0100

    Fixed formatting in vega.ts

--- panel/models/vega.ts
+++ panel/models/vega.ts
@@ -7,21 +7,21 @@ export class VegaPlotView extends LayoutDOMView {
   initialize(options): void {
     super.initialize(options)
     const vega_url = "https://cdn.jsdelivr.net/npm/vega@4.2.0"
-	const vega_lite_url = "https://cdn.jsdelivr.net/npm/vega-lite@3.0.0-rc4"
-	const vega_embed_url = "https://cdn.jsdelivr.net/npm/vega-embed@3.18.2"
+    const vega_lite_url = "https://cdn.jsdelivr.net/npm/vega-lite@3.0.0-rc4"
+    const vega_embed_url = "https://cdn.jsdelivr.net/npm/vega-embed@3.18.2"
 
     if (window.vega) {
       this._init()
     } else {
-	  const init = () => { this._init() }
-	  const load_vega_embed = () => { this._add_script(vega_embed_url, init) }
-	  const load_vega_lite = () => { this._add_script(vega_lite_url, load_vega_embed) }
+      const init = () => { this._init() }
+      const load_vega_embed = () => { this._add_script(vega_embed_url, init) }
+      const load_vega_lite = () => { this._add_script(vega_lite_url, load_vega_embed) }
       this._add_script(vega_url, load_vega_lite)
     }
   }
 
   _add_script(url: string, callback): void {
-	const script = document.createElement('script')
+    const script = document.createElement('script')
     script.src = url
     script.async = false
     script.onreadystatechange = script.onload = callback
@@ -42,28 +42,28 @@ export class VegaPlotView extends LayoutDOMView {
   }
 
   _fetch_datasets() {
-	const datasets = {}
-	for (const ds in this.model.data_sources) {
+    const datasets = {}
+    for (const ds in this.model.data_sources) {
       const cds = this.model.data_sources[ds];
-	  const data = []
-	  const columns = cds.columns()
-	  for (const i = 0; i < cds.data[columns[0]].length; i++) {
+      const data = []
+      const columns = cds.columns()
+      for (const i = 0; i < cds.data[columns[0]].length; i++) {
         const item = {}
-	    for (const column of columns) {
-		  item[column] = cds.data[column][i]
+        for (const column of columns) {
+          item[column] = cds.data[column][i]
         }
-		data.push(item)
+        data.push(item)
       }
-	  datasets[ds] = data;
-	}
-	return datasets
+      datasets[ds] = data;
+    }
+    return datasets
   }
 
   _plot(): void {
-	if (!('datasets' in this.model.data)) {
-	  this.model.data['datasets'] = this._fetch_datasets()
-	}
-	vegaEmbed(this.el, this.model.data);
+    if (!('datasets' in this.model.data)) {
+      this.model.data['datasets'] = this._fetch_datasets()
+    }
+    vegaEmbed(this.el, this.model.data);
   }
 }
 
