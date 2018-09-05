736acd39f2d07c1a5d5b37fa7142f51fe2bec8bc
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-09-05 18:30:35 +0100

    Added altair tests

--- panel/tests/test_vega.py
+++ panel/tests/test_vega.py
@@ -1,5 +1,13 @@
 from __future__ import absolute_import
 
+import pytest
+
+try:
+    import altair as alt
+except:
+    alt = None
+altair_available = pytest.mark.skipif(alt is None, reason="requires altair")
+
 import numpy as np
 from bokeh.models import Row as BkRow
 from panel.pane import Pane, PaneBase
@@ -17,7 +25,6 @@ vega_example = {
                  'y': {'type': 'quantitative', 'field': 'y'}},
     '$schema': 'https://vega.github.io/schema/vega-lite/v2.6.0.json'
 }
-
 
 def test_get_vega_pane_type_from_dict():
     assert PaneBase.get_pane_type(vega_example) is Vega
@@ -44,6 +51,59 @@ def test_vega_pane(document, comm):
     point_example['data']['values'][0]['x'] = 'C'
     pane.object = point_example
     point_example['data'].pop('values')
+    assert model.data == point_example
+    cds_data = model.data_sources['data'].data
+    assert np.array_equal(cds_data['x'], np.array(['C', 'B', 'C', 'D', 'E'])) 
+    assert np.array_equal(cds_data['y'], np.array([5, 3, 6, 7, 2]))
+
+    pane._cleanup(model)
+    assert pane._callbacks == {}
+
+
+def altair_example():
+    import altair as alt
+    data = alt.Data(values=[{'x': 'A', 'y': 5},
+                            {'x': 'B', 'y': 3},
+                            {'x': 'C', 'y': 6},
+                            {'x': 'D', 'y': 7},
+                            {'x': 'E', 'y': 2}])
+    chart = alt.Chart(data).mark_bar().encode(
+        x='x:O',  # specify ordinal data
+        y='y:Q',  # specify quantitative data
+    )
+    return chart
+
+
+@altair_available
+def test_get_vega_pane_type_from_altair():
+    assert PaneBase.get_pane_type(altair_example()) is Vega
+
+
+@altair_available
+def test_altair_pane(document, comm):
+    pane = Pane(altair_example())
+
+    # Create pane
+    row = pane._get_root(document, comm=comm)
+    assert isinstance(row, BkRow)
+    assert len(row.children) == 1
+    model = row.children[0]
+    assert isinstance(model, VegaPlot)
+
+    expected = dict(vega_example, data={})
+
+    assert model.data == expected
+    
+    print(model.data)
+    cds_data = model.data_sources['data'].data
+    assert np.array_equal(cds_data['x'], np.array(['A', 'B', 'C', 'D', 'E'])) 
+    assert np.array_equal(cds_data['y'], np.array([5, 3, 6, 7, 2]))
+
+    chart = altair_example()
+    chart.mark = 'point'
+    chart.data.values[0]['x'] = 'C'
+    pane.object = chart
+    point_example = dict(vega_example, mark='point')
     assert model.data == point_example
     cds_data = model.data_sources['data'].data
     assert np.array_equal(cds_data['x'], np.array(['C', 'B', 'C', 'D', 'E'])) 
