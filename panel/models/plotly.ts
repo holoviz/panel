bf382daf0d4b29de53a04d180333aee7f6e5b2aa
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-09-20 14:13:57 +0100

    Minor fixes

--- panel/models/plotly.ts
+++ panel/models/plotly.ts
@@ -60,7 +60,7 @@ export class PlotlyPlotView extends LayoutDOMView {
         trace[column] = array
       }
     }
-    setTimeout(() => Plotly.react(this.el, this.model.data.data, this.model.data.layout), 100)
+	setTimeout(() => Plotly.react(this.el, this.model.data.data, this.model.data.layout), 50)
   }
 }
 
