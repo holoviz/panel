841fe381eb54e1a2d7f0fbd038d5bb5735d7b172
Philipp Rudiger <prudiger@anaconda.com>
2019-01-28 16:47:29 +0000

    Fixed py2 incompatible test

--- panel/tests/test_models.py
+++ panel/tests/test_models.py
@@ -3,7 +3,7 @@ import panel
 
 
 def test_models_encoding():
-    model_dir = os.path.join(*panel.__path__, 'models')
+    model_dir = os.path.join(panel.__path__[0], 'models')
     for file in os.listdir(model_dir):
         if file.endswith('.ts'):
             open(os.path.join(model_dir, file), 'r').read()
