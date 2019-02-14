ec923e7363e98a1aa6a64ed15a7fcb4696976ae6
jlstevens <jlrstevens@gmail.com>
2019-02-13 18:19:52 -0600

    Assuming dict ordering preserved for all future Python versions

--- panel/param.py
+++ panel/param.py
@@ -359,7 +359,8 @@ class Param(PaneBase):
                     if ((p.precedence is None) or (p.precedence >= self.display_threshold))]
         groups = itertools.groupby(filtered, key=key_fn)
         # Params preserve definition order in Python 3.6+
-        dict_ordered = sys.version_info.major == 3 and sys.version_info.minor >= 6
+        dict_ordered_py3 = (sys.version_info.major == 3 and sys.version_info.minor >= 6)
+        dict_ordered = dict_ordered_py3 or (sys.version_info.major > 3)
         ordered_groups = [list(grp) if dict_ordered else sorted(grp) for (k,grp) in groups]
         ordered_params = [el[0] for group in ordered_groups for el in group]
 
