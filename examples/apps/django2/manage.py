1ac473e832bcd8965a36a9f6320d6373174bcffd
Philipp Rudiger <prudiger@anaconda.com>
2019-02-15 13:26:54 +0000

    Fixed flake

--- examples/apps/django2/manage.py
+++ examples/apps/django2/manage.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
