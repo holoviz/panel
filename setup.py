3c67cf66f2f4a46037f51bade983fb8f8b2b0c5a
Philipp Rudiger <prudiger@anaconda.com>
2019-02-27 19:32:13 +0000

    Pin bokeh 1.1.0dev8

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ class CustomInstallCommand(install):
 ########## dependencies ##########
 
 install_requires = [
-    'bokeh >=1.1.0dev7',
+    'bokeh >=1.1.0dev8',
     'param >=1.8.2',
     'pyviz_comms >=0.7.0',
     'markdown',
