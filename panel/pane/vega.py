ca656c859ab4bd16ccf40f8b78f0b99ebbef653c
Philipp Rudiger <prudiger@anaconda.com>
2019-03-11 19:20:30 +0000

    Fixed bug in vega pane

--- panel/pane/vega.py
+++ panel/pane/vega.py
@@ -93,9 +93,9 @@ class Vega(PaneBase):
 
     def _update(self, model):
         if self.object is None:
+            json = None
+        else:
             json = self._to_json(self.object)
             self._get_sources(json, model.data_sources)
-        else:
-            json = None
         model.data = json
 
