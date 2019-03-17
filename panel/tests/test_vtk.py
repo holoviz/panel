c2db28c714e8d9d2c54e5b20ca782707c14199f0
Xav <xavart2011@gmail.com>
2019-03-17 19:42:42 +0100

    test _dump data array

--- panel/tests/test_vtk.py
+++ panel/tests/test_vtk.py
@@ -41,15 +41,7 @@ def test_get_vtk_pane_type_from_file():
 
 @vtk_available
 def test_get_vtk_pane_type_from_render_window():
-    cone = vtk.vtkConeSource()
-    coneMapper = vtk.vtkPolyDataMapper()
-    coneMapper.SetInputConnection(cone.GetOutputPort())
-    coneActor = vtk.vtkActor()
-    coneActor.SetMapper(coneMapper)
-    ren = vtk.vtkRenderer()
-    ren.AddActor(coneActor)
-    renWin = vtk.vtkRenderWindow()
-    renWin.AddRenderer(ren)
+    renWin = make_render_window()
     assert PaneBase.get_pane_type(renWin) is VTK
 
 
@@ -66,19 +58,20 @@ def test_vtk_pane_from_url(document, comm):
 
 
 @vtk_available
-def test_serialisation():
-    pass
-
-
-@vtk_available
-def test_vtk_pane_from_render_window(document, comm):
-    render_window = make_render_window()
-
-#     pane = Pane(render_window)
-#
-#     # Create pane
-#     model = pane._get_root(document, comm=comm)
-#     assert isinstance(model, VTKPlot)
-#     assert pane._models[model.ref['id']][0] is model
-#     assert isinstance(model.vtkjs, string_types)
-
+def test_vtk_data_array_dump():
+    from panel.pane.vtk import _dump_data_array
+    root_keys = ['ref', 'vtkClass', 'name', 'dataType',
+                 'numberOfComponents', 'size', 'ranges']
+    renWin = make_render_window()
+    renderers = list(renWin.GetRenderers())
+    ren_props = list(renderers[0].GetViewProps())
+    mapper = ren_props[0].GetMapper()
+    mapper.Update() # create data
+    data = mapper.GetInput().GetPoints().GetData()
+    scDir = []
+    root = {}
+    _dump_data_array(scDir, '', 'test', data, root=root, compress=False)
+    assert root_keys == list(root.keys())
+    assert len(scDir) == 1
+    assert isinstance(scDir[0][0], string_types)
+    assert isinstance(scDir[0][1], bytes)
