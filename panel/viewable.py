7c1e71853cb1a89724750c65d676c89d8918b2df
Philipp Rudiger <prudiger@anaconda.com>
2019-03-31 15:23:03 +0100

    Implement PeriodicCallback

--- panel/viewable.py
+++ panel/viewable.py
@@ -753,6 +753,37 @@ class Reactive(Viewable):
         self._callbacks.append(cb)
         return cb
 
+    def add_periodic_callback(self, callback, period=500, count=None,
+                              timeout=None, start=True):
+        """
+        Schedules a periodic callback to be run at an interval set by
+        the period. Returns a PeriodicCallback object with the option
+        to stop and start the callback.
+
+        Arguments
+        ---------
+        callback: callable
+          Callable function to be executed at periodic interval.
+        period: int
+          Interval in milliseconds at which callback will be executed.
+        count: int
+          Maximum number of times callback will be invoked.
+        timeout: int
+          Timeout in seconds when the callback should be stopped.
+        start: boolean (default=True)
+          Whether to start callback immediately.
+
+        Returns
+        -------
+        Return a PeriodicCallback object with start and stop methods.
+        """
+        from .callbacks import PeriodicCallback
+        cb = PeriodicCallback(callback=callback, period=period,
+                              count=count, timeout=timeout)
+        if start:
+            cb.start()
+        return cb
+
     def jslink(self, target, code=None, **links):
         """
         Links properties on the source object to those on the target
