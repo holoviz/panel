e14f9738c920a1123eae91c65e1b9767ef3d87c2
Jon Mease <jon.mease@gmail.com>
2019-07-19 14:55:00 -0400

    Disable default renderer in plotly tests

--- panel/tests/pane/test_plotly.py
+++ panel/tests/pane/test_plotly.py
@@ -5,6 +5,8 @@ import pytest
 try:
     import plotly
     import plotly.graph_objs as go
+    import plotly.io as pio
+    pio.renderers.default = None
 except:
     plotly = None
 plotly_available = pytest.mark.skipif(plotly is None, reason="requires plotly")
