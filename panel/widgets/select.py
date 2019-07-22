3416a77e3443516c72faeee4001c3e0daf7d6b97
jlstevens <jlrstevens@gmail.com>
2019-07-22 08:03:15 -0500

    Passing sizing_mode through to the whitelist and blacklist columns

--- panel/widgets/select.py
+++ panel/widgets/select.py
@@ -338,9 +338,11 @@ class CrossSelector(CompositeWidget, MultiSelect):
         self._search[True].param.watch(self._filter_options, 'value')
 
         # Define Layout
-        blacklist = Column(self._search[False], self._lists[False])
-        whitelist = Column(self._search[True], self._lists[True])
-        buttons = Column(self._buttons[True], self._buttons[False], width=50)
+        blacklist = Column(self._search[False], self._lists[False],
+                           sizing_mode=self.sizing_mode)
+        whitelist = Column(self._search[True], self._lists[True],
+                           sizing_mode=self.sizing_mode)
+        buttons = Column(self._buttons[True], self._buttons[False])
 
         self._composite = Row(blacklist, Column(VSpacer(), buttons, VSpacer()), whitelist,
                               css_classes=self.css_classes, margin=self.margin, **layout)
