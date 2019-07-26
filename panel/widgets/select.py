7a9fb669cf7c430db4f163df971fadbf2c446e78
Jean-Luc Stevens <jlrstevens@gmail.com>
2019-07-26 18:16:55 -0500

    Crossselector layout fix (#545)

--- panel/widgets/select.py
+++ panel/widgets/select.py
@@ -338,9 +338,9 @@ class CrossSelector(CompositeWidget, MultiSelect):
         self._search[True].param.watch(self._filter_options, 'value')
 
         # Define Layout
-        blacklist = Column(self._search[False], self._lists[False])
-        whitelist = Column(self._search[True], self._lists[True])
-        buttons = Column(self._buttons[True], self._buttons[False], width=50)
+        blacklist = Column(self._search[False], self._lists[False], **layout)
+        whitelist = Column(self._search[True], self._lists[True], **layout)
+        buttons = Column(self._buttons[True], self._buttons[False])
 
         self._composite = Row(blacklist, Column(VSpacer(), buttons, VSpacer()), whitelist,
                               css_classes=self.css_classes, margin=self.margin, **layout)
@@ -367,7 +367,7 @@ class CrossSelector(CompositeWidget, MultiSelect):
                 setattr(self._lists[True], event.name, width)
                 setattr(self._lists[False], event.name, width)
 
-        
+
     @param.depends('size', watch=True)
     def _update_size(self):
         self._lists[False].size = self.size
@@ -380,7 +380,7 @@ class CrossSelector(CompositeWidget, MultiSelect):
 
     @param.depends('value', watch=True)
     def _update_value(self):
-        labels, values = self.labels, self.values 
+        labels, values = self.labels, self.values
         selected = [labels[indexOf(v, values)] for v in self.value
                     if isIn(v, values)]
         unselected = [k for k in labels if k not in selected]
