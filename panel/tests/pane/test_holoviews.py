210685c24030bf3f3f96733819e0666d329906c9
Philipp Rudiger <prudiger@anaconda.com>
2019-08-05 18:10:09 +0200

    Fixed flake

--- panel/tests/pane/test_holoviews.py
+++ panel/tests/pane/test_holoviews.py
@@ -278,12 +278,11 @@ def test_holoviews_fancy_layout(document, comm):
 
 
 @hv_available
-def test_holoviews_fancy_layout_scrubber(document, comm):
+def test_holoviews_fancy_layout_scrubber():
     hmap = hv.HoloMap({(i, chr(65+i)): hv.Curve([i]) for i in range(3)}, kdims=['X', 'Y'])
 
     hv_pane = HoloViews(hmap, fancy_layout=True, widget_type='scrubber')
     layout_obj = hv_pane.layout
-    layout = layout_obj.get_root(document, comm)
     assert hv_pane is layout_obj[1][0]
     assert len(hv_pane.widget_box.objects) == 1
     assert hv_pane.widget_box is layout_obj[1][1][1]
