ae9cff4f3da34b0e2071cf7871f520a470d46fdf
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2019-10-12 15:59:08 +0200

    Support file like objects for images (#686)

--- panel/tests/pane/test_image.py
+++ panel/tests/pane/test_image.py
@@ -1,8 +1,12 @@
 from __future__ import absolute_import, division, unicode_literals
 
+import sys
+import pytest
+
 from base64 import b64decode, b64encode
 
 from panel.pane import GIF, JPG, PNG, SVG
+from io import BytesIO, StringIO
 
 
 def test_svg_pane(document, comm):
@@ -53,3 +57,33 @@ def test_imgshape():
         w,h = t._imgshape(b64decode(twopixel[t.name.lower()]))
         assert w == 2
         assert h == 1
+
+def test_load_from_byteio():
+    """Testing a loading a image from a ByteIo"""
+    memory = BytesIO()
+    with open('panel/tests/test_data/logo.png', 'rb') as image_file:
+        memory.write(image_file.read())
+    memory.seek(0)
+    image_pane = PNG(memory)
+    image_data = image_pane._img()
+    assert b'PNG' in image_data
+
+@pytest.mark.skipif(sys.version_info.major <= 2, reason="Doesn't work with python 2")
+def test_load_from_stringio():
+    """Testing a loading a image from a StringIO"""
+    memory = StringIO()
+    with open('panel/tests/test_data/logo.png', 'rb') as image_file:
+        memory.write(str(image_file.read()))
+    memory.seek(0)
+    image_pane = PNG(memory)
+    image_data = image_pane._img()
+    assert 'PNG' in image_data
+
+def test_loading_a_image_from_url():
+    """Tests the loading of a image from a url"""
+    url = 'https://upload.wikimedia.org/wikipedia/commons/7/71/' \
+          '1700_CE_world_map.PNG'
+
+    image_pane = PNG(url)
+    image_data = image_pane._img()
+    assert b'PNG' in image_data
