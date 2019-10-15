91f69b022f00f65a2a8c7087746cde3d5620f6ff
Philipp Rudiger <prudiger@anaconda.com>
2019-10-15 12:02:46 +0200

    Add DataFrame widget tests

--- panel/tests/conftest.py
+++ panel/tests/conftest.py
@@ -25,6 +25,16 @@ def comm():
     return Comm()
 
 
+@pytest.fixture
+def dataframe():
+    import pandas as pd
+    return pd.DataFrame({
+        'int': [1, 2, 3],
+        'float': [3.14, 6.28, 9.42],
+        'str': ['A', 'B', 'C']
+    }, index=[1, 2, 3])
+
+
 @pytest.yield_fixture
 def hv_bokeh():
     import holoviews as hv
