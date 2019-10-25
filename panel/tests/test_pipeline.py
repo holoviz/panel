19e8ba31af15391ba5a9ef05b0c28e71d826a0d0
Philipp Rudiger <prudiger@anaconda.com>
2019-10-25 15:44:08 -0500

    Added new tests

--- panel/tests/test_pipeline.py
+++ panel/tests/test_pipeline.py
@@ -7,11 +7,15 @@ from panel.layout import Row, Column, Spacer
 from panel.pane import HoloViews
 from panel.param import Param, ParamMethod
 from panel.pipeline import Pipeline
-from panel.widgets import Button
-from panel.tests.util import hv_available
+from panel.widgets import Button, Select
 
 if LooseVersion(param.__version__) < '1.8.2':
     pytest.skip("skipping if param version < 1.8.2", allow_module_level=True)
+
+try:
+    import holoviews as hv
+except:
+    pytest.skip('Pipeline requires HoloViews.')
 
 
 class Stage1(param.Parameterized):
@@ -21,6 +25,8 @@ class Stage1(param.Parameterized):
     b = param.Number(default=5, bounds=(0, 10))
 
     ready = param.Boolean(default=False)
+
+    next = param.String(default=None)
 
     @param.output(c=param.Number)
     def output(self):
@@ -47,10 +53,28 @@ class Stage2(param.Parameterized):
     def panel(self):
         return Row(self.param, self.view)
 
-@hv_available
-def test_pipeline_from_classes():
-    import holoviews as hv
 
+class Stage2b(param.Parameterized):
+
+    c = param.Number(default=5, precedence=-1, bounds=(0, None))
+
+    root = param.Parameter(default=0.1)
+
+    @param.depends('c', 'root')
+    def view(self):
+        return '%s^-%s=%.3f' % (self.c, self.root, self.c**(-self.root))
+
+    def panel(self):
+        return Row(self.param, self.view)
+
+
+class DummyStage(param.Parameterized):
+
+    def panel(self):
+        return 'foo'
+
+
+def test_pipeline_from_classes():
     pipeline = Pipeline([('Stage 1', Stage1), ('Stage 2', Stage2)])
 
     layout = pipeline.layout
@@ -91,10 +115,7 @@ def test_pipeline_from_classes():
     assert stage[1].object() == '5 * 5 = 25'
 
 
-@hv_available
 def test_pipeline_from_instances():
-    import holoviews as hv
-
     pipeline = Pipeline([('Stage 1', Stage1()), ('Stage 2', Stage2())])
 
     layout = pipeline.layout
@@ -135,9 +156,7 @@ def test_pipeline_from_instances():
     assert stage[1].object() == '5 * 5 = 25'
 
 
-@hv_available
 def test_pipeline_from_add_stages():
-    import holoviews as hv
 
     pipeline = Pipeline()
     pipeline.add_stage('Stage 1', Stage1)
@@ -181,14 +200,193 @@ def test_pipeline_from_add_stages():
     assert stage[1].object() == '5 * 5 = 25'
 
 
-@hv_available
+def test_pipeline_define_graph_missing_node():
+    pipeline = Pipeline()
+    pipeline.add_stage('Stage 1', Stage1)
+    pipeline.add_stage('Stage 2', Stage2)
+
+    with pytest.raises(ValueError):
+        pipeline.define_graph({'Stage 1': ('Stage 2', 'Stage 2b')})
+
+
+def test_pipeline_define_graph():
+    pipeline = Pipeline()
+    pipeline.add_stage('Stage 2', Stage2)
+    pipeline.add_stage('Stage 2b', Stage2b)
+    pipeline.add_stage('Stage 1', Stage1)
+
+    pipeline.define_graph({'Stage 1': ('Stage 2', 'Stage 2b')})
+
+    pipeline.init()
+
+    assert pipeline._stage == 'Stage 1'
+
+    assert isinstance(pipeline.buttons, Row)
+    (pselect, pbutton), (nselect, nbutton) = pipeline.buttons
+    assert isinstance(pselect, Select)
+    assert pselect.disabled
+    assert isinstance(pbutton, Button)
+    assert pbutton.disabled
+
+    assert isinstance(nselect, Select)
+    assert not nselect.disabled
+    assert nselect.options == ['Stage 2', 'Stage 2b']
+    assert nselect.value == 'Stage 2'
+    assert isinstance(nbutton, Button)
+    assert not nbutton.disabled
+
+    pipeline._next()
+
+    assert isinstance(pipeline._state, Stage2)
+
+    pipeline._previous()
+
+    nselect.value = 'Stage 2b'
+
+    pipeline._next()
+
+    assert isinstance(pipeline._state, Stage2b)
+
+
+def test_pipeline_define_next_parameter_respected():
+    pipeline = Pipeline()
+    pipeline.add_stage('Stage 2', Stage2)
+    pipeline.add_stage('Stage 2b', Stage2b)
+    pipeline.add_stage('Stage 1', Stage1(next='Stage 2b'), next_parameter='next')
+
+    pipeline.define_graph({'Stage 1': ('Stage 2', 'Stage 2b')})
+
+    pipeline.init()
+
+    assert pipeline.next_selector.value == 'Stage 2b'
+
+    pipeline._state.next = 'Stage 2'
+
+    assert pipeline.next_selector.value == 'Stage 2'
+
+
+def test_pipeline_error_condition():
+    pipeline = Pipeline()
+    stage2b = Stage2b(root='error')
+    pipeline.add_stage('Stage 2', Stage2)
+    pipeline.add_stage('Stage 2b', stage2b)
+    pipeline.add_stage('Stage 1', Stage1)
+
+    pipeline.define_graph({'Stage 1': ('Stage 2', 'Stage 2b')})
+
+    pipeline.init()
+
+    pipeline.next_selector.value = 'Stage 2b'
+    pipeline._next()
+
+    assert isinstance(pipeline.error[0], Button)
+    
+    stage2b.root = 2
+
+    pipeline._next()
+
+    assert len(pipeline.error) == 0
+
+    
+def test_pipeline_previous_follows_initial_path():
+    pipeline = Pipeline()
+    pipeline.add_stage('Stage 1', Stage1)
+    pipeline.add_stage('Stage 2', Stage2)
+    pipeline.add_stage('Stage 2b', Stage2b)
+    pipeline.add_stage('Stage 3', DummyStage)
+
+    pipeline.define_graph({
+        'Stage 1': ('Stage 2', 'Stage 2b'),
+        'Stage 2': 'Stage 3',
+        'Stage 2b': 'Stage 3'
+    })
+
+    pipeline.init()
+
+    assert pipeline._route == ['Stage 1']
+
+    pipeline.next_selector.value = 'Stage 2b'
+    pipeline._next()
+
+    assert pipeline._route == ['Stage 1', 'Stage 2b']
+    
+    pipeline._next()
+
+    assert pipeline._route == ['Stage 1', 'Stage 2b', 'Stage 3']
+    
+    pipeline._previous()
+
+    assert pipeline._stage == 'Stage 2b'
+    assert pipeline._route == ['Stage 1', 'Stage 2b']
+
+    
+def test_pipeline_ready_respected():
+    pipeline = Pipeline(ready_parameter='ready')
+    pipeline.add_stage('Stage 1', Stage1)
+    pipeline.add_stage('Stage 2', Stage2)
+
+    pipeline.init()
+
+    assert pipeline.next_button.disabled
+
+    pipeline._state.ready = True
+
+    assert not pipeline.next_button.disabled
+
+
+def test_pipeline_auto_advance_respected():
+    pipeline = Pipeline(ready_parameter='ready', auto_advance=True)
+    pipeline.add_stage('Stage 1', Stage1)
+    pipeline.add_stage('Stage 2', Stage2)
+
+    pipeline.init()
+
+    assert pipeline.next_button.disabled
+
+    pipeline._state.ready = True
+
+    assert isinstance(pipeline._state, Stage2)
+
+
+def test_pipeline_network_diagram_states():
+    pipeline = Pipeline(ready_parameter='ready', auto_advance=True)
+    pipeline.add_stage('Stage 1', Stage1)
+    pipeline.add_stage('Stage 2', Stage2)
+    pipeline.add_stage('Stage 2b', Stage2b)
+
+    pipeline.define_graph({'Stage 1': ('Stage 2', 'Stage 2b')})
+
+    pipeline.init()
+
+    [s1, s2, s2b] = pipeline.network.object.nodes['State']
+
+    assert s1 == 'active'
+    assert s2 == 'inactive'
+    assert s2b == 'next'
+
+    pipeline._next()
+
+    [s1, s2, s2b] = pipeline.network.object.nodes['State']
+
+    assert s1 == 'inactive'
+    assert s2 == 'inactive'
+    assert s2b == 'active'
+
+    pipeline._previous()
+
+    [s1, s2, s2b] = pipeline.network.object.nodes['State']
+
+    assert s1 == 'active'
+    assert s2 == 'inactive'
+    assert s2b == 'next'
+
+
 def test_pipeline_add_stage_validate_wrong_type():
     pipeline = Pipeline()
     with pytest.raises(ValueError):
         pipeline.add_stage('Stage 1', 1)
 
 
-@hv_available
 def test_pipeline_add_stage_validate_add_twice():
     pipeline = Pipeline()
     pipeline.add_stage('Stage 1', Stage1)
@@ -196,14 +394,12 @@ def test_pipeline_add_stage_validate_add_twice():
         pipeline.add_stage('Stage 1', Stage1)
 
 
-@hv_available
 def test_pipeline_getitem():
     pipeline = Pipeline()
     pipeline.add_stage('Stage 1', Stage1)
     assert pipeline['Stage 1'] == Stage1
 
 
-@hv_available
 def test_pipeline_repr():
     pipeline = Pipeline()
     pipeline.add_stage('Stage 1', Stage1)
