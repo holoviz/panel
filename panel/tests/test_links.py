73d5c6148a5f0db606654a2e70ea9aa165442b48
Philipp Rudiger <prudiger@anaconda.com>
2019-11-08 16:32:00 -0600

    Add bi-directional jslink

--- panel/tests/test_links.py
+++ panel/tests/test_links.py
@@ -9,8 +9,29 @@ from bokeh.plotting import figure
 from panel.layout import Row
 from panel.links import Link
 from panel.pane import HoloViews
-from panel.widgets import FloatSlider, RangeSlider, ColorPicker
+from panel.widgets import FloatSlider, RangeSlider, ColorPicker, TextInput
 from panel.tests.util import hv_available
+
+
+def test_widget_link_bidirectional(document, comm):
+    t1 = TextInput()
+    t2 = TextInput()
+
+    t1.jslink(t2, value='value', bidirectional=True)
+
+    row = Row(t1, t2)
+
+    model = row.get_root(document, comm)
+
+    tm1, tm2 = model.children
+
+    link1_customjs = tm1.js_property_callbacks['change:value'][-1]
+    link2_customjs = tm2.js_property_callbacks['change:value'][-1]
+
+    assert link1_customjs.args['source'] is tm1
+    assert link2_customjs.args['source'] is tm2
+    assert link1_customjs.args['target'] is tm2
+    assert link2_customjs.args['target'] is tm1
 
 
 @hv_available
