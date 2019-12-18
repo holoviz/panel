c14728a2d2075fe3eb0fe66ea38cfb2bb3800108
Philipp Rudiger <prudiger@anaconda.com>
2019-12-18 03:15:41 +0100

    Allow multiple Callbacks on one source (#877)

--- panel/links.py
+++ panel/links.py
@@ -59,7 +59,9 @@ class Callback(param.Parameterized):
             for link in links:
                 link_params = {
                     k: v for k, v in link.get_param_values() if k != 'name'}
-                if (type(link) is type(self) and link.source is self.source
+                if not hasattr(link, 'target'):
+                    pass
+                elif (type(link) is type(self) and link.source is self.source
                     and link.target is self.target and params == link_params):
                     return
             self.registry[self.source].append(self)
