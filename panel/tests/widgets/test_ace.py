d31b71838167efbc80e70c3dadd21716df0fc990
kgullikson <kgullikson@sparkcognition.com>
2019-12-23 16:49:15 -0600

    Remove the test I just added

--- panel/tests/widgets/test_ace.py
+++ panel/tests/widgets/test_ace.py
@@ -1,17 +1,6 @@
 from __future__ import absolute_import, division, unicode_literals
 
 from panel.widgets import Ace
-import sys
-
-
-def test_with_no_pn_extension(document, comm):
-    print('panel.models.ace' in sys.modules)
-    sys.modules.pop('panel.models.ace', None)
-
-    ace = Ace(value="Hello, World", language="python")
-    widget = ace.get_root(document, comm=comm)
-
-    assert isinstance(widget, ace._widget_type)
 
 
 def test_ace(document, comm):
