dcd3272971dcc860b4a5eb800d28112dac474561
Marc Skov Madsen <MASMA@orsted.dk>
2020-02-16 08:17:57 +0100

    boiler plate code to make things concrete

--- panel/io/state.py
+++ panel/io/state.py
@@ -12,6 +12,24 @@ import param
 from bokeh.document import Document
 from bokeh.io import curdoc as _curdoc
 from pyviz_comms import CommManager as _CommManager
+from panel.models.location import Location
+
+
+class _Window(param.Parameterized):
+    """
+    Provides selected functionality of the JS window api
+    """
+
+    location = param.Parameter(
+        default=Location(),
+        doc="""
+        Provides access to selected functionality of the JS window.location api""",
+    )
+
+    def __init__(self, **params):
+        if "default" not in params:
+            params["default"] = Location()
+        super().__init__(**params)
 
 
 class _state(param.Parameterized):
@@ -20,17 +38,27 @@ class _state(param.Parameterized):
     apps to indicate their state to a user.
     """
 
-    cache = param.Dict(default={}, doc="""
-        Global location you can use to cache large datasets or
-        expensive computation results across multiple client sessions
-        for a given server.""")
+    cache = param.Dict(
+        default={},
+        doc="""
+       Global location you can use to cache large datasets or expensive computation results
+       across multiple client sessions for a given server.""",
+    )
 
-    webdriver = param.Parameter(default=None, doc="""
-        Selenium webdriver used to export bokeh models to pngs.""")
+    webdriver = param.Parameter(
+        default=None,
+        doc="""
+        Selenium webdriver used to export bokeh models to pngs.""",
+    )
 
-    _curdoc = param.ClassSelector(class_=Document, doc="""
+    window = param.Parameter(default=_Window())
+
+    _curdoc = param.ClassSelector(
+        class_=Document,
+        doc="""
         The bokeh Document for which a server event is currently being
-        processed.""")
+        processed.""",
+    )
 
     # Whether to hold comm events
     _hold = False
@@ -58,8 +86,8 @@ class _state(param.Parameterized):
     def __repr__(self):
         server_info = []
         for server, panel, docs in self._servers.values():
-            server_info.append("{}:{:d} - {!r}".format(
-                server.address or "localhost", server.port, panel)
+            server_info.append(
+                "{}:{:d} - {!r}".format(server.address or "localhost", server.port, panel)
             )
         if not server_info:
             return "state(servers=[])"
@@ -77,7 +105,7 @@ class _state(param.Parameterized):
     def _unblocked(self, doc):
         thread = threading.current_thread()
         thread_id = thread.ident if thread else None
-        return (doc is self.curdoc and self._thread_id == thread_id)
+        return doc is self.curdoc and self._thread_id == thread_id
 
     @property
     def curdoc(self):
