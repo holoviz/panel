686afa3728a0d5248cdc7b57762352ab35a4fd41
Marc Skov Madsen <MASMA@orsted.dk>
2020-02-24 05:53:53 +0100

    added many tests. added readonly and regex to Location class

--- panel/widgets/location.py
+++ panel/widgets/location.py
@@ -8,23 +8,33 @@ from panel.models.location import Location as _BkLocation
 from .base import Widget
 import param
 
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Optional
+
+import param
+from panel.widgets.base import Widget
 
 
 class Location(Widget):
     href = param.String(
+        readonly=True,
         doc="""The full url, e.g. \
-            'https://panel.holoviz.org/user_guide/Interact.html:80?color=blue#interact'"""
+            'https://panel.holoviz.org/user_guide/Interact.html:80?color=blue#interact'""",
     )
-    hostname = param.String(doc="hostname in window.location e.g. 'panel.holoviz.org'")
-    pathname = param.String(doc="pathname in window.location e.g. 'user_guide/Interact.html'")
-    protocol = param.String(doc="protocol in window.location e.g. 'https:'")
-    port = param.String(doc="port in window.location e.g. '80'")
-    search = param.String(doc="search in window.location e.g. '?color=blue'")
-    hash_ = param.String(doc="hash in window.location e.g. '#interact'")
+    hostname = param.String(
+        readonly=True, doc="hostname in window.location e.g. 'panel.holoviz.org'"
+    )
+    # Todo: Find the corect regex for pathname
+    pathname = param.String(
+        regex="([^\/])(.*)([^\/])",
+        doc="pathname in window.location e.g. 'user_guide/Interact.html'",
+    )
+    protocol = param.String(readonly=True, doc="protocol in window.location e.g. 'https:'")
+    port = param.String(readonly=True, doc="port in window.location e.g. '80'")
+    search = param.String(regex=r"(\?.*)?", doc="search in window.location e.g. '?color=blue'")
+    hash_ = param.String(regex=r"(\#.*)?", doc="hash in window.location e.g. '#interact'")
 
     refresh = param.Boolean(
-        default=True,
+        default=False,
         doc="""Refresh the page when the location is updated. For multipage apps this should be \
         set to True, For single page apps this should be set to False""",
     )
@@ -32,7 +42,7 @@ class Location(Widget):
     _widget_type = _BkLocation  # type: ignore
 
     # Mapping from parameter name to bokeh model property name
-    _rename: Dict[str, str] = {"name": None}
+    _rename: Dict[str, Optional[str]] = {"name": None}
 
     def update_search(
         self, param_class: param.Parameterized, parameters: Optional[List[str]] = None
