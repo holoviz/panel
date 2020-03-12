9dd5ebb8ea5db28eb517adc931499293acee03c0
Philipp Rudiger <prudiger@anaconda.com>
2020-03-12 09:48:27 +0100

    Update test

--- panel/tests/widgets/test_base.py
+++ panel/tests/widgets/test_base.py
@@ -4,12 +4,14 @@ import param
 import pytest
 
 from panel.io import block_comm
-from panel.widgets import CompositeWidget, DataFrame, TextInput, ToggleGroup, Widget
+from panel.widgets import (
+    CompositeWidget, DataFrame, FileDownload, TextInput, ToggleGroup, Widget
+)
 from panel.tests.util import check_layoutable_properties, py3_only
 
 all_widgets = [w for w in param.concrete_descendents(Widget).values()
                if not w.__name__.startswith('_') and
-               not issubclass(w, (CompositeWidget, DataFrame, ToggleGroup))]
+               not issubclass(w, (CompositeWidget, DataFrame, FileDownload, ToggleGroup))]
 
 
 @py3_only
