479abf05b6abe763dc804e68d2a88773cc29e9a9
Philipp Rudiger <prudiger@anaconda.com>
2020-04-13 16:35:33 +0200

    Cleaned up Card implementation

--- panel/layout/card.py
+++ panel/layout/card.py
@@ -3,29 +3,68 @@ import param
 from bokeh.models import Div as BkDiv
 
 from ..models import Card as BkCard
-from .base import Column
+from ..viewable import Viewable
+from .base import Column, Row
 
 
 class Card(Column):
+    """
+    A Card layout allows arranging multiple panel objects in a
+    collapsible, vertical container with a header bar.
+    """
 
-    collapsed = param.Boolean(default=False)
+    button_color = param.String(doc="""
+        A valid CSS color to apply to the button.""")
 
-    title = param.String()
-    
+    button_css_classes = param.List(['card-button'], doc="""
+        CSS classes to apply to the button element.""")
+
+    collapsed = param.Boolean(default=False, doc="""
+        Whether the contents of the Card are collapsed.""")
+
+    css_classes = param.List(['card'], doc="""
+        CSS classes to apply to the overall Card.""")
+
+    header = param.Parameter(doc="""
+        A Panel component to display in the header bar of the Card.
+        Will override the given title if defined.""")
+
+    header_background = param.String(doc="""
+        A valid CSS color to the header background.""")
+
+    header_css_classes = param.List(['card-header'], doc="""
+        CSS claasses to apply to the heaader element.""")
+
     margin = param.Parameter(default=5)
 
-    css_classes = param.List(['card'])
+    title = param.String(doc="""
+        A title to be displayed in the Card header, will be overridden
+        by the header if defined.""")
 
-    button_css_classes = param.List(['card-button'])
-
-    header_css_classes = param.List(['card-header'])
-    
     _bokeh_model = BkCard
     
-    _rename = dict(Column._rename, title=None)
+    _rename = dict(Column._rename, title=None, header=None)
+
+    def __init__(self, *objects, **params):
+        self._header_layout = Row(css_classes=['card-header-row'])
+        super(Card, self).__init__(*objects, **params)
+        self.param.watch(self._update_header, ['title', 'header'])
+        self._update_header()
+
+    def _update_header(self, *events):
+        from ..pane import HTML, panel
+        if self.header is None:
+            item = HTML('%s' % (self.title or "&#8203;"),
+                        margin=(0, 10), css_classes=['card-title'])
+        else:
+            item = panel(self.header)
+        self._header_layout[:] = [item]
 
     def _get_objects(self, model, old_objects, doc, root, comm=None):
-        from ..pane import panel
-        header = BkDiv(text='<h3>%s</h3>' % self.title, margin=(0, 10))
+        ref = root.ref['id']
+        if ref in self._header_layout._models:
+            header = self._header_layout._models[ref][0]
+        else:
+            header = self._header_layout._get_model(doc, root, model, comm)
         objects = super(Card, self)._get_objects(model, old_objects, doc, root)
         return [header]+objects
--- panel/models/card.ts
+++ panel/models/card.ts
@@ -11,19 +11,19 @@ import {Size} from "@bokehjs/core/layout/types"
 
 export class CollapseableColumnLayout extends ColumnLayout {
   collapsed: boolean
-  
+
   constructor(items: Layoutable[], collapsed: boolean = false) {
     super(items)
-	this.collapsed = collapsed
+    this.collapsed = collapsed
   }
 
   protected _measure_totals(row_heights: number[], col_widths: number[]): Size {
-	// Do not count height of collapsed height
-	const heights = this.collapsed ? row_heights.slice(0, 1) : row_heights
-	const last = heights.length-1
-	if (!this.collapsed)
+    // Do not count height of collapsed height
+    const heights = this.collapsed ? row_heights.slice(0, 1) : row_heights
+    const last = heights.length-1
+    if (!this.collapsed)
       heights[last] = heights[last] + 5
-	return super._measure_totals(heights, col_widths)
+    return super._measure_totals(heights, col_widths)
   }
 }
 
@@ -38,7 +38,7 @@ export class CardView extends ColumnView {
 
   _update_layout(): void {
     const items = this.child_views.map((child) => child.layout)
-	this.layout = new CollapseableColumnLayout(items, this.model.collapsed)
+    this.layout = new CollapseableColumnLayout(items, this.model.collapsed)
     this.layout.rows = this.model.rows
     this.layout.spacing = [this.model.spacing, 0]
     this.layout.set_sizing(this.box_sizing())
@@ -47,40 +47,43 @@ export class CardView extends ColumnView {
   render(): void {
     empty(this.el)
 
-    const {background, button_css_classes, header_tag, header_css_classes} = this.model
+    const {background, button_color, header_background, button_css_classes, header_tag, header_css_classes} = this.model
+
     this.el.style.backgroundColor = background != null ? background : ""
     classes(this.el).clear().add(...this.css_classes())
 
     const header_el = DOM.createElement((header_tag as any), {class: header_css_classes})
+    header_el.style.backgroundColor = header_background != null ? header_background : ""
     const header = this.child_views[0]
     header_el.appendChild(header.el)
 
     if (this.model.collapsible) {
       this.button_el = DOM.createElement("button", {class: button_css_classes, type: "button"})
       this.button_el.innerHTML = this.model.collapsed ? "+" : "-"
+      this.button_el.style.color = button_color != null ? button_color : ""
       this.button_el.onclick = () => this._toggle_button()
       header_el.appendChild(this.button_el)
     }
 
-	this.el.appendChild(header_el)
+    this.el.appendChild(header_el)
     header.render()
 
     for (const child_view of this.child_views.slice(1)) {
       if (!this.model.collapsed)
-		this.el.appendChild(child_view.el)
+        this.el.appendChild(child_view.el)
       child_view.render()
     }
   }
 
   _toggle_button(): void {
-	this.model.collapsed = !this.model.collapsed
-	this._collapse()
+    this.model.collapsed = !this.model.collapsed
+    this._collapse()
   }
-  
+
   _collapse(): void {
     this.button_el.innerHTML = this.model.collapsed ? '+': '-'
-	this.rebuild()
-	this.resize_layout()
+    this.rebuild()
+    this.resize_layout()
   }
 
   protected _createElement(): HTMLElement {
@@ -92,11 +95,13 @@ export namespace Card {
   export type Attrs = p.AttrsOf<Props>
 
   export type Props = Column.Props & {
+    button_color: p.Property<string | null>
     button_css_classes: p.Property<string[]>
     collapsed: p.Property<boolean>
     collapsible: p.Property<boolean>
+    header_background: p.Property<string | null>
     header_css_classes: p.Property<string[]>
-    header_tag: p.Property<string> 
+    header_tag: p.Property<string>
     tag: p.Property<string>
   }
 }
@@ -116,9 +121,11 @@ export class Card extends Column {
     this.prototype.default_view = CardView
 
     this.define<Card.Props>({
+      button_color:       [ p.String,  null  ],
       button_css_classes: [ p.Array,   []    ],
       collapsed:          [ p.Boolean, true  ],
       collapsible:        [ p.Boolean, true  ],
+      header_background:  [ p.String,  null  ],
       header_css_classes: [ p.Array,   []    ],
       header_tag:         [ p.String,  "div" ],
       tag:                [ p.String,  "div" ],
