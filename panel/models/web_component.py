371cd44690ebcbb29bca8e3966142aa7156ba443
Marc Skov Madsen <MASMA@orsted.dk>
2020-05-24 05:15:12 +0200

    style changes

--- panel/models/web_component.py
+++ panel/models/web_component.py
@@ -3,13 +3,12 @@
 See https://www.predix-ui.com/#/elements/px-app-nav"""
 
 from bokeh.core import properties
-from bokeh.models.layouts import HTMLBox
 from bokeh.models import ColumnDataSource
+from bokeh.models.layouts import HTMLBox
+
 
 class WebComponent(HTMLBox):
-    """A Bokeh Model that enables easily creating new Panel components from web components
-
-    See https://www.predix-ui.com/#/elements/px-app-nav"""
+    """A Bokeh Model that enables easily creating new Panel components from web components"""
     componentType = properties.String('htmlbox')
     innerHTML = properties.String('')
     attributesToWatch = properties.Dict(properties.String, properties.Any)
