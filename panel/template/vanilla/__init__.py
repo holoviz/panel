f007f95c7a16bc8d77c26545c25552d90e90f7f6
Kenneth Bowen <kebowen730@gmail.com>
2020-06-09 12:08:49 -0500

    adding a vanilla html template to panel

---
+++ panel/template/vanilla/__init__.py
@@ -0,0 +1,34 @@
+"""
+Vanilla template 
+"""
+import pathlib
+
+import param
+
+from ...layout import Card
+from ..base import BasicTemplate
+from ..theme import DarkTheme, DefaultTheme
+
+
+
+class VanillaTemplate(BasicTemplate):
+    """
+    VanillaTemplate is built on top of Vanilla web components.
+    """
+
+    _css = pathlib.Path(__file__).parent / 'vanilla.css'
+
+    _template = pathlib.Path(__file__).parent / 'vanilla.html'
+
+class VanillaDefaultTheme(DefaultTheme):
+
+    css = param.Filename(default=pathlib.Path(__file__).parent / 'default.css')
+
+    _template = VanillaTemplate
+
+
+class VanillaDarkTheme(DarkTheme):
+
+    css = param.Filename(default=pathlib.Path(__file__).parent / 'dark.css')
+
+    _template = VanillaTemplate
