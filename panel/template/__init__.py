951b4b5377934ad63792fe3cce3c38db36b46dcd
Kenneth Bowen <kebowen730@gmail.com>
2020-06-10 13:17:37 -0500

    fixing noqa declaration

--- panel/template/__init__.py
+++ panel/template/__init__.py
@@ -2,4 +2,4 @@ from .base import Template, BaseTemplate # noqa
 from .bootstrap import BootstrapTemplate # noqa
 from .material import MaterialTemplate # noqa
 from .theme import DarkTheme, DefaultTheme # noqa
-from .vanilla import VanillaTemplate #noqa
+from .vanilla import VanillaTemplate # noqa
