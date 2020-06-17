626ac27ae7294401445e4aea6956951e831f0a46
Marc Skov Madsen <marc.skov.madsen@gmail.com>
2020-06-17 18:55:24 +0200

    Alert pane: Allows providing contextual feedback messages for typical user actions (#1181)

---
+++ panel/pane/alert.py
@@ -0,0 +1,51 @@
+"""Bootstrap inspired Alerts
+
+See https://getbootstrap.com/docs/4.0/components/alerts/
+"""
+import param
+
+from panel.pane.markup import Markdown
+
+ALERT_TYPES = ["primary", "secondary", "success", "danger", "warning", "info", "light", "dark"]
+
+
+class Alert(Markdown):
+    """
+    An Alert that renders Markdown
+
+    - CSS Styling is done via the classes `alert` and `alert-TYPE`, where TYPE is the alert_type.
+    - sizing_mode is set to `stretch_width` by default
+    """
+
+    alert_type = param.ObjectSelector("primary", objects=ALERT_TYPES)
+
+    def __init__(self, text: str, **kwargs):
+        """An Primary Alert that renders Markdown
+
+        - CSS Styling is done via the classes `alert` and `alert-primary`.
+        - sizing_mode is set to `stretch_width` by default
+
+        Arguments:
+            text {str} -- Some MarkDown text
+        """
+        if "margin" not in kwargs:
+            kwargs["margin"] = (0, 0, 25, 0)
+        if "sizing_mode" not in kwargs:
+            kwargs["sizing_mode"] = "stretch_width"
+
+        super().__init__(text, **kwargs)
+
+        self._set_css_classes()
+
+    @param.depends("alert_type", watch=True)
+    def _set_css_classes(self):
+        css_classes = []
+        if self.css_classes:
+            for class_ in self.css_classes:
+                if class_ != "alert" and not class_.startswith("alert-"):
+                    css_classes.append(class_)
+
+        css_classes.append("alert")
+        css_classes.append(f"alert-{self.alert_type}")
+
+        self.css_classes = css_classes
