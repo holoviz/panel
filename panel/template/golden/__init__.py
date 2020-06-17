7971ce0b871082eb6618efa3e556f0f4f72a5392
Kenneth Bowen <kebowen730@gmail.com>
2020-06-17 03:21:32 -0500

    solving an issue with golden layout by copying the golden layout light and dark themes into corresponding css themes to get around issues with importing the css files and using links in html

--- panel/template/golden/__init__.py
+++ panel/template/golden/__init__.py
@@ -9,13 +9,13 @@ from ...config import config
 from ...layout import Row, Column, HSpacer, Spacer, ListLike, Card	
 from ...pane import HTML	
 from ..base import BasicTemplate	
+from ..theme import DarkTheme, DefaultTheme
 
 
 class GoldenTemplate(BasicTemplate):	
     """	
     GoldenTemplate is built on top of golden-layout library.	
     """	
-
     _css = pathlib.Path(__file__).parent / 'golden.css'	
 
     _template = pathlib.Path(__file__).parent / 'golden.html'	
@@ -25,3 +25,16 @@ class GoldenTemplate(BasicTemplate):
             'button_css_classes': ['golden-card-button']	
         },	
     }
+
+class GoldenDefaultTheme(DefaultTheme):
+
+    css = param.Filename(default=pathlib.Path(__file__).parent / 'default.css')
+
+    _template = GoldenTemplate
+
+
+class GoldenDarkTheme(DarkTheme):
+
+    css = param.Filename(default=pathlib.Path(__file__).parent / 'dark.css')
+
+    _template = GoldenTemplate
