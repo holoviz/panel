ee6b1a8c000b684f625fe19c5df189f71a6b38bf
Kenneth Bowen <kebowen730@gmail.com>
2020-06-18 00:57:19 -0500

    adding GoldenTemplate to the template __init__.py file

--- panel/template/__init__.py
+++ panel/template/__init__.py
@@ -2,3 +2,4 @@ from .base import Template, BaseTemplate # noqa
 from .bootstrap import BootstrapTemplate # noqa
 from .material import MaterialTemplate # noqa
 from .theme import DarkTheme, DefaultTheme # noqa
+from .golden import GoldenTemplate # noqa
