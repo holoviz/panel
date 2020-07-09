8954a7004d20902c8a771c9d1ccd461a6ef4de94
Philipp Rudiger <prudiger@anaconda.com>
2020-07-09 13:38:13 +0200

    Fix issue with requirejs

--- panel/command/serve.py
+++ panel/command/serve.py
@@ -95,10 +95,12 @@ class Serve(_BkServe):
             kwargs['index'] = INDEX_HTML
 
         # Handle tranquilized functions in the supplied functions
-        kwargs['extra_patterns'] = patterns = []
+        kwargs['extra_patterns'] = patterns = kwargs.get('extra_patterns', [])
 
         if args.static_dirs:
-            patterns += get_static_routes(parse_vars(args.static_dirs))
+            static_dirs = parse_vars(args.static_dirs)
+            static_dirs['panel_dist'] = os.path.join(os.path.split(__file__)[0], 'dist')
+            patterns += get_static_routes()
 
         if args.oauth_provider:
             config.oauth_provider = args.oauth_provider
