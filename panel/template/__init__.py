e7d817cc5e9ecf3eb558d1bf64f450a8b3408ffd
kbowen <kebowen730@gmail.com>
2020-07-09 05:25:35 -0500

    Add vanilla template (#1421)

--- panel/template/__init__.py
+++ panel/template/__init__.py
@@ -3,3 +3,4 @@ from .bootstrap import BootstrapTemplate # noqa
 from .material import MaterialTemplate # noqa
 from .theme import DarkTheme, DefaultTheme # noqa
 from .golden import GoldenTemplate # noqa
+from .vanilla import VanillaTemplate # noqa
