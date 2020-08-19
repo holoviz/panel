066a7fa7e53ee236bf1b381bb026bd26d9cd2dc4
Philipp Rudiger <prudiger@anaconda.com>
2020-08-19 15:23:02 +0200

    Add MenuButton widget (#1533)
    * Add MenuButton widget
    
    * Add docs

--- panel/widgets/button.py
+++ panel/widgets/button.py
@@ -8,7 +8,9 @@ from functools import partial
 
 import param
 
-from bokeh.models import Button as _BkButton, Toggle as _BkToggle
+from bokeh.models import (
+    Button as _BkButton, Toggle as _BkToggle, Dropdown as _BkDropdown
+)
 
 from .base import Widget
 
@@ -23,37 +25,17 @@ class _ButtonBase(Widget):
     __abstract = True
 
 
-class Button(_ButtonBase):
+class _ClickButton(_ButtonBase):
 
-    clicks = param.Integer(default=0)
+    __abstract = True
 
-    _rename = {'clicks': None, 'name': 'label'}
-
-    _widget_type = _BkButton
+    _event = 'button_click'
 
     def _get_model(self, doc, root=None, parent=None, comm=None):
-        model = super(Button, self)._get_model(doc, root, parent, comm)
+        model = super(_ClickButton, self)._get_model(doc, root, parent, comm)
         ref = (root or model).ref['id']
         model.on_click(partial(self._server_click, doc, ref))
         return model
-
-    def _server_click(self, doc, ref, event):
-        self._events.update({"clicks": 1})
-        if not self._processing:
-            self._processing = True
-            if doc.session_context:
-                doc.add_timeout_callback(partial(self._change_event, doc), self._debounce)
-            else:
-                self._change_event(doc)
-
-    def _process_property_change(self, msg):
-        msg = super(Button, self)._process_property_change(msg)
-        if 'clicks' in msg:
-            msg['clicks'] = self.clicks + 1
-        return msg
-
-    def on_click(self, callback):
-        self.param.watch(callback, 'clicks')
 
     def js_on_click(self, args={}, code=""):
         """
@@ -73,7 +55,7 @@ class Button(_ButtonBase):
           The Callback which can be used to disable the callback.
         """
         from ..links import Callback
-        return Callback(self, code={'event:button_click': code}, args=args)
+        return Callback(self, code={'event:'+self._event: code}, args=args)
 
     def jscallback(self, args={}, **callbacks):
         """
@@ -98,10 +80,36 @@ class Button(_ButtonBase):
         from ..links import Callback
         for k, v in list(callbacks.items()):
             if k == 'clicks':
-                k = 'event:button_click'
+                k = 'event:'+self._event
             callbacks[k] = self._rename.get(v, v)
         return Callback(self, code=callbacks, args=args)
 
+
+class Button(_ClickButton):
+
+    clicks = param.Integer(default=0)
+
+    _rename = {'clicks': None, 'name': 'label'}
+
+    _widget_type = _BkButton
+
+    def _server_click(self, doc, ref, event):
+        self._events.update({"clicks": self.clicks+1})
+        if not self._processing:
+            self._processing = True
+            if doc.session_context:
+                doc.add_timeout_callback(partial(self._change_event, doc), self._debounce)
+            else:
+                self._change_event(doc)
+
+    def _process_property_change(self, msg):
+        msg = super(Button, self)._process_property_change(msg)
+        if 'clicks' in msg:
+            msg['clicks'] = self.clicks + 1
+        return msg
+
+    def on_click(self, callback):
+        self.param.watch(callback, 'clicks', onlychanged=False)
 
 
 class Toggle(_ButtonBase):
@@ -118,3 +126,34 @@ class Toggle(_ButtonBase):
     def _get_embed_state(self, root, values=None, max_opts=3):
         return (self, self._models[root.ref['id']][0], [False, True],
                 lambda x: x.active, 'active', 'cb_obj.active')
+
+
+class MenuButton(_ClickButton):
+
+    clicked = param.String(default=None, doc="""
+      Last menu item that was clicked.""")
+
+    items = param.List(default=[], doc="""
+      Menu items in the dropdown. Allows strings, tuples of the form
+      (title, value) or Nones to separate groups of items.""")
+
+    split = param.Boolean(default=False, doc="""
+      Whether to add separate dropdown area to button.""")
+
+    _widget_type = _BkDropdown
+
+    _rename = {'name': 'label', 'items': 'menu', 'clicked': None}
+
+    _event = 'menu_item_click'
+
+    def on_click(self, callback):
+        self.param.watch(callback, 'clicked', onlychanged=False)
+
+    def _server_click(self, doc, ref, event):
+        self._events.update({"clicked": event.item})
+        if not self._processing:
+            self._processing = True
+            if doc.session_context:
+                doc.add_timeout_callback(partial(self._change_event, doc), self._debounce)
+            else:
+                self._change_event(doc)
