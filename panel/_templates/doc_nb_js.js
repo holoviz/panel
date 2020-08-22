09e7da833631d2ff21e05b72b101c16045858beb
Philipp Rudiger <prudiger@anaconda.com>
2020-08-22 19:55:20 +0200

    Fix for async panel load

--- panel/_templates/doc_nb_js.js
+++ panel/_templates/doc_nb_js.js
@@ -1,15 +1,15 @@
 (function(root) {
   function embed_document(root) {
-  var docs_json = {{ docs_json }};
-  var render_items = {{ render_items }};
-  root.Bokeh.embed.embed_items_notebook(docs_json, render_items);
+    var docs_json = {{ docs_json }};
+    var render_items = {{ render_items }};
+    root.Bokeh.embed.embed_items_notebook(docs_json, render_items);
   }
   if (root.Bokeh !== undefined && root.Bokeh.Panel !== undefined) {
     embed_document(root);
   } else {
     var attempts = 0;
     var timer = setInterval(function(root) {
-      if (root.Bokeh !== undefined) {
+      if (root.Bokeh !== undefined && root.Bokeh.Panel !== undefined) {
         clearInterval(timer);
         embed_document(root);
       } else if (document.readyState == "complete") {
