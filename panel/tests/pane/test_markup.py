35ccfc40825a3d34249d0e80dc3e46a04ed92035
Philipp Rudiger <prudiger@anaconda.com>
2020-10-13 11:49:11 +0200

    Fixed test

--- panel/tests/pane/test_markup.py
+++ panel/tests/pane/test_markup.py
@@ -88,7 +88,7 @@ def test_markdown_pane_extensions(document, comm):
     # Create pane
     model = pane.get_root(document, comm=comm)
     assert pane._models[model.ref['id']][0] is model
-    assert model.text.startswith('&lt;div class=&quot;python codehilite')
+    assert model.text.startswith('&lt;div class=&quot;codehilite')
 
     pane.extensions = ["extra", "smarty"]
     assert model.text.startswith('&lt;pre&gt;&lt;code class=&quot;language-python')
