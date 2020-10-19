0fcff75e69ddb668666a5c2fdc72208a16d18d93
Philipp Rudiger <prudiger@anaconda.com>
2020-10-19 15:48:05 +0200

    Do not bundle latex dependencies

--- panel/models/katex.py
+++ panel/models/katex.py
@@ -3,35 +3,23 @@ Defines a custom KaTeX bokeh model to render text using KaTeX.
 """
 from bokeh.models import Markup
 
-from ..util import classproperty, bundled_files
-
 
 class KaTeX(Markup):
     """
     A bokeh model that renders text using KaTeX.
     """
 
-    __css_raw__ = ["https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css"]
+    __css__ = ["https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css"]
 
-    @classproperty
-    def __css__(cls):
-        return bundled_files(cls, 'css')
-
-    __javascript_raw__ = [
+    __javascript__ = [
         "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.js",
         "https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/contrib/auto-render.min.js"
     ]
 
-    @classproperty
-    def __javascript__(cls):
-        return bundled_files(cls)
-
-    @classproperty
-    def __js_skip__(cls):
-        return {
-            'katex': cls.__javascript__[:1],
-            'renderMathInElement': cls.__javascript__[1:]
-        }
+    __js_skip__ = {
+        'katex': __javascript__[:1],
+        'renderMathInElement': __javascript__[1:]
+    }
 
     __js_require__ = {
         'paths': {
--- panel/models/mathjax.py
+++ panel/models/mathjax.py
@@ -3,25 +3,17 @@ Defines a custom MathJax bokeh model to render text using MathJax.
 """
 from bokeh.models import Markup
 
-from ..util import classproperty, bundled_files
-
 
 class MathJax(Markup):
     """
     A bokeh model that renders text using MathJax.
     """
 
-    __javascript_raw__ = [
-        "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-MML-AM_CHTML"
+    __javascript__ = [
+        "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML"
     ]
 
-    @classproperty
-    def __javascript__(cls):
-        return bundled_files(cls)
-
-    @classproperty
-    def __js_skip__(cls):
-        return {'MathJax': cls.__javascript__}
+    __js_skip__ = {'MathJax': __javascript__}
 
     __js_require__ = {
         'paths': {
