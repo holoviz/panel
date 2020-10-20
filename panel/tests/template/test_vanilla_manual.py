25bd1b64baa66eaa71890938dede0edcc1fc4702
Marc Skov Madsen <MASMA@orsted.dk>
2020-10-20 17:30:39 +0200

    test vanilla template

--- panel/tests/template/test_vanilla_manual.py
+++ panel/tests/template/test_vanilla_manual.py
@@ -1,11 +1,12 @@
 import panel as pn
 import numpy as np
 import holoviews as hv
+import pytest
 
 LOGO = "https://panel.holoviz.org/_static/logo_horizontal.png"
 
 
-def test_vanilla_with_sidebar():
+def test_template_with_sidebar(template_class=pn.template.VanillaTemplate):
     """Returns an app that uses the vanilla template in various ways.
 
 Inspect the app and verify that the issues of [Issue 1641]\
@@ -17,7 +18,7 @@ Inspect the app and verify that the issues of [Issue 1641]\
 - Independent scroll for sidebar and main
 - Only vertical scrollbars
 """
-    vanilla = pn.template.VanillaTemplate(
+    vanilla = template_class(
         title="Vanilla Template",
         logo=LOGO,
     )
@@ -38,7 +39,7 @@ Inspect the app and verify that the issues of [Issue 1641]\
 
     vanilla.sidebar.append(freq)
     vanilla.sidebar.append(phase)
-    vanilla.sidebar.append(pn.pane.Markdown(test_vanilla_with_sidebar.__doc__))
+    vanilla.sidebar.append(pn.pane.Markdown(test_template_with_sidebar.__doc__))
     vanilla.sidebar.append(pn.pane.Markdown("## Sidebar Item\n" * 50))
 
     vanilla.main.append(
@@ -66,7 +67,7 @@ Inspect the app and verify that the issues of [Issue 1641]\
     return vanilla
 
 
-def test_vanilla_with_no_sidebar():
+def test_template_with_no_sidebar(template_class=pn.template.VanillaTemplate):
     """Returns an app that uses the vanilla template in various ways.
 
 Inspect the app and verify that the issues of [Issue 1641]\
@@ -78,7 +79,7 @@ Inspect the app and verify that the issues of [Issue 1641]\
 - Independent scroll for sidebar and main
 - Only vertical scrollbars
 """
-    vanilla = pn.template.VanillaTemplate(
+    vanilla = template_class(
         title="Vanilla Template",
         logo=LOGO,
         favicon="https://raw.githubusercontent.com/MarcSkovMadsen/awesome-panel/2781d86d4ed141889d633748879a120d7d8e777a/assets/images/favicon.ico",
@@ -100,7 +101,7 @@ Inspect the app and verify that the issues of [Issue 1641]\
 
     vanilla.main.append(freq)
     vanilla.main.append(phase)
-    vanilla.main.append(pn.pane.Markdown(test_vanilla_with_no_sidebar.__doc__))
+    vanilla.main.append(pn.pane.Markdown(test_template_with_no_sidebar.__doc__))
     vanilla.main.append(
         pn.Row(
             pn.Card(hv.DynamicMap(sine), title="Sine"),
@@ -125,7 +126,21 @@ Inspect the app and verify that the issues of [Issue 1641]\
     vanilla.main_max_width = "600px"
     return vanilla
 
+TEMPLATES = [
+    (pn.template.VanillaTemplate, ),
+    (pn.template.MaterialTemplate),
+    (pn.template.BootstrapTemplate),
+    (pn.template.GoldenTemplate),
+]
+
+@pytest.mark.parametrize(["template_class"], TEMPLATES)
+def test_all_templates_with_sidebar(template_class):
+    test_template_with_sidebar(template_class=template_class)
+
+@pytest.mark.parametrize(["template_class"], TEMPLATES)
+def test_all_templates_with_no_sidebar(template_class):
+    test_template_with_no_sidebar(template_class=template_class)
 
 if __name__.startswith("bokeh"):
-    test_vanilla_with_sidebar().servable()
-    # test_vanilla_with_no_sidebar().servable()
+    test_template_with_sidebar(template_class=pn.template.GoldenTemplate).servable()
+    # test_template_with_no_sidebar().servable()
