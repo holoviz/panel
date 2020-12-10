dd8e73c4c60a423dec164ae08cad7be69c695276
Hoxbro <simon.hansen@me.com>
2020-12-10 19:15:53 +0100

    Add throttled argument to pn.param widgets parameter (#1800)
    * Adding throttled to param
    
    * Moved throttled to widgets argument
    
    * Added value_throttled to updates
    
    * Added unittest
    
    * More clear seperation if widget is throttled or not
    
    * Added documentation

--- panel/tests/test_param.py
+++ panel/tests/test_param.py
@@ -626,6 +626,47 @@ def test_set_widgets(document, comm):
     assert number.height == 100
     assert isinstance(text, TextInput)
 
+
+def test_set_widgets_throttled(document, comm):
+    class Test(param.Parameterized):
+        a = param.Number(default=0, bounds=(0, 10), precedence=1)
+
+    test = Test()
+    pane = Param(test)
+    model = pane.get_root(document, comm=comm)
+
+    pane.widgets = {"a": {"throttled": False}}
+    assert len(model.children) == 2
+    _, number = model.children
+
+    number.value = 1
+    assert number.value == 1
+    assert number.value_throttled != 1
+    assert test.a == 1
+
+    test.a = 2
+    assert number.value == 2
+    assert number.value_throttled != 2
+    assert test.a == 2
+
+    # By setting throttled to true,
+    # `test.a` is linked to `number.value_throttled`
+    # instead of `number.value`.
+    pane.widgets = {"a": {"throttled": True}}
+    assert len(model.children) == 2
+    _, number = model.children
+
+    number.value_throttled = 3
+    assert number.value != 3
+    assert number.value_throttled == 3
+    assert test.a == 3
+
+    test.a = 4
+    assert number.value != 4
+    assert number.value_throttled == 4
+    assert test.a == 4
+
+
 def test_set_show_name(document, comm):
     class Test(param.Parameterized):
         a = param.Number(bounds=(0, 10))
