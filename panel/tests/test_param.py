3d8abe1dc701633bc4d60d040e18aafa68347317
Simon Hoxbro <simon.hansen@me.com>
2020-12-14 17:12:26 +0100

    Added unittest

--- panel/tests/test_param.py
+++ panel/tests/test_param.py
@@ -419,6 +419,16 @@ def test_object_selector_param_overrides(document, comm):
     assert select.disabled == False
 
 
+def test_number_input_none_support():
+    class Test(param.Parameterized) :
+        number = param.Number(default=0, allow_None=True)
+        none = param.Number(default=None, allow_None=True)
+
+    test_widget = Param(Test())
+    assert test_widget[1].value == 0
+    assert test_widget[2].value is None
+
+
 def test_explicit_params(document, comm):
     class Test(param.Parameterized):
         a = param.Boolean(default=False)
