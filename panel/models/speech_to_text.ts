a4f9f1fd678f7cee1d0d5df85b6f5e4e9a138d56
Marc Skov Madsen <masma@orsted.dk>
2021-01-01 11:58:40 +0100

    firsts working version

--- panel/models/speech_to_text.ts
+++ panel/models/speech_to_text.ts
@@ -1,46 +1,133 @@
 import * as p from "@bokehjs/core/properties"
 import { HTMLBox, HTMLBoxView } from "@bokehjs/models/layouts/html_box"
 
+const iconStarted = `<svg xmlns="http://www.w3.org/2000/svg" height="22px" style="vertical-align: middle;" fill="currentColor" class="bi bi-mic" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/>
+  <path fill-rule="evenodd" d="M10 8V3a2 2 0 1 0-4 0v5a2 2 0 1 0 4 0zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3z"/>
+</svg>`
+const iconNotStarted = `<svg xmlns="http://www.w3.org/2000/svg" height="22px" style="vertical-align: middle;" fill="currentColor" class="bi bi-mic-mute" viewBox="0 0 16 16">
+<path fill-rule="evenodd" d="M12.734 9.613A4.995 4.995 0 0 0 13 8V7a.5.5 0 0 0-1 0v1c0 .274-.027.54-.08.799l.814.814zm-2.522 1.72A4 4 0 0 1 4 8V7a.5.5 0 0 0-1 0v1a5 5 0 0 0 4.5 4.975V15h-3a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-3v-2.025a4.973 4.973 0 0 0 2.43-.923l-.718-.719zM11 7.88V3a3 3 0 0 0-5.842-.963l.845.845A2 2 0 0 1 10 3v3.879l1 1zM8.738 9.86l.748.748A3 3 0 0 1 5 8V6.121l1 1V8a2 2 0 0 0 2.738 1.86zm4.908 3.494l-12-12 .708-.708 12 12-.708.707z"/>
+</svg>`
+
+const titleStarted = "Click to STOP the speech recognition.";
+const titleNotStarted = "Click to START the speech recognition.";
+
 // Hack inspired by https://stackoverflow.com/questions/38087013/angular2-web-speech-api-voice-recognition
-export interface IWindow extends Window {
+interface IWindow extends Window {
     webkitSpeechRecognition: any;
+    webkitSpeechGrammarList: any;
 }
 const {webkitSpeechRecognition} : IWindow = <IWindow><unknown>window;
+const {webkitSpeechGrammarList} : IWindow = <IWindow><unknown>window;
+
+function htmlToElement(html: string) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return <HTMLElement>template.content.firstChild;
+}
+
+function deserializeGrammars(grammars: any[]){
+    if (grammars){
+        var speechRecognitionList = new webkitSpeechGrammarList();
+        for (let grammar of grammars){
+            if (grammar.src){
+                speechRecognitionList.addFromString(grammar.src, grammar.weight)
+            } else if (grammar.uri) {
+                speechRecognitionList.addFromURI(grammar.uri, grammar.weight)
+            }
+        }
+        return speechRecognitionList
+    } else {
+        return null;
+    }
+}
+
+function serializeResults(results_: any) {
+    const results = [];
+    for (let result of results_) {
+        let alternatives: { confidence: number; transcript: string; }[] = [];
+        let item = { is_final: result.isFinal, alternatives: alternatives };
+        for (let i = 0; i < result.length; i++) {
+            let alternative = { confidence: result[i].confidence, transcript: result[i].transcript };
+            alternatives.push(alternative);
+        }
+        item.alternatives = alternatives;
+        results.push(item);
+    }
+    return results;
+}
 
 export class SpeechToTextView extends HTMLBoxView {
     model: SpeechToText
     recognition: any
+    buttonEl: HTMLElement
 
     initialize(): void {
-        console.log("initialize")
-        console.log(this.model)
         super.initialize()
 
         this.recognition = new webkitSpeechRecognition();
-        console.log(this.recognition);
+        this.setGrammars()
+        const this_ = this;
+
+        this.recognition.onresult = function(event: any) {
+            this_.model.results=serializeResults(event.results);
+          }
+        this.recognition.onerror = function(event: any) {
+            console.log("SpeechToText Error")
+            console.log(event);
+        }
+        this.recognition.onnomatch = function(event: any){
+            console.log("SpeechToText No Match")
+            console.log(event)
+        }
+
+        this.recognition.onaudiostart = () => {this_.model.audio_started=true}
+        this.recognition.onaudioend = () => {this_.model.audio_started=false}
+        this.recognition.onsoundstart = () => {this.model.sound_started=true}
+        this.recognition.onsoundend = () => {this_.model.sound_started=false}
+        this.recognition.onspeechstart = () => {this_.model.speech_started=true}
+        this.recognition.onspeechend = () => {this_.model.speech_started=false}
+
+        this.recognition.onstart = function(){
+            this_.buttonEl.onclick = () => {this_.recognition.end()}
+            this_.buttonEl.innerHTML = iconStarted;
+            this_.buttonEl.setAttribute("title", titleStarted);
+            this_.model.started = true;
+        }
+        this.recognition.onend = function(){
+            this_.buttonEl.onclick = () => {this_.recognition.start()}
+            this_.buttonEl.innerHTML = iconNotStarted;
+            this_.buttonEl.setAttribute("title", titleNotStarted);
+            this_.model.started = false;
+
+        }
     }
 
     connect_signals(): void {
-        console.log("connect")
-        console.log(this.model)
         super.connect_signals()
 
-        this.connect(this.model.properties.starts.change, () => {this.recognition.starts=this.model.starts;console.log("starts");})
         this.connect(this.model.properties.stops.change, () => {this.recognition.stops=this.model.stops;console.log("stops");})
         this.connect(this.model.properties.aborts.change, () => {this.recognition.aborts=this.model.aborts;console.log("aborts");})
-        // this.connect(this.model.properties.grammars.change, () => {this.recognition.grammars=this.model.grammars;console.log("grammars");})
+        this.connect(this.model.properties.grammars.change, () => {this.setGrammars})
         this.connect(this.model.properties.lang.change, () => {this.recognition.lang=this.model.lang;console.log("lang");})
         this.connect(this.model.properties.continous.change, () => {this.recognition.continous=this.model.continous;console.log("continous");})
         this.connect(this.model.properties.interim_results.change, () => {this.recognition.interim_results=this.model.interim_results;console.log("interim_results");})
         this.connect(this.model.properties.max_alternatives.change, () => {this.recognition.max_alternatives=this.model.max_alternatives;console.log("max_alternatives");})
         this.connect(this.model.properties.service_uri.change, () => {this.recognition.service_uri=this.model.service_uri;console.log("service_uri");})
+        this.connect(this.model.properties.button_type.change, () => {this.buttonEl.className=`bk bk-btn bk-btn-${this.model.button_type}`})
     }
 
     render(): void {
-        console.log("render")
-        console.log(this.model)
         super.render()
-        this.el.innerHTML="Hello Speech Recognition"
+        this.buttonEl = htmlToElement(`<button class="bk bk-btn bk-btn-${this.model.button_type}" type="button" title="${titleNotStarted}"></button>`)
+        this.buttonEl.innerHTML = iconNotStarted
+        this.buttonEl.onclick = () => {this.recognition.start()}
+        this.el.appendChild(this.buttonEl)
+    }
+
+    setGrammars(): void {
+        this.recognition.grammars=deserializeGrammars(this.model.grammars);
     }
 }
 
@@ -57,6 +144,12 @@ export namespace SpeechToText {
     interim_results: p.Property<boolean>
     max_alternatives: p.Property<number>
     service_uri: p.Property<string>
+    started: p.Property<boolean>
+    audio_started: p.Property<boolean>
+    sound_started: p.Property<boolean>
+    speech_started: p.Property<boolean>
+    button_type: p.Property<string>
+    results: p.Property<any[]>
   }
 }
 
@@ -84,7 +177,13 @@ export class SpeechToText extends HTMLBox {
         continous: [ p.Boolean,   false ],
         interim_results: [ p.Boolean,   false ],
         max_alternatives: [ p.Number,   1 ],
-        service_uri: [p.String, ]
+        service_uri: [p.String, ],
+        started: [ p.Boolean,   false ],
+        audio_started: [ p.Boolean,   false ],
+        sound_started: [ p.Boolean,   false ],
+        speech_started: [ p.Boolean,   false ],
+        button_type: [p.String, 'light'],
+        results: [ p.Array, []],
     })
   }
 }
--- panel/tests/widgets/test_speech_to_text.py
+++ panel/tests/widgets/test_speech_to_text.py
@@ -1,24 +1,14 @@
-from os import stat_result
-
 import panel as pn
 import pytest
-from panel.widgets import (SpeechGrammer, SpeechGrammerList, SpeechToText,
-                           speech_to_text)
-from panel.widgets.speech_to_text import (SpeechRecognitionAlternative,
-                                          SpeechRecognitionError,
-                                          SpeechRecognitionEvent,
-                                          SpeechRecognitionResult,
-                                          SpeechRecognitionResultList)
+from panel.widgets import SpeechGrammar, SpeechGrammarList, SpeechToText
+from panel.widgets.speech_to_text import SpeechRecognitionAlternative, SpeechRecognitionResult
 
 CLASSES = [
-    SpeechGrammer,
-    SpeechGrammerList,
+    SpeechGrammar,
+    SpeechGrammarList,
     SpeechToText,
     SpeechRecognitionAlternative,
-    SpeechRecognitionError,
-    SpeechRecognitionEvent,
     SpeechRecognitionResult,
-    SpeechRecognitionResultList,
 ]
 
 
@@ -26,20 +16,156 @@ CLASSES = [
 def test_constructor(class_):
     class_()
 
+
+def test_can_construct_speech_grammar_from_src():
+    # Given
+    src = "#JSGF V1.0; grammar colors; public <color> = aqua | azure | beige;"
+    weight = 0.7
+    # When
+    grammar = SpeechGrammar(
+        src=src,
+        weight=weight,
+    )
+    serialized = grammar.serialize()
+    # Then
+    assert serialized == {"src": src, "weight": weight}
+
+
+def test_can_construct_speech_grammar_from_uri():
+    # Given
+    uri = "http://www.example.com/grammar.txt"
+    weight = 0.7
+    # When
+    grammar = SpeechGrammar(
+        uri=uri,
+        weight=weight,
+    )
+    serialized = grammar.serialize()
+    # Then
+    assert serialized == {"uri": uri, "weight": weight}
+
+
+def test_add_from_string_to_speech_grammar_list():
+    # Given
+    src = "#JSGF V1.0; grammar colors; public <color> = aqua | azure | beige | bisque | black | blue | brown | chocolate | coral | crimson | cyan | fuchsia | ghostwhite | gold | goldenrod | gray | green | indigo | ivory | khaki | lavender | lime | linen | magenta | maroon | moccasin | navy | olive | orange | orchid | peru | pink | plum | purple | red | salmon | sienna | silver | snow | tan | teal | thistle | tomato | turquoise | violet | white | yellow ;"
+    weight = 0.5
+    grammar_list = SpeechGrammarList()
+    # When
+    result = grammar_list.add_from_string(src, weight)
+    serialized = grammar_list.serialize()
+    # Then
+    assert isinstance(result, SpeechGrammar)
+    assert result in grammar_list
+    assert serialized == [{"src": src, "weight": weight}]
+
+
+def test_add_from_uri_to_speech_grammar_list():
+    # Given
+    uri = "http://www.example.com/grammar.txt"
+    weight = 0.5
+    grammar_list = SpeechGrammarList()
+    # When
+    result = grammar_list.add_from_uri(uri, weight)
+    serialized = grammar_list.serialize()
+    # Then
+    assert isinstance(result, SpeechGrammar)
+    assert result in grammar_list
+    assert serialized == [{"uri": uri, "weight": weight}]
+
+
+def test_can_deserialize_alternative():
+    # Given
+    alternative = {"confidence": 0.9190853834152222, "transcript": "but why"}
+    # When
+    actual = SpeechRecognitionAlternative(**alternative)
+    # Then
+    assert actual.confidence == alternative["confidence"]
+    assert actual.transcript == alternative["transcript"]
+
+
+def test_can_create_result_from_dict():
+    # Given
+    result = {
+        "is_final": True,
+        "alternatives": [{"confidence": 0.9190853834152222, "transcript": "and why"}],
+    }
+    # When
+    actual = SpeechRecognitionResult.create_from_dict(result)
+    # Then
+    assert actual.is_final == result["is_final"]
+    assert len(actual.alternatives) == 1
+    assert actual.alternatives[0].confidence == 0.9190853834152222
+    assert actual.alternatives[0].transcript == "and why"
+
+
+def test_can_create_result_from_list():
+    # Given
+    results = [
+        {
+            "is_final": True,
+            "alternatives": [{"confidence": 0.9190853834152222, "transcript": "and why"}],
+        }
+    ]
+    # When
+    actual = SpeechRecognitionResult.create_from_list(results)
+    # Then
+    assert len(actual) == 1
+    assert actual[0].is_final == results[0]["is_final"]
+    assert len(actual[0].alternatives) == 1
+    assert actual[0].alternatives[0].confidence == 0.9190853834152222
+    assert actual[0].alternatives[0].transcript == "and why"
+
+
 def test_get_app():
-    speech_to_text = SpeechToText()
-    speech_to_text_settings = pn.Param(
-        speech_to_text,
-        parameters = [
-            "start", "stop", "abort", "grammars", "lang", "continous", "interim_results", "max_alternatives", "service_uri", "_starts", "_stops", "_aborts"
-        ]
+    src = "#JSGF V1.0; grammar colors; public <color> = aqua | azure | beige | bisque | black | blue | brown | chocolate | coral | crimson | cyan | fuchsia | ghostwhite | gold | goldenrod | gray | green | indigo | ivory | khaki | lavender | lime | linen | magenta | maroon | moccasin | navy | olive | orange | orchid | peru | pink | plum | purple | red | salmon | sienna | silver | snow | tan | teal | thistle | tomato | turquoise | violet | white | yellow ;"
+    speech_to_text = SpeechToText(button_type="success")
+
+    grammar_list = SpeechGrammarList()
+    grammar_list.add_from_string(src, 1)
+    speech_to_text.grammars = grammar_list
+    result_panel = pn.pane.Markdown()
+
+    @pn.depends(speech_to_text.param.results, watch=True)
+    def _update_result_panel(results=None):
+        result_panel.object = speech_to_text.results_as_html
+    _update_result_panel()
+
+    speech_to_text_settings = pn.WidgetBox(
+        pn.Param(
+            speech_to_text,
+            parameters=[
+                "stop",
+                "abort",
+                "grammars",
+                "lang",
+                "continous",
+                "interim_results",
+                "max_alternatives",
+                "service_uri",
+                "started",
+                "results",
+                "started",
+                "audio_started",
+                "sound_started",
+                "speech_started",
+                "button_type",
+            ],
+        ),
     )
     app = pn.Column(
-        pn.pane.Markdown("# Panel - Speech to Text App"),
+        pn.pane.HTML("<h1>Speech to Text <img style='float:right;height:40px;width:164px;margin-right:40px' src='https://panel.holoviz.org/_static/logo_horizontal.png'></h1>", background="black", style={"color": "white", "margin-left": "20px"}, margin=(0,0,15,0)),
         speech_to_text,
-        speech_to_text_settings,
+        pn.Row(
+            pn.Column(pn.pane.Markdown("## Settings"), speech_to_text_settings),
+            pn.layout.VSpacer(width=25),
+            pn.Column(pn.pane.Markdown("## Results"), result_panel)
+        ),
+        width=600,
+        sizing_mode="fixed",
     )
     return app
 
+
 if __name__.startswith("bokeh"):
+    pn.config.sizing_mode = "stretch_width"
     test_get_app().servable()
