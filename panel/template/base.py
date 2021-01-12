bf5e78bcc09cd8f4a43f0d7fffc271caab9cf4d0
Ajay Thorve <AjayThorve@users.noreply.github.com>
2021-01-12 11:51:00 -0800

    fix local bundled paths for template resources (#1891)
    * fix local bundled paths for template resources
    
    * replace app_url with state.base_url

--- panel/template/base.py
+++ panel/template/base.py
@@ -448,8 +448,9 @@ class BasicTemplate(BaseTemplate):
     def _template_resources(self):
         name = type(self).__name__.lower()
         resources = _settings.resources(default="server")
+        base_url = state.base_url[1:] if state.base_url.startswith('/') else state.base_url        
         if resources == 'server':
-            dist_path = urljoin(state.base_url, LOCAL_DIST)
+            dist_path = urljoin(base_url, LOCAL_DIST)
         else:
             dist_path = CDN_DIST
 
