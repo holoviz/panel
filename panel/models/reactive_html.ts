503d87da877d7f60abde9fc3c4ac3600361e1934
Philipp Rudiger <prudiger@anaconda.com>
2021-01-12 16:54:22 +0100

    Further fixes

--- panel/models/reactive_html.ts
+++ panel/models/reactive_html.ts
@@ -11,20 +11,16 @@ import {DOMEvent, htmlDecode, runScripts} from "./html"
 import {PanelHTMLBoxView, set_size} from "./layout"
 
 
-function serialize_attrs(attrs: any, model: any): any {
+function serialize_attrs(attrs: any): any {
   const serialized: any = {}
   for (const attr in attrs) {
     let value = attrs[attr]
-    const property = model.properties[attr]
-    if (!property.valid(value)) {
-      if (typeof value !== "string") {
-        console.warn(`Model property '${attr}' value of ${value} could not be serialized.`)
-        continue
-      } else if (value === "NaN" || !isNaN(Number(value)))
-        value = Number(value)
-      else if (value === 'false' || value === 'true')
-        value = value === 'true' ? true : false
-      }
+    if (typeof value !== "string")
+      value = value
+    else if (value !== "" && (value === "NaN" || !isNaN(Number(value))))
+      value = Number(value)
+    else if (value === 'false' || value === 'true')
+      value = value === 'true' ? true : false
     serialized[attr] = value
   }
   return serialized
@@ -50,36 +46,36 @@ export class ReactiveHTMLView extends PanelHTMLBoxView {
     this.connect(this.model.properties.sizing_mode.change, resize)
     this.connect(this.model.properties.html.change, resize)
     this.connect(this.model.properties.scripts.change, () => this._update(null, false, true))
-	for (const prop in this.model.data.properties) {
+    for (const prop in this.model.data.properties) {
       this.connect(this.model.data.properties[prop].change, () => {
-		if (!this._changing)
+        if (!this._changing)
           this._update(prop)
       })
-	}
+    }
   }
 
   _send_event(elname: string, attr: string, event: any) {
-	let serialized = serializeEvent(event)
-	serialized.type = attr
-	this.model.trigger_event(new DOMEvent(elname, serialized))
+    let serialized = serializeEvent(event)
+    serialized.type = attr
+    this.model.trigger_event(new DOMEvent(elname, serialized))
   }
 
   private _render_html(literal: any): string {
-	let htm = literal
-	let callbacks = ''
-	for (const elname in this.model.callbacks) {
-	  for (const callback of this.model.callbacks[elname]) {
-		const [cb, method] = callback;
-		callbacks = `
+    let htm = literal
+    let callbacks = ''
+    for (const elname in this.model.callbacks) {
+      for (const callback of this.model.callbacks[elname]) {
+        const [cb, method] = callback;
+        callbacks = `
         const ${method} = (event) => {
           view._send_event("${elname}", "${cb}", event)
         }
         `
-		htm = htm.replace('${'+method, '$--{'+method)
-	  }
-	}
+        htm = htm.replace('${'+method, '$--{'+method)
+      }
+    }
     htm = htm.replaceAll('${model.', '$-{model.').replaceAll('${', '${data.').replaceAll('$-{model.', '${model.').replaceAll('$--{', '${')
-	return new Function("view, model, data, html, useCallback", callbacks+"return html`"+htm+"`;")(this, this.model, this.model.data, html, useCallback)
+    return new Function("view, model, data, html, useCallback", callbacks+"return html`"+htm+"`;")(this, this.model, this.model.data, html, useCallback)
   }
 
   private _render_script(literal: any): string {
@@ -139,17 +135,17 @@ export class ReactiveHTMLView extends PanelHTMLBoxView {
   _update(property: string | null = null, js: boolean = true, html: boolean = true): void {
     if (html) {
       const decoded = htmlDecode(this.model.html) || this.model.html
-	  if (property == null || (decoded.indexOf(`\${${property}}`) > -1)) {
-		const rendered = this._render_html(decoded)
-		render(rendered, this.el, this._render_el)
-		this._render_el = this.el.children[0]
-		set_size(this._render_el, this.model)
-	  }
+      if (property == null || (decoded.indexOf(`\${${property}}`) > -1)) {
+        const rendered = this._render_html(decoded)
+        render(rendered, this.el, this._render_el)
+        this._render_el = this.el.children[0]
+        set_size(this._render_el, this.model)
+      }
     }
 
     if (js) {
       for (const script_obj of this.model.scripts) {
-		const name = script_obj[0] 
+        const name = script_obj[0]
         if (!(name in this._script_els)) {
           const script_el = document.createElement('div')
           this._script_els[name] = script_el
@@ -158,13 +154,12 @@ export class ReactiveHTMLView extends PanelHTMLBoxView {
       }
 
       for (const script_obj of this.model.scripts) {
-		const [name, script] = script_obj
+        const [name, script] = script_obj
         const decoded_script = htmlDecode(script) || script
         const rendered_script = this._render_script(decoded_script)
 
-		const script_el = this._script_els[name]
+        const script_el = this._script_els[name]
         if (script_el.innerHTML !== rendered_script) {
-		  console.log("Execute", name)
           script_el.innerHTML = rendered_script
           runScripts(script_el)
         }
@@ -183,12 +178,11 @@ export class ReactiveHTMLView extends PanelHTMLBoxView {
   }
 
   private _update_model(el: any, name: string): void {
-    const data_model = this.model.data
     const attrs: any = {}
     for (const attr of this.model.attrs[name])
       attrs[attr[1]] = el[attr[0]]
     this._changing = true
-    data_model.setv(serialize_attrs(attrs, data_model))
+    this.model.data.setv(serialize_attrs(attrs))
     this._changing = false
   }
 }
@@ -223,7 +217,7 @@ export class ReactiveHTML extends Markup {
     this.prototype.default_view = ReactiveHTMLView
     this.define<ReactiveHTML.Props>({
       attrs:    [ p.Any, {} ],
-	  callbacks: [ p.Any, {} ],
+      callbacks: [ p.Any, {} ],
       children: [ p.Any, {} ],
       data:    [ p.Any,  ],
       events:   [ p.Any, {}  ],
