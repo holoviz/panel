c8e852d63d99fa688ac657d6b84a7171bcff1697
Philipp Rudiger <prudiger@anaconda.com>
2021-01-13 17:37:06 +0100

    Add unit test

--- panel/tests/widgets/test_tables.py
+++ panel/tests/widgets/test_tables.py
@@ -526,6 +526,37 @@ def test_tabulator_stream_series_paginated_follow(document, comm):
         np.testing.assert_array_equal(values, expected[col])
 
 
+def test_tabulator_paginated_sorted_selection(document, comm):
+    df = makeMixedDataFrame()
+    table = Tabulator(df, pagination='remote', page_size=2)
+
+    table.sorters = [{'field': 'A', 'dir': 'dec'}]
+
+    model = table.get_root(document, comm)
+
+    table.selection = [3]
+    assert model.source.selected.indices == [1]
+
+    table.selection = [0, 1]
+    assert model.source.selected.indices == []
+
+    table.selection = [3, 4]
+    assert model.source.selected.indices == [1, 0]
+
+    table.selection = []
+    assert model.source.selected.indices == []
+
+    table._process_events({'indices': [0, 1]})
+    assert table.selection == [4, 3]
+
+    table._process_events({'indices': [1]})
+    assert table.selection == [3]
+
+    table.sorters = [{'field': 'A', 'dir': 'asc'}]
+    table._process_events({'indices': [1]})
+    assert table.selection == [1]
+
+
 def test_tabulator_stream_dataframe(document, comm):
     df = makeMixedDataFrame()
     table = Tabulator(df)
