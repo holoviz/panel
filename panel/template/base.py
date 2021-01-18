5344dce1cc5a3c539e344b3ce4808289b45a9357
Philipp Rudiger <prudiger@anaconda.com>
2021-01-18 15:41:07 +0100

    Correctly set up preprocessor on Template root (#1922)
    * Correctly set up preprocessor on Template root
    
    * Improve docstrings

--- panel/template/base.py
+++ panel/template/base.py
@@ -142,12 +142,19 @@ class BaseTemplate(param.Parameterized, ServableMixin):
             loc = self._add_location(doc, location)
             doc.on_session_destroyed(loc._server_destroy)
         doc.title = title
+
+        # Initialize fake root. This is needed to ensure preprocessors
+        # which assume that all models are owned by a single root can
+        # link objects across multiple roots in a template.
         col = Column()
         preprocess_root = col.get_root(doc, comm)
+        col._hooks.append(self._apply_hooks)
         ref = preprocess_root.ref['id']
+
         for name, (obj, tags) in self._render_items.items():
             if self._apply_hooks not in obj._hooks:
                 obj._hooks.append(self._apply_hooks)
+            # We skip preprocessing on the individual roots
             model = obj.get_root(doc, comm, preprocess=False)
             mref = model.ref['id']
             doc.on_session_destroyed(obj._server_destroy)
@@ -165,9 +172,10 @@ class BaseTemplate(param.Parameterized, ServableMixin):
             self._apply_root(name, model, tags)
             add_to_doc(model, doc, hold=bool(comm))
 
+        # Here we ensure that the preprocessor is run across all roots
+        # and set up session cleanup hooks for the fake root.
         state._fake_roots.append(ref)
         state._views[ref] = (col, preprocess_root, doc, comm)
-
         col._preprocess(preprocess_root)
         col._documents[doc] = preprocess_root
         doc.on_session_destroyed(col._server_destroy)
