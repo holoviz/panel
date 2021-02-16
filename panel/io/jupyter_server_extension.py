d428554fcbadb68e53ae710ab593223bb06c15b8
Philipp Rudiger <prudiger@anaconda.com>
2021-02-16 12:17:46 +0100

    Changed endpoint

--- panel/io/jupyter_server_extension.py
+++ panel/io/jupyter_server_extension.py
@@ -7,7 +7,7 @@ from .resources import DIST_DIR
 
 def load_jupyter_server_extension(notebook_app):
     web_app = notebook_app.web_app
-    route_pattern = urljoin(web_app.settings["base_url"], "_panel/(.*)")
+    route_pattern = urljoin(web_app.settings["base_url"], "panel_dist/(.*)")
     web_app.add_handlers(
         host_pattern=".*$",
         host_handlers=[
