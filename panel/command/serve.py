b74b568d8abfc74acfad93e3cebd91878d699464
Philipp Rudiger <prudiger@anaconda.com>
2021-02-17 13:57:00 +0100

    Fix nested modules

--- panel/command/serve.py
+++ panel/command/serve.py
@@ -165,13 +165,13 @@ class Serve(_BkServe):
         if config.autoreload:
             for f in files:
                 watch(f)
-            record_modules()
 
-        if args.warm:
+        if args.warm or args.autoreload:
             argvs = {f: args.args for f in files}
             applications = build_single_handler_applications(files, argvs)
-            for app in applications.values():
-                app.create_document()
+            with record_modules():
+                for app in applications.values():
+                    app.create_document()
 
         config.session_history = args.session_history
         if args.rest_session_info:
