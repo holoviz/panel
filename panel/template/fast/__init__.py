bdb2f1489d8eb0ed23723249fac4014dda38546a
Philipp Rudiger <prudiger@anaconda.com>
2021-02-25 16:47:32 +0100

    Move templates

--- panel/template/fast/__init__.py
+++ panel/template/fast/__init__.py
@@ -2,5 +2,6 @@
 The Fast Templates provide easy to use, great looking templates for
 Panel based on the https://fast.design framework.
 """
-from .fast_grid_template import FastGridTemplate
-from .fast_list_template import FastListTemplate
+
+from .grid import FastGridTemplate
+from .list import FastListTemplate
