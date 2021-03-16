73226c3bed27c24f6c6ea7bdaa4766df3e7d8088
MarcSkovMadsen <marc.skov.madsen@gmail.com>
2021-03-16 05:51:16 +0000

    poc

--- panel/models/terminal.ts
+++ panel/models/terminal.ts
@@ -3,19 +3,19 @@ import { HTMLBox, HTMLBoxView } from "@bokehjs/models/layouts/html_box"
 
 // See https://docs.bokeh.org/en/latest/docs/reference/core/properties.html
 import * as p from "@bokehjs/core/properties"
-import { div } from "@bokeh/bokehjs/build/js/types/core/dom"
+import { div } from "@bokehjs/core/dom";
 
 // The view of the Bokeh extension/ HTML element
 // Here you can define how to render the model as well as react to model changes or View events.
 export class TerminalView extends HTMLBoxView {
     model: Terminal
-    terminal: any // Element
+    term: any // Element
 
     connect_signals(): void {
         super.connect_signals()
 
         this.connect(this.model.properties.object.change, () => {
-            this.render();
+            this.term.write(this.model.object);
         })
     }
 
@@ -24,28 +24,9 @@ export class TerminalView extends HTMLBoxView {
         const container = div({id: "terminal-container"})
 
         const wn = (window as any)
-        wn.Terminal.applyAddon(wn.fullscreen)
-        wn.Terminal.applyAddon(wn.fit)
-        wn.Terminal.applyAddon(wn.webLinks)
-        wn.Terminal.applyAddon(wn.search)
-        const term = new wn.Terminal({
-                cursorBlink: true,
-                macOptionIsMeta: true,
-                scrollback: true,
-            });
-        term.open(container);
-        term.fit()
-        term.resize(15, 50)
-        console.log(`size: ${term.cols} columns, ${term.rows} rows`)
-        // term.toggleFullScreen(true)
-        term.fit()
-        term.write("Welcome to pyxterm.js!\nhttps://github.com/cs01/pyxterm.js\n")
-        // term.on('key', (key, ev) => {
-        //     console.log("pressed key", key)
-        //     console.log("event", ev)
-        //     socket.emit("pty-input", {"input": key})
-        // });
-
+        this.term = new wn.Terminal();
+        this.term.open(container);
+        this.term.write(this.model.object);
         this.el.appendChild(container)
     }
 }
--- panel/tests/pane/test_terminal.py
+++ panel/tests/pane/test_terminal.py
@@ -5,12 +5,13 @@ def test_constructor():
     terminal = pn.pane.Terminal(object="Click Me Now!")
 
 def get_app():
-    pn.config.js_files["xtermjs"]="https://unpkg.com/xterm@3.6.0/dist/xterm.js"
-    pn.config.js_files["xtermjs-fit"]="https://unpkg.com/xterm@3.6.0/dist/addons/fit/fit.js"
-    pn.config.js_files["xtermjs-web-links"]="https://unpkg.com/xterm@3.6.0/dist/addons/webLinks/webLinks.js"
-    pn.config.js_files["xtermjs-fullscreen"]="https://unpkg.com/xterm@3.6.0/dist/addons/fullscreen/fullscreen.js"
-    pn.config.js_files["xtermjs-search"]="https://unpkg.com/xterm@3.6.0/dist/addons/search/search.js"
-    terminal = pn.pane.Terminal(object="Click Me Now!")
+    pn.config.js_files["xtermjs"]="https://unpkg.com/xterm@4.11.0/lib/xterm.js"
+    pn.config.css_files.append("https://unpkg.com/xterm@4.11.0/css/xterm.css")
+    # pn.config.js_files["xtermjs-fit"]="https://unpkg.com/xterm@4.11.0/lib/addons/fit/fit.js"
+    # pn.config.js_files["xtermjs-web-links"]="https://unpkg.com/xterm@4.11.0/lib/addons/webLinks/webLinks.js"
+    # pn.config.js_files["xtermjs-fullscreen"]="https://unpkg.com/xterm@4.11.0/lib/addons/fullscreen/fullscreen.js"
+    # pn.config.js_files["xtermjs-search"]="https://unpkg.com/xterm@4.11.0/lib/addons/search/search.js"
+    terminal = pn.pane.Terminal(object="Welcome to the Panel Terminal!", height=400)
     return pn.Column(
         terminal, pn.Param(terminal, parameters=["object", "out"])
     )
