9e40ce52d836f5d624e71e09b8cddd9d903b7fda
ARTUSI <xavier.artusi@cea.fr>
2021-04-27 16:18:50 +0200

    export_scene correction

--- panel/pane/vtk/vtk.py
+++ panel/pane/vtk/vtk.py
@@ -296,12 +296,13 @@ class BaseVTKRenderWindow(AbstractVTK):
             filename += '.synch'
         import panel.pane.vtk.synchronizable_serializer as rws
         context = rws.SynchronizationContext(serialize_all_data_arrays=all_data_arrays, debug=self._debug_serializer)
-        scene, arrays = self._serialize_ren_win(self.object, context, binary=True, compression=False)
+        scene, arrays, annotations = self._serialize_ren_win(self.object, context, binary=True, compression=False)
 
         with zipfile.ZipFile(filename, mode='w') as zf:
             zf.writestr('index.json', json.dumps(scene))
             for name, data in arrays.items():
                 zf.writestr('data/%s' % name, data, zipfile.ZIP_DEFLATED)
+            zf.writestr('annotations.json', json.dumps(annotations))
         return filename
 
     def _update_color_mappers(self):
