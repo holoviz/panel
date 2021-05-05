e50b652c82b9ea1360a2ffa261abaf5e5c274f93
Seshu Yamajala <syamajala@gmail.com>
2021-05-05 03:39:45 -0700

    Add dimensions and prevent_collision to ReactTemplate. (#2282)
    * Add dimensions and prevent_collision to ReactTemplate.
    
    * Update ReactTemplate documentation.
    
    * Fix test_react_template.

--- panel/tests/test_template.py
+++ panel/tests/test_template.py
@@ -162,12 +162,17 @@ def test_react_template(document, comm):
     markdown2 = Markdown('# Some title')
     tmplt.main[:4, 6:] = markdown2
 
-    assert tvars['layouts'] == {
-        'lg': [{'h': 4, 'i': '1', 'w': 6, 'x': 0, 'y': 0},
-               {'h': 4, 'i': '2', 'w': 6, 'x': 6, 'y': 0}],
-        'md': [{'h': 4, 'i': '1', 'w': 6, 'x': 0, 'y': 0},
-               {'h': 4, 'i': '2', 'w': 6, 'x': 6, 'y': 0}]
-    }
+    layouts = {'lg': [{'h': 4, 'i': '1', 'w': 6, 'x': 0, 'y': 0},
+                      {'h': 4, 'i': '2', 'w': 6, 'x': 6, 'y': 0}],
+               'md': [{'h': 4, 'i': '1', 'w': 6, 'x': 0, 'y': 0},
+                      {'h': 4, 'i': '2', 'w': 6, 'x': 6, 'y': 0}]
+               }
+
+    for size in layouts:
+        for layout in layouts[size]:
+            layout.update({'minW': 0, 'minH': 0, 'maxW': 'Infinity', 'maxH': 'Infinity'})
+
+    assert tvars['layouts'] == layouts
 
 @pytest.mark.parametrize(["template_class"], [(t,) for t in LIST_TEMPLATES])
 def test_list_template_insert_order(template_class):
