d9557c85560a928dcc61a86408120e3553638d27
Marc Skov Madsen <masma@orsted.dk>
2021-05-14 04:59:57 +0200

    document sidebar_with and implement for golden

--- panel/template/golden/__init__.py
+++ panel/template/golden/__init__.py
@@ -14,6 +14,9 @@ class GoldenTemplate(BasicTemplate):
     """
     GoldenTemplate is built on top of golden-layout library.
     """
+    sidebar_width = param.Integer(20, doc="""
+        The width of the sidebar in percent. Default is 20.""")
+
     _css = pathlib.Path(__file__).parent / 'golden.css'
 
     _template = pathlib.Path(__file__).parent / 'golden.html'
