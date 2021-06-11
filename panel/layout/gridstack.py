ab92a6fca69b17918c825acd5061aadb1c0ce259
Philipp Rudiger <prudiger@anaconda.com>
2021-06-11 15:22:51 +0200

    Update sizing

--- panel/layout/gridstack.py
+++ panel/layout/gridstack.py
@@ -106,3 +106,34 @@ class GridStack(ReactiveHTML, GridSpec):
             objects[(p['y0'], p['x0'], p['y1'], p['x1'])] = obj
         self.objects.clear()
         self.objects.update(objects)
+
+    @param.depends('objects', watch=True)
+    def _update_sizing(self):
+        if self.ncols:
+            width = int(float(self.width)/self.ncols)
+        else:
+            width = 0
+
+        if self.nrows:
+            height = int(float(self.height)/self.nrows)
+        else:
+            height = 0
+
+        children = []
+        for i, ((y0, x0, y1, x1), obj) in enumerate(self.objects.items()):
+            x0 = 0 if x0 is None else x0
+            x1 = (self.ncols) if x1 is None else x1
+            y0 = 0 if y0 is None else y0
+            y1 = (self.nrows) if y1 is None else y1
+            r, c, h, w = (y0, x0, y1-y0, x1-x0)
+
+            if self.sizing_mode in ['fixed', None]:
+                properties = {'width': w*width, 'height': h*height}
+            else:
+                properties = {'sizing_mode': self.sizing_mode}
+                if 'width' in self.sizing_mode:
+                    properties['height'] = h*height
+                elif 'height' in self.sizing_mode:
+                    properties['width'] = w*width
+            obj.param.set_param(**{k: v for k, v in properties.items()
+                                   if not obj.param[k].readonly})
