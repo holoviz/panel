a5880edd3807664e1155b35c04d0a33dcfe38d71
Philipp Rudiger <prudiger@anaconda.com>
2021-06-24 13:05:53 +0200

    Remove deprecated Ace pane and Audio Widget (#2427)

--- panel/tests/pane/test_media.py
+++ panel/tests/pane/test_media.py
@@ -1,10 +1,22 @@
 import pathlib
 
+from base64 import b64encode
+from io import BytesIO
+
 import numpy as np
+import pytest
+
+try:
+    from scipy.io import wavfile
+except Exception:
+    wavfile = None
 
 from panel.pane import Audio, Video
 
 ASSETS = pathlib.Path(__file__).parent / 'assets'
+
+scipy_available = pytest.mark.skipif(wavfile is None, reason="requires scipy")
+
 
 
 def test_video_url(document, comm):
@@ -50,3 +62,27 @@ def test_numpy_audio(document, comm):
     model = audio.get_root(document, comm=comm)
 
     assert model.value == 'data:audio/wav;base64,UklGRsQAAABXQVZFZm10IBAAAAABAAEAQB8AAIA+AAACABAAZGF0YaAAAAAAAF4ErQjgDOgQuBRDGH0bXB7WIOMifCScJT8mYyYHJi0l1yMLIs0fJR0dGr4WFBMqDw4LzQZ1Ahf+vfl59VfxZu2z6UrmN+OD4DjeXNz42g7aotm22UjaWNvi3ODeTOEe5E3nzuqU7pXywvYO+2v/yAMZCFAMXhA1FMoXDxv7HYMgoCJJJHolLyZlJhwmVSURJFciKiCTHZoaSBeqE8oP' # noqa
+
+
+@scipy_available
+def test_audio_array(document, comm):
+    data = np.random.randint(-100,100, 100).astype('int16')
+    sample_rate = 10
+    buffer = BytesIO()
+    wavfile.write(buffer, sample_rate, data)
+    b64_encoded = b64encode(buffer.getvalue()).decode('utf-8')
+
+    audio = Audio(data, sample_rate=sample_rate)
+    model = audio.get_root(document, comm=comm)
+    model_value = model.value
+
+    assert model_value.split(',')[1] == b64_encoded
+    assert model.value.startswith('data:audio/wav;base64')
+
+
+def test_audio_url(document, comm):
+    audio_url = 'http://ccrma.stanford.edu/~jos/mp3/pno-cs.mp3'
+    audio = Audio(audio_url)
+    model = audio.get_root(document, comm=comm)
+
+    assert audio_url == model.value
--- panel/widgets/misc.py
+++ panel/widgets/misc.py
@@ -3,99 +3,17 @@ Miscellaneous widgets which do not fit into the other main categories.
 """
 import os
 
-from io import BytesIO
 from base64 import b64encode
-from six import string_types
 
 import param
-import numpy as np
 
 from ..io.notebook import push
 from ..io.state import state
 from ..models import (
-    Audio as _BkAudio, VideoStream as _BkVideoStream, FileDownload as _BkFileDownload
+    VideoStream as _BkVideoStream, FileDownload as _BkFileDownload
 )
 from .base import Widget
 from .indicators import Progress # noqa
-
-
-class _MediaBase(Widget):
-
-    loop = param.Boolean(default=False, doc="""
-        Whether the meida should loop""")
-
-    time = param.Number(default=0, doc="""
-        The current timestamp""")
-
-    throttle = param.Integer(default=250, doc="""
-        How frequently to sample the current playback time in
-        milliseconds""")
-
-    paused = param.Boolean(default=True, doc="""
-        Whether the media is currently paused""")
-
-    value = param.String(default='', doc="""
-        The media file either local or remote.""")
-
-    volume = param.Number(default=None, bounds=(0, 100), doc="""
-        The volume of the media player.""")
-
-    _rename = {'name': None, 'sample_rate': None}
-
-    _default_mime = None
-
-    _media_type = None
-
-    def __init__(self, **params):
-        self.param.warning('%s widget is deprecated, use the equivalent '
-                           'Pane type instead.' % type(self).__name__)
-        super().__init__(**params)
-
-    def _from_numpy(self, data):
-        from scipy.io import wavfile
-        buffer = BytesIO()
-        wavfile.write(buffer, self.sample_rate, data)
-        return buffer
-
-    def _process_param_change(self, msg):
-        msg = super()._process_param_change(msg)
-
-        if 'value' in msg:
-            value =  msg['value']
-            if isinstance(value, np.ndarray):
-                fmt = 'wav'
-                buffer = self._from_numpy(value)
-                data = b64encode(buffer.getvalue())
-            elif os.path.isfile(value):
-                fmt = value.split('.')[-1]
-                with open(value, 'rb') as f:
-                    data = f.read()
-                data = b64encode(data)
-            elif value.lower().startswith('http'):
-                return msg
-            elif not value:
-                data, fmt = b'', self._default_mime
-            else:
-                raise ValueError('Value should be either path to a sound file or numpy array')
-            template = 'data:audio/{mime};base64,{data}'
-            msg['value'] = template.format(data=data.decode('utf-8'),
-                                           mime=fmt)
-        return msg
-
-
-class Audio(_MediaBase):
-
-    sample_rate = param.Integer(default=44100, doc="""
-        The sample_rate of the audio when given a NumPy array.""")
-
-    value = param.ClassSelector(default='', class_=(string_types + (np.ndarray,)), doc="""
-        The audio file either local or remote.""")
-
-    _media_type = 'audio'
-
-    _default_mime = 'wav'
-
-    _widget_type = _BkAudio
 
 
 class VideoStream(Widget):
