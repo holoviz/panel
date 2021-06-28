45c16f2c59684e11b5e8d349f686fcf564991d36
Philipp Rudiger <prudiger@anaconda.com>
2021-06-28 11:27:47 +0200

    Remove add_periodic_callback method

--- panel/reactive.py
+++ panel/reactive.py
@@ -311,41 +311,6 @@ class Reactive(Syncable, Viewable):
     # Public API
     #----------------------------------------------------------------
 
-    def add_periodic_callback(self, callback, period=500, count=None,
-                              timeout=None, start=True):
-        """
-        Schedules a periodic callback to be run at an interval set by
-        the period. Returns a PeriodicCallback object with the option
-        to stop and start the callback.
-
-        Arguments
-        ---------
-        callback: callable
-          Callable function to be executed at periodic interval.
-        period: int
-          Interval in milliseconds at which callback will be executed.
-        count: int
-          Maximum number of times callback will be invoked.
-        timeout: int
-          Timeout in seconds when the callback should be stopped.
-        start: boolean (default=True)
-          Whether to start callback immediately.
-
-        Returns
-        -------
-        Return a PeriodicCallback object with start and stop methods.
-        """
-        self.param.warning(
-            "Calling add_periodic_callback on a Panel component is "
-            "deprecated and will be removed in the next minor release. "
-            "Use the pn.state.add_periodic_callback API instead."
-        )
-        cb = PeriodicCallback(callback=callback, period=period,
-                              count=count, timeout=timeout)
-        if start:
-            cb.start()
-        return cb
-
     def link(self, target, callbacks=None, bidirectional=False,  **links):
         """
         Links the parameters on this object to attributes on another
