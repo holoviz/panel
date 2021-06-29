1c395fdde658afdabfedc6492c68e1969808f525
Philipp Rudiger <prudiger@anaconda.com>
2021-06-29 12:47:36 +0200

    Fix Ace widget disabled parameter

--- panel/widgets/ace.py
+++ panel/widgets/ace.py
@@ -27,7 +27,7 @@ class Ace(Widget):
         Whether to show the a print margin.""")
 
     readonly = param.Boolean(default=False, doc="""
-        Define if editor content can be modified""")
+        Define if editor content can be modified. Alias for disabled.""")
 
     theme = param.ObjectSelector(default="chrome", objects=list(ace_themes),
                                  doc="Theme of the editor")
@@ -36,9 +36,25 @@ class Ace(Widget):
 
     _rename = {"value": "code", "name": None}
 
+    def __init__(self, **params):
+        if 'readonly' in params:
+            params['disabled'] = params['readonly']
+        elif 'disabled' in params:
+            params['readonly'] = params['disabled']
+        super().__init__(**params)
+        self.param.watch(self._update_disabled, ['disabled', 'readonly'])
+        self.jslink(self, readonly='disabled', bidirectional=True)
+
     def _get_model(self, doc, root=None, parent=None, comm=None):
         if self._widget_type is None:
             self._widget_type = lazy_load(
                 'panel.models.ace', 'AcePlot', isinstance(comm, JupyterComm)
             )
         return super()._get_model(doc, root, parent, comm)
+
+    def _update_disabled(self, *events):
+        for event in events:
+            if event.name == 'disabled':
+                self.readonly = event.new
+            elif event.name == 'readonly':
+                self.disabled = event.new
