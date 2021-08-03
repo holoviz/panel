a62b277295d010b93881d97bdb9f929860cd2be3
Philipp Rudiger <prudiger@anaconda.com>
2021-08-03 12:56:32 +0200

    Fix cleanup of callbacks and modules

--- panel/command/serve.py
+++ panel/command/serve.py
@@ -168,15 +168,24 @@ class Serve(_BkServe):
         if args.warm or args.autoreload:
             argvs = {f: args.args for f in files}
             applications = build_single_handler_applications(files, argvs)
+            docs = []
             if args.autoreload:
                 with record_modules():
                     for app in applications.values():
                         doc = app.create_document()
-                        doc.destroy()
+                        docs.append(doc)
             else:
                 for app in applications.values():
                     doc = app.create_document()
-                    doc.destroy()
+                    docs.append(doc)
+            for doc in docs:
+                for callback in doc.session_destroyed_callbacks:
+                    try:
+                        callback(None)
+                    except Exception:
+                        pass
+                doc._callbacks[None] = {}
+                doc.destroy(None)
 
         config.session_history = args.session_history
         if args.rest_session_info:
