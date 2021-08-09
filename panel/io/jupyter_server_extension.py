aa87e0463fa3dd74ea2a815858d66b20d7a501aa
Philipp Rudiger <prudiger@anaconda.com>
2021-08-09 17:22:17 +0200

    Attempt to fix jupyter server extension url prefix

--- panel/io/jupyter_server_extension.py
+++ panel/io/jupyter_server_extension.py
@@ -26,9 +26,22 @@ from .resources import DIST_DIR, Resources
 
 _RESOURCES = None
 
-_APPS = {
+_APPS = {}
 
-}
+
+def url_path_join(*pieces):
+    """Join components of url into a relative url
+    Use to prevent double slash when joining subpath. This will leave the
+    initial and final / in place
+    """
+    initial = pieces[0].startswith('/')
+    final = pieces[-1].endswith('/')
+    stripped = [s.strip('/') for s in pieces]
+    result = '/'.join(s for s in stripped if s)
+    if initial: result = '/' + result
+    if final: result = result + '/'
+    if result == '//': result = '/'
+    return result
 
 
 class ServerApplicationProxy:
@@ -148,7 +161,7 @@ class PanelWSHandler(WSHandler):
 def _load_jupyter_server_extension(notebook_app):
     global RESOURCES
 
-    base_url = notebook_app.web_app.settings["base_url"]
+    base_url = url_path_join(notebook_app.web_app.settings["base_url"])
 
     # Configure Panel
     RESOURCES = Resources(
