04a92a64d3c3e60ecf2349dfc3cf9621bca12451
Tyler Houssian <tylerhoussian@gmail.com>
2021-11-03 12:39:54 -0600

    Update the FastApi tutorial (#2877)
    * Added FastApi in the user guide for embedding apps
    
    * Changed the tutorial and apps to use pn.serve() instead of a thread. Also got rid of the middleware as it is not needed.

--- examples/apps/fastApi_multi_apps/main.py
+++ examples/apps/fastApi_multi_apps/main.py
@@ -1,25 +1,13 @@
+import panel as pn
 from bokeh.embed import server_document
-from panel.io.server import Server
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from starlette.middleware.cors import CORSMiddleware
-from starlette.middleware import Middleware
-from threading import Thread
-from tornado.ioloop import IOLoop
 
 from sliders.pn_app import createApp
 from sliders2.pn_app import createApp2
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-
-app.add_middleware( # Middleware to serve panel apps asynchronously via starlette
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get("/")
@@ -32,14 +20,6 @@ async def bkapp_page2(request: Request):
     script = server_document('http://127.0.0.1:5000/app2')
     return templates.TemplateResponse("app2.html", {"request": request, "script": script})
 
-def bk_worker():
-    server = Server({'/app': createApp, '/app2': createApp2},
-        port=5000, io_loop=IOLoop(), 
-        allow_websocket_origin=["*"])
-
-    server.start()
-    server.io_loop.start()
-
-th = Thread(target=bk_worker)
-th.daemon = True
-th.start()
+pn.serve({'/app': createApp, '/app2': createApp2},
+        port=5000, allow_websocket_origin=["127.0.0.1:8000"],
+         address="127.0.0.1", show=False)
--- examples/apps/fastApi_multi_apps/sliders/pn_app.py
+++ examples/apps/fastApi_multi_apps/sliders/pn_app.py
@@ -2,7 +2,6 @@ import panel as pn
 
 from .sinewave import SineWave
 
-def createApp(doc):
+def createApp():
     sw = SineWave()
-    row = pn.Row(sw.param, sw.plot)
-    row.server_doc(doc)
+    return pn.Row(sw.param, sw.plot).servable()
