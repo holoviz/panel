29bfc82c14645ad662f433c7b33ca20e4558f7ec
Jean-Luc Stevens <jlrstevens@gmail.com>
2021-11-04 05:43:58 -0500

    Fixed UTF-8 decoding in Terminal widget (#2880)

--- panel/widgets/terminal.py
+++ panel/widgets/terminal.py
@@ -163,13 +163,25 @@ class TerminalSubprocess(param.Parameterized):
     def _remove_last_line_from_string(value):
         return value[: value.rfind("CompletedProcess")]
 
+    def _decode_utf8_on_boundary(self, fd, max_read_bytes, max_extra_bytes=2):
+        "UTF-8 characters can be multi-byte so need to decode on correct boundary"
+        data = os.read(fd, max_read_bytes)
+        for _ in range(max_extra_bytes+1):
+            try:
+                return data.decode('utf-8')
+            except UnicodeDecodeError:
+                data = data + os.read(fd, 1)
+        raise UnicodeError('Could not find decode boundary for UTF-8')
+
     def _forward_subprocess_output_to_terminal(self):
         if not self._fd:
             return
         (data_ready, _, _) = select.select([self._fd], [], [], self._timeout_sec)
         if not data_ready:
             return
-        output = os.read(self._fd, self._max_read_bytes).decode()
+
+        output = self._decode_utf8_on_boundary(self._fd, self._max_read_bytes)
+
         # If Child Process finished it will signal this by appending "CompletedProcess(...)"
         if "CompletedProcess" in output:
             self._reset()
