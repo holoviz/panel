353c3b6a63844075826da6d16dfcbec63dc8ffe4
Philipp Rudiger <prudiger@anaconda.com>
2021-12-03 17:21:15 +0100

    Improve handling of ReactiveHTML cleanup (#2974)

--- panel/tests/test_reactive.py
+++ panel/tests/test_reactive.py
@@ -291,12 +291,14 @@ def test_reactive_html_children():
     test = TestChildren(children=[widget])
     root = test.get_root()
     assert root.children == {'div': [widget._models[root.ref['id']][0]]}
+    assert len(widget._models) == 1
+    assert test._panes == {'children': [widget]}
 
     widget_new = TextInput()
     test.children = [widget_new]
     assert len(widget._models) == 0
     assert root.children == {'div': [widget_new._models[root.ref['id']][0]]}
-
+    assert test._panes == {'children': [widget_new]}
 
 
 def test_reactive_html_templated_children():
@@ -323,12 +325,52 @@ def test_reactive_html_templated_children():
     root = test.get_root()
     assert root.looped == ['option']
     assert root.children == {'option': [widget._models[root.ref['id']][0]]}
+    assert test._panes == {'children': [widget]}
 
     widget_new = TextInput()
     test.children = [widget_new]
     assert len(widget._models) == 0
     assert root.children == {'option': [widget_new._models[root.ref['id']][0]]}
+    assert test._panes == {'children': [widget_new]}
 
+
+def test_reactive_html_templated_dict_children():
+
+    class TestTemplatedChildren(ReactiveHTML):
+
+        children = param.Dict(default={})
+
+        _template = """
+        <select id="select">
+        {% for key, option in children.items() %}
+        <option id="option-{{ loop.index0 }}">${children[{{ key }}]}</option>
+        {% endfor %}
+        </div>
+        """
+
+    assert TestTemplatedChildren._attrs == {}
+    assert TestTemplatedChildren._node_callbacks == {}
+    assert TestTemplatedChildren._inline_callbacks == []
+    assert TestTemplatedChildren._parser.children == {'option': 'children'}
+
+    widget = TextInput()
+    test = TestTemplatedChildren(children={'test': widget})
+    root = test.get_root()
+    assert root.looped == ['option']
+    assert root.children == {'option': [widget._models[root.ref['id']][0]]}
+    assert test._panes == {'children': [widget]}
+    widget_model = widget._models[root.ref['id']][0]
+
+    widget_new = TextInput()
+    test.children = {'test': widget_new, 'test2': widget}
+    assert len(widget._models) == 1
+    assert root.children == {
+        'option': [
+            widget_new._models[root.ref['id']][0],
+            widget_model
+        ]
+    }
+    assert test._panes == {'children': [widget_new, widget]}
 
 
 def test_reactive_html_templated_children_add_loop_id():
@@ -366,7 +408,6 @@ def test_reactive_html_templated_children_add_loop_id():
 
     model = test.get_root()
     assert model.looped == ['option']
-
 
 
 def test_reactive_html_templated_children_add_loop_id_and_for_loop_var():
