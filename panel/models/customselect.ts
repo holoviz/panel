0cc5c1ff5a06986e719badc005c8a7f0bcedf184
maximlt <mliquet@anaconda.com>
2022-01-11 18:25:17 +0100

    fix typescript indentation from 4 to 2

--- panel/models/customselect.ts
+++ panel/models/customselect.ts
@@ -3,61 +3,61 @@ import * as p from "@bokehjs/core/properties"
 
 
 export class CustomSelectView extends SelectView {
-    override model: CustomSelect
+  override model: CustomSelect
 
-    connect_signals(): void {
-        super.connect_signals()
-        this.connect(this.model.properties.disabled_options.change, () => this._update_disabled_options())
+  connect_signals(): void {
+    super.connect_signals()
+    this.connect(this.model.properties.disabled_options.change, () => this._update_disabled_options())
+  }
+
+  protected options_el(): HTMLOptionElement[] | HTMLOptGroupElement[] {
+    let opts = super.options_el()
+    opts.forEach((element) => {
+      if (this.model.disabled_options.includes(element.value)) {
+        element.setAttribute('disabled', 'true')
       }
+  })
+    return opts
+}
 
-    protected options_el(): HTMLOptionElement[] | HTMLOptGroupElement[] {
-        let opts = super.options_el()
-        opts.forEach((element) => {
-            if (this.model.disabled_options.includes(element.value)) {
-                element.setAttribute('disabled', 'true')
-            }
-        })
-        return opts
+  protected _update_disabled_options(): void {
+    for (const element of this.input_el.options) {
+      if (this.model.disabled_options.includes(element.value)) {
+        element.setAttribute('disabled', 'true')
+      } else {
+        element.removeAttribute('disabled')
+      }
     }
-
-    protected _update_disabled_options(): void {
-        for (const element of this.input_el.options) {
-            if (this.model.disabled_options.includes(element.value)) {
-                element.setAttribute('disabled', 'true')
-            } else {
-                element.removeAttribute('disabled')
-            }
-        }
-    }
+  }
 }
 
 export namespace CustomSelect {
-    export type Attrs = p.AttrsOf<Props>
+  export type Attrs = p.AttrsOf<Props>
 
-    export type Props = Select.Props & {
-        disabled_options: p.Property<string[]>
-    }
+  export type Props = Select.Props & {
+    disabled_options: p.Property<string[]>
+  }
 }
 
 export interface CustomSelect extends CustomSelect.Attrs {}
 
 export class CustomSelect extends Select {
-    override properties: CustomSelect.Props
-    override __view_type__: CustomSelectView
+  override properties: CustomSelect.Props
+  override __view_type__: CustomSelectView
 
-    constructor(attrs?: Partial<CustomSelect.Attrs>) {
-        super(attrs)
-    }
+  constructor(attrs?: Partial<CustomSelect.Attrs>) {
+    super(attrs)
+  }
 
-    static __module__ = "panel.models.widgets"
+  static __module__ = "panel.models.widgets"
 
-    static {
-        this.prototype.default_view = CustomSelectView
+  static {
+    this.prototype.default_view = CustomSelectView
 
-        this.define<CustomSelect.Props>(({Array, String}) => {
-        return {
-            disabled_options:   [ Array(String), [] ],
-        }
-        })
-    }
+    this.define<CustomSelect.Props>(({Array, String}) => {
+      return {
+        disabled_options:   [ Array(String), [] ],
+      }
+    })
+  }
 }
