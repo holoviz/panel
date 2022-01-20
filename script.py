42b513cdfc5bf2d3c327c8f8830d0514f6b2548d
Marc Skov Madsen <masma@orsted.dk>
2022-01-20 05:27:14 +0100

    remove script

--- script.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from panel.widgets import Tabulator
-
-value = pd.DataFrame({"x": [1, 2], "y": ["a", "b"]})
-tabulator = Tabulator(value=value)
-patch_value = pd.Series({"index": 1, "x": 4, "y": "d"})
-tabulator.patch(patch_value)
-print(tabulator.value.to_dict("list"))
