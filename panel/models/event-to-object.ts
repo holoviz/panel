eefb6ea9fa84712e24433e4a1f2bd2c93db2ee59
Philipp Rudiger <prudiger@anaconda.com>
2022-01-28 18:36:06 +0100

    Update event-to-object

--- panel/models/event-to-object.ts
+++ panel/models/event-to-object.ts
@@ -1,35 +1,107 @@
-export function serializeEvent(event: any) {
-  const data: any = {type: event.type};
-  if ("value" in event.target) {
-    data.value = event.target.value;
-  }
+export function serializeEvent(event) {
+  const data = {};
+
   if (event.type in eventTransforms) {
     Object.assign(data, eventTransforms[event.type](event));
   }
+
+  data.target = serializeDomElement(event.target);
+  data.currentTarget =
+    event.target === event.currentTarget
+      ? data.target
+      : serializeDomElement(event.currentTarget);
+  data.relatedTarget = serializeDomElement(event.relatedTarget);
+
   return data;
 }
 
-const eventCategoryTransforms: any = {
-  clipboard: (event: any) => ({
+function serializeDomElement(element) {
+  let elementData = null;
+  if (element) {
+    elementData = defaultElementTransform(element);
+    if (element.tagName in elementTransforms) {
+      elementTransforms[element.tagName].forEach((trans) =>
+        Object.assign(elementData, trans(element))
+      );
+    }
+  }
+  return elementData;
+}
+
+const elementTransformCategories = {
+  hasValue: (element) => ({
+    value: element.value,
+  }),
+  hasCurrentTime: (element) => ({
+    currentTime: element.currentTime,
+  }),
+  hasFiles: (element) => {
+    if (element?.type === "file") {
+      return {
+        files: Array.from(element.files).map((file) => ({
+          lastModified: file.lastModified,
+          name: file.name,
+          size: file.size,
+          type: file.type,
+        })),
+      };
+    } else {
+      return {};
+    }
+  },
+};
+
+function defaultElementTransform(element) {
+  return { boundingClientRect: {...element.getBoundingClientRect()} };
+}
+
+const elementTagCategories = {
+  hasValue: [
+    "BUTTON",
+    "INPUT",
+    "OPTION",
+    "LI",
+    "METER",
+    "PROGRESS",
+    "PARAM",
+    "SELECT",
+    "TEXTAREA",
+  ],
+  hasCurrentTime: ["AUDIO", "VIDEO"],
+  hasFiles: ["INPUT"],
+};
+
+const elementTransforms = {};
+
+Object.keys(elementTagCategories).forEach((category) => {
+  elementTagCategories[category].forEach((type) => {
+    const transforms =
+      elementTransforms[type] || (elementTransforms[type] = []);
+    transforms.push(elementTransformCategories[category]);
+  });
+});
+
+function EventTransformCategories() {
+  this.clipboard = (event) => ({
     clipboardData: event.clipboardData,
-  }),
-  composition: (event: any) => ({
+  });
+  this.composition = (event) => ({
     data: event.data,
-  }),
-  keyboard: (event: any) => ({
+  });
+  this.keyboard = (event) => ({
     altKey: event.altKey,
     charCode: event.charCode,
     ctrlKey: event.ctrlKey,
     key: event.key,
     keyCode: event.keyCode,
-    locale: event.locale || null,
+    locale: event.locale,
     location: event.location,
     metaKey: event.metaKey,
     repeat: event.repeat,
     shiftKey: event.shiftKey,
     which: event.which,
-  }),
-  mouse: (event: any) => ({
+  });
+  this.mouse = (event) => ({
     altKey: event.altKey,
     button: event.button,
     buttons: event.buttons,
@@ -42,8 +114,9 @@ const eventCategoryTransforms: any = {
     screenX: event.screenX,
     screenY: event.screenY,
     shiftKey: event.shiftKey,
-  }),
-  pointer: (event: any) => ({
+  });
+  this.pointer = (event) => ({
+    ...this.mouse(event),
     pointerId: event.pointerId,
     width: event.width,
     height: event.height,
@@ -52,35 +125,38 @@ const eventCategoryTransforms: any = {
     tiltY: event.tiltY,
     pointerType: event.pointerType,
     isPrimary: event.isPrimary,
-  }),
-  touch: (event: any) => ({
+  });
+  this.selection = () => {
+    return { selectedText: window.getSelection().toString() };
+  };
+  this.touch = (event) => ({
     altKey: event.altKey,
     ctrlKey: event.ctrlKey,
     metaKey: event.metaKey,
     shiftKey: event.shiftKey,
-  }),
-  ui: (event: any) => ({
+  });
+  this.ui = (event) => ({
     detail: event.detail,
-  }),
-  wheel: (event: any) => ({
+  });
+  this.wheel = (event) => ({
     deltaMode: event.deltaMode,
     deltaX: event.deltaX,
     deltaY: event.deltaY,
     deltaZ: event.deltaZ,
-  }),
-  animation: (event: any) => ({
+  });
+  this.animation = (event) => ({
     animationName: event.animationName,
     pseudoElement: event.pseudoElement,
     elapsedTime: event.elapsedTime,
-  }),
-  transition: (event: any) => ({
+  });
+  this.transition = (event) => ({
     propertyName: event.propertyName,
     pseudoElement: event.pseudoElement,
     elapsedTime: event.elapsedTime,
-  }),
-};
+  });
+}
 
-const eventTypeCategories: any = {
+const eventTypeCategories = {
   clipboard: ["copy", "cut", "paste"],
   composition: ["compositionend", "compositionstart", "compositionupdate"],
   keyboard: ["keydown", "keypress", "keyup"],
@@ -124,12 +200,11 @@ const eventTypeCategories: any = {
   transition: ["transitionend"],
 };
 
-const eventTransforms: any = {};
+const eventTransforms = {};
 
-Object.keys(eventTypeCategories).forEach((category: string) => {
-  eventTypeCategories[category].forEach((type: string) => {
-    eventTransforms[type] = eventCategoryTransforms[category];
+const eventTransformCategories = new EventTransformCategories();
+Object.keys(eventTypeCategories).forEach((category) => {
+  eventTypeCategories[category].forEach((type) => {
+    eventTransforms[type] = eventTransformCategories[category];
   });
 });
-
-export default serializeEvent;
