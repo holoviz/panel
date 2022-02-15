7fee213fb09a8c38ad27777064092d0983e26b5f
Philipp Rudiger <prudiger@anaconda.com>
2022-02-15 11:02:58 +0100

    Increase tolerance

--- panel/tests/test_server.py
+++ panel/tests/test_server.py
@@ -369,8 +369,8 @@ def test_server_schedule_at_callable():
 
     # Check callbacks were executed within small margin of error
     assert len(state.cache['at']) == 2
-    assert abs(state.cache['at'][0] - scheduled[0]) < dt.timedelta(seconds=0.02)
-    assert abs(state.cache['at'][1] - scheduled[1]) < dt.timedelta(seconds=0.02)
+    assert abs(state.cache['at'][0] - scheduled[0]) < dt.timedelta(seconds=0.2)
+    assert abs(state.cache['at'][1] - scheduled[1]) < dt.timedelta(seconds=0.2)
     assert len(state._scheduled) == 0
 
     server.stop()
