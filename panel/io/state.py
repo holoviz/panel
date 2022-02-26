f5cd6200ee4d85597f47caba6c00c5405794e9e9
Simon Hoxbro <simon.hansen@me.com>
2022-02-26 17:34:31 +0100

    Check if base url have any search parameters

--- panel/io/state.py
+++ panel/io/state.py
@@ -488,12 +488,18 @@ class _state(param.Parameterized):
     def location(self):
         if self.curdoc and self.curdoc not in self._locations:
             from .location import Location
-            self._locations[self.curdoc] = loc = Location()
-            return loc
+            loc = self._locations[self.curdoc] = Location()
         elif self.curdoc is None:
-            return self._location
+            loc = self._location
         else:
-            return self._locations.get(self.curdoc) if self.curdoc else None
+            loc = self._locations.get(self.curdoc) if self.curdoc else None
+
+        try:
+            loc.search = self.base_url.replace("/", "")
+        except Exception:
+            pass
+
+        return loc
 
     @property
     def notifications(self):
