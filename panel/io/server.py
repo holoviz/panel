f109ca85efe50450218d391837ea047ea5c84e6b
Philipp Rudiger <prudiger@anaconda.com>
2022-03-31 10:35:37 +0200

    Do not override ComponentResourceHandler.get

--- panel/io/server.py
+++ panel/io/server.py
@@ -309,7 +309,7 @@ class ComponentResourceHandler(StaticFileHandler):
         self.root = path
         self.default_filename = default_filename
 
-    def parse_url_pattern(self, path):
+    def parse_url_path(self, path):
         """
         Resolves the resource the URL pattern refers to.
         """
@@ -321,7 +321,6 @@ class ComponentResourceHandler(StaticFileHandler):
             module = importlib.import_module(module)
         except Exception:
             raise HTTPError(404, 'Module not found')
-        print(module)
         try:
             component = getattr(module, cls)
         except AttributeError:
@@ -357,87 +356,8 @@ class ComponentResourceHandler(StaticFileHandler):
             path = pathlib.Path(module.__file__).parent / path
         return path
 
-    async def get(self, path: str, include_body: bool = True) -> None:
-        # Set up our path instance variables.
-        self.path = self.parse_url_pattern(path)
-        del path  # make sure we don't refer to path instead of self.path again
-        self.absolute_path = self.validate_absolute_path(self.root, self.path)
-        if self.absolute_path is None:
-            return
-
-        self.modified = self.get_modified_time()
-        self.set_headers()
-
-        if self.should_return_304():
-            self.set_status(304)
-            return
-
-        request_range = None
-        range_header = self.request.headers.get("Range")
-        if range_header:
-            # As per RFC 2616 14.16, if an invalid Range header is specified,
-            # the request will be treated as if the header didn't exist.
-            request_range = httputil._parse_request_range(range_header)
-
-        size = self.get_content_size()
-        if request_range:
-            start, end = request_range
-            if start is not None and start < 0:
-                start += size
-                if start < 0:
-                    start = 0
-            if (
-                start is not None
-                and (start >= size or (end is not None and start >= end))
-            ) or end == 0:
-                # As per RFC 2616 14.35.1, a range is not satisfiable only: if
-                # the first requested byte is equal to or greater than the
-                # content, or when a suffix with length 0 is specified.
-                # https://tools.ietf.org/html/rfc7233#section-2.1
-                # A byte-range-spec is invalid if the last-byte-pos value is present
-                # and less than the first-byte-pos.
-                self.set_status(416)  # Range Not Satisfiable
-                self.set_header("Content-Type", "text/plain")
-                self.set_header("Content-Range", "bytes */%s" % (size,))
-                return
-            if end is not None and end > size:
-                # Clients sometimes blindly use a large range to limit their
-                # download size; cap the endpoint at the actual file size.
-                end = size
-            # Note: only return HTTP 206 if less than the entire range has been
-            # requested. Not only is this semantically correct, but Chrome
-            # refuses to play audio if it gets an HTTP 206 in response to
-            # ``Range: bytes=0-``.
-            if size != (end or size) - (start or 0):
-                self.set_status(206)  # Partial Content
-                self.set_header(
-                    "Content-Range", httputil._get_content_range(start, end, size)
-                )
-        else:
-            start = end = None
-
-        if start is not None and end is not None:
-            content_length = end - start
-        elif end is not None:
-            content_length = end
-        elif start is not None:
-            content_length = size - start
-        else:
-            content_length = size
-        self.set_header("Content-Length", content_length)
-
-        if include_body:
-            content = self.get_content(self.absolute_path, start, end)
-            if isinstance(content, bytes):
-                content = [content]
-            for chunk in content:
-                try:
-                    self.write(chunk)
-                    await self.flush()
-                except iostream.StreamClosedError:
-                    return
-        else:
-            assert self.request.method == "HEAD"
+    def get_absolute_path(self, root, path):
+        return path
 
     def validate_absolute_path(self, root, absolute_path):
         if not os.path.exists(absolute_path):
