03cceeaac9d1fe65cd954d59bd285405a8660f65
Marc Skov Madsen <masma@orsted.dk>
2022-04-16 21:29:20 +0200

    prepare for static type checing using redundant form

--- panel/widgets/__init__.py
+++ panel/widgets/__init__.py
@@ -32,56 +32,86 @@ How to use Panel widgets in 4 simple steps
 For more detail see the Getting Started Guide
 https://panel.holoviz.org/getting_started/index.html
 """
-from .ace import Ace  # noqa
-from .base import Widget, CompositeWidget  # noqa
-from .button import Button, MenuButton, Toggle  # noqa
-from .file_selector import FileSelector  # noqa
-from .indicators import ( # noqa
-    BooleanStatus,
-    Dial,
-    Gauge,
-    LinearGauge,
-    LoadingSpinner,
-    Number,
-    Progress,
-    Trend,
-    Tqdm,
+from .ace import Ace as Ace  # noqa
+from .base import Widget as Widget, CompositeWidget as CompositeWidget  # noqa
+from .button import ( # noqa
+    Button as Button,
+    MenuButton as MenuButton,
+    Toggle as Toggle,
+)  
+from .file_selector import FileSelector as FileSelector  # noqa
+from .indicators import (  # noqa
+    BooleanStatus as BooleanStatus,
+    Dial as Dial,
+    Gauge as Gauge,
+    LinearGauge as LinearGauge,
+    LoadingSpinner as LoadingSpinner,
+    Number as Number,
+    Progress as Progress,
+    Trend as Trend,
+    Tqdm as Tqdm,
 )
 from .input import (  # noqa
-    ArrayInput,
-    ColorPicker,
-    Checkbox,
-    DatetimeInput,
-    DatePicker,
-    DatetimePicker,
-    DatetimeRangeInput,
-    DatetimeRangePicker,
-    FileInput,
-    LiteralInput,
-    StaticText,
-    TextInput,
-    IntInput,
-    FloatInput,
-    NumberInput,
-    Spinner,
-    PasswordInput,
-    TextAreaInput,
+    ArrayInput as ArrayInput,
+    ColorPicker as ColorPicker,
+    Checkbox as Checkbox,
+    DatetimeInput as DatetimeInput,
+    DatePicker as DatePicker,
+    DatetimePicker as DatetimePicker,
+    DatetimeRangeInput as DatetimeRangeInput,
+    DatetimeRangePicker as DatetimeRangePicker,
+    FileInput as FileInput,
+    LiteralInput as LiteralInput,
+    StaticText as StaticText,
+    TextInput as TextInput,
+    IntInput as IntInput,
+    FloatInput as FloatInput,
+    NumberInput as NumberInput,
+    Spinner as Spinner,
+    PasswordInput as PasswordInput,
+    TextAreaInput as TextAreaInput,
 )
-from .misc import FileDownload, JSONEditor, VideoStream # noqa
-from .player import DiscretePlayer, Player # noqa
-from .slider import ( # noqa
-    DateSlider, DateRangeSlider, DiscreteSlider, EditableRangeSlider,
-    EditableFloatSlider, EditableIntSlider, FloatSlider, IntSlider,
-    IntRangeSlider, RangeSlider
+from .misc import ( # noqa
+    FileDownload as FileDownload,
+    JSONEditor as JSONEditor,
+    VideoStream as VideoStream,
+)  
+from .player import DiscretePlayer as DiscretePlayer, Player as Player  # noqa
+from .slider import (  # noqa
+    DateSlider as DateSlider,
+    DateRangeSlider as DateRangeSlider,
+    DiscreteSlider as DiscreteSlider,
+    EditableRangeSlider as EditableRangeSlider,
+    EditableFloatSlider as EditableFloatSlider,
+    EditableIntSlider as EditableIntSlider,
+    FloatSlider as FloatSlider,
+    IntSlider as IntSlider,
+    IntRangeSlider as IntRangeSlider,
+    RangeSlider as RangeSlider,
 )
-from .select import ( # noqa
-    AutocompleteInput, CheckBoxGroup, CheckButtonGroup, CrossSelector,
-    MultiChoice, MultiSelect, RadioButtonGroup, RadioBoxGroup, Select,
-    ToggleGroup
+from .select import (  # noqa
+    AutocompleteInput as AutocompleteInput,
+    CheckBoxGroup as CheckBoxGroup,
+    CheckButtonGroup as CheckButtonGroup,
+    CrossSelector as CrossSelector,
+    MultiChoice as MultiChoice,
+    MultiSelect as MultiSelect,
+    RadioButtonGroup as RadioButtonGroup,
+    RadioBoxGroup as RadioBoxGroup,
+    Select as Select,
+    ToggleGroup as ToggleGroup,
 )
-from .speech_to_text import SpeechToText, Grammar, GrammarList # noqa
-from .tables import DataFrame, Tabulator  # noqa
-from .terminal import Terminal # noqa
-from .debugger import Debugger # noqa
-from .text_to_speech import TextToSpeech, Utterance, Voice # noqa
-from .texteditor import TextEditor # noqa
+from .speech_to_text import ( # noqa
+    SpeechToText as SpeechToText,
+    Grammar as Grammar,
+    GrammarList as GrammarList,
+)  
+from .tables import DataFrame as DataFrame, Tabulator as Tabulator  # noqa
+from .terminal import Terminal as Terminal  # noqa
+from .debugger import Debugger as Debugger  # noqa
+from .text_to_speech import ( # noqa
+    TextToSpeech as TextToSpeech,
+    Utterance as Utterance,
+    Voice as Voice,
+)  
+from .texteditor import TextEditor as TextEditor  # noqa
