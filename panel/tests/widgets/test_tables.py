9b83c5ae13daa135f219883e1ee360f437b1586d
Philipp Rudiger <prudiger@anaconda.com>
2022-04-28 18:07:15 +0200

    Add test

--- panel/tests/widgets/test_tables.py
+++ panel/tests/widgets/test_tables.py
@@ -1734,3 +1734,19 @@ def test_tabulator_cell_click_event():
             event = CellClickEvent(model=None, column=col, row=row)
             table._process_event(event)
             assert values[-1] == (col, row, data[col].iloc[row])
+
+def test_tabulator_pagination_remote_cell_click_event():
+    df = makeMixedDataFrame()
+    table = Tabulator(df, pagination='remote', page_size=2)
+
+    values = []
+    table.on_click(lambda e: values.append((e.column, e.row, e.value)))
+
+    data = df.reset_index()
+    for col in data.columns:
+        for p in range(len(df)//2):
+            table.page = p+1
+            for row in range(2):
+                event = CellClickEvent(model=None, column=col, row=row)
+                table._process_event(event)
+                assert values[-1] == (col, (p*2)+row, data[col].iloc[(p*2)+row])
