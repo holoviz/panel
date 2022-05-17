f74e548bb9687c5c7ec1cd0dd57b817f64ea26a9
Philipp Rudiger <prudiger@anaconda.com>
2022-05-17 17:13:38 +0200

    Add test

--- panel/tests/layout/test_tabs.py
+++ panel/tests/layout/test_tabs.py
@@ -677,3 +677,15 @@ def test_tabs_clear(document, comm):
     assert tabs._names == []
     assert len(model.tabs) == 0
     assert p1._models == p2._models == {}
+
+
+def test_tabs_pane_update(document, comm):
+    div1 = Div()
+    div2 = Div()
+    tabs = Tabs(div1, div2)
+
+    model = tabs.get_root(document, comm=comm)
+
+    new_div = Div()
+    tabs[1].object = new_div
+    assert model.tabs[1].child is new_div
