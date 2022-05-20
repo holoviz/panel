6ba34e151316c0273cebcf1e23e537f68ee9e260
Philipp Rudiger <prudiger@anaconda.com>
2022-05-20 11:32:31 +0200

    Do not patch event loop on jupyter_server on windows

--- panel/io/server.py
+++ panel/io/server.py
@@ -521,18 +521,14 @@ def create_static_handler(prefix, key, app):
 bokeh.server.tornado.create_static_handler = create_static_handler
 
 # Bokeh 2.4.x patches the asyncio event loop policy but Tornado 6.1
-# support the WindowsProactorEventLoopPolicy so we restore it.
-try:
-    JUPYTER_CONTEXT = get_ipython() is not None # noqa
-except Exception:
-    JUPYTER_CONTEXT = False
-
+# support the WindowsProactorEventLoopPolicy so we restore it,
+# unless we detect we are running on jupyter_server.
 if (
     sys.platform == 'win32' and
     sys.version_info[:3] >= (3, 8, 0) and
     tornado.version_info >= (6, 1) and
     type(asyncio.get_event_loop_policy()) is asyncio.WindowsSelectorEventLoopPolicy and
-    not JUPYTER_CONTEXT
+    'jupyter_server' not in sys.modules
 ):
     asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
