b1f9968bfc490c5be82008d42ef45ab833b80329
Marc Skov Madsen <masma@orsted.dk>
2022-05-29 20:35:36 +0200

    improve fast docstrings

--- panel/template/fast/list/__init__.py
+++ panel/template/fast/list/__init__.py
@@ -13,7 +13,34 @@ from ..theme import FastDarkTheme, FastDefaultTheme
 
 class FastListTemplate(FastBaseTemplate):
     """
-    The FastListTemplate is build on top of Fast.design.
+    The FastListTemplate is a list based Template with a header, sidebar and main area. Its based
+    on the fast.design style and works well in both default (light) and dark mode.
+
+    Reference: https://panel.holoviz.org/reference/templates/FastListTemplate.html
+
+    Example:
+
+    >>> pn.template.FastListTemplate(
+    ...     site="Panel", title="FastListTemplate", accent="#A01346",
+    ...     sidebar=[pn.pane.Markdown("## Settings"), some_slider],
+    ...     main=[some_python_object]
+    ... ).servable()
+
+    Some *accent* colors that work well are #A01346 (Fast), #00A170 (Mint), #DAA520 (Golden Rod),
+    #2F4F4F (Dark Slate Grey), #F08080 (Light Coral) and #4099da (Summer Sky).
+    
+    You can also use the FastListTemplate as shown below
+
+    >>> pn.extension(..., template="fast")
+    >>> pn.state.template.param.update(site="Panel", title="FastListTemplate", accent="#A01346")
+    >>> pn.pane.Markdown("## Settings").servable(target="sidebar")
+    >>> some_slider = pn.widgets.IntSlider(...).servable(target="sidebar")
+    >>> ...
+    >>> pn.panel(some_python_object).servable(target="main")
+
+    This api is great for more exploratory use cases.
+
+    Please note the FastListTemplate cannot display in a notebook output cell.
     """
 
     _css = FastBaseTemplate._css + [
