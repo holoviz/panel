45b401cc228320cce8c9af2ae278ae17e7814989
Marc Skov Madsen <masma@orsted.dk>
2022-05-29 20:38:19 +0200

    improve fast docstrings

--- panel/template/fast/grid/__init__.py
+++ panel/template/fast/grid/__init__.py
@@ -13,8 +13,8 @@ from ..theme import FastDarkTheme, FastDefaultTheme
 
 class FastGridTemplate(FastGridBaseTemplate):
     """
-    The FastGridTemplate is a grid based Template with a header, sidebar and main area. Its based
-    on the fast.design style and works well in both default (light) and dark mode.
+    The `FastGridTemplate` is a grid based Template with a header, sidebar and main area. It is
+    based on the fast.design style and works well in both default (light) and dark mode.
 
     Reference: https://panel.holoviz.org/reference/templates/FastGridTemplate.html
 
@@ -29,7 +29,7 @@ class FastGridTemplate(FastGridBaseTemplate):
     Some *accent* colors that work well are #A01346 (Fast), #00A170 (Mint), #DAA520 (Golden Rod),
     #2F4F4F (Dark Slate Grey), #F08080 (Light Coral) and #4099da (Summer Sky).
 
-    Please note the FastListTemplate cannot display in a notebook output cell.
+    Please note the `FastListTemplate` cannot display in a notebook output cell.
     """
 
     _css = FastGridBaseTemplate._css + [
--- panel/template/fast/list/__init__.py
+++ panel/template/fast/list/__init__.py
@@ -13,8 +13,8 @@ from ..theme import FastDarkTheme, FastDefaultTheme
 
 class FastListTemplate(FastBaseTemplate):
     """
-    The FastListTemplate is a list based Template with a header, sidebar and main area. Its based
-    on the fast.design style and works well in both default (light) and dark mode.
+    The `FastListTemplate` is a list based Template with a header, sidebar and main area. It is
+    based on the fast.design style and works well in both default (light) and dark mode.
 
     Reference: https://panel.holoviz.org/reference/templates/FastListTemplate.html
 
@@ -29,7 +29,7 @@ class FastListTemplate(FastBaseTemplate):
     Some *accent* colors that work well are #A01346 (Fast), #00A170 (Mint), #DAA520 (Golden Rod),
     #2F4F4F (Dark Slate Grey), #F08080 (Light Coral) and #4099da (Summer Sky).
     
-    You can also use the FastListTemplate as shown below
+    You can also use the `FastListTemplate` as shown below
 
     >>> pn.extension(..., template="fast")
     >>> pn.state.template.param.update(site="Panel", title="FastListTemplate", accent="#A01346")
@@ -40,7 +40,7 @@ class FastListTemplate(FastBaseTemplate):
 
     This api is great for more exploratory use cases.
 
-    Please note the FastListTemplate cannot display in a notebook output cell.
+    Please note the `FastListTemplate` cannot display in a notebook output cell.
     """
 
     _css = FastBaseTemplate._css + [
