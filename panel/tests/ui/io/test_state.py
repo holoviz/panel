ba35b77edde3289d258d63c215b1d6798ecf84aa
Philipp Rudiger <prudiger@anaconda.com>
2022-06-08 18:14:13 +0200

    Add more tests

---
+++ panel/tests/ui/io/test_state.py
@@ -0,0 +1,29 @@
+import time
+
+import pytest
+
+pytestmark = pytest.mark.ui
+
+from panel.io.server import serve
+from panel.io.state import state
+from panel.pane import Markdown
+
+
+def test_on_load(page):
+    def app():
+        md = Markdown('Initial')
+
+        def cb():
+            md.object = 'Loaded'
+
+        state.onload(cb)
+        return md
+
+    port = 7002
+    serve(app, port=port, threaded=True, show=False)
+
+    time.sleep(0.2)
+
+    page.goto(f"http://localhost:{port}")
+
+    assert page.text_content(".bk.markdown") == 'Loaded'
---
+++ panel/tests/ui/widgets/test_button.py
@@ -0,0 +1,30 @@
+import time
+
+import pytest
+
+pytestmark = pytest.mark.ui
+
+from panel.io.server import serve
+from panel.widgets import Button
+
+
+def test_button_click(page):
+    button = Button(name='Click')
+
+    events = []
+    def cb(event):
+        events.append(event)
+    button.on_click(cb)
+
+    port = 7003
+    serve(button, port=port, threaded=True, show=False)
+
+    time.sleep(0.2)
+
+    page.goto(f"http://localhost:{port}")
+
+    page.click('.bk.bk-btn')
+
+    time.sleep(0.1)
+
+    assert len(events) == 1
