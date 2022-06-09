8b825f7534156c6dca76da307eed939d66d568db
Philipp Rudiger <prudiger@anaconda.com>
2022-06-09 18:11:02 +0200

    One more test

--- panel/tests/ui/test_reactive.py
+++ panel/tests/ui/test_reactive.py
@@ -18,7 +18,7 @@ class ReactiveComponent(ReactiveHTML):
     _scripts = {'render': 'data.count += 1; reactive.innerText = `${data.count}`;'}
 
 
-def test_reactive_html_loading_no_rerender(page, port):
+def test_reactive_html_set_loading_no_rerender(page, port):
     component = ReactiveComponent()
 
     serve(component, port=port, threaded=True, show=False)
@@ -28,21 +28,14 @@ def test_reactive_html_loading_no_rerender(page, port):
     page.goto(f"http://localhost:{port}")
 
     assert page.text_content(".reactive") == '1'
-
     component.loading = True
-
     time.sleep(0.1)
-
     assert page.text_content(".reactive") == '1'
-
     component.loading = False
-
     time.sleep(0.1)
-
     assert page.text_content(".reactive") == '1'
 
-
-def test_reactive_html_loading_css_classes_rerender(page, port):
+def test_reactive_html_changing_css_classes_rerenders(page, port):
     component = ReactiveComponent()
 
     serve(component, port=port, threaded=True, show=False)
@@ -52,15 +45,29 @@ def test_reactive_html_loading_css_classes_rerender(page, port):
     page.goto(f"http://localhost:{port}")
 
     assert page.text_content(".reactive") == '1'
-
     component.css_classes = ['custom']
-
     time.sleep(0.1)
-
     assert page.text_content(".reactive") == '2'
-
-    component.param.trigger('css_classes')
-
+    component.loading = True
     time.sleep(0.1)
-
     assert page.text_content(".reactive") == '2'
+    component.css_classes = []
+    time.sleep(0.1)
+    assert page.text_content(".reactive") == '3'
+
+def test_reactive_html_set_background_no_rerender(page, port):
+    component = ReactiveComponent()
+
+    serve(component, port=port, threaded=True, show=False)
+
+    time.sleep(0.2)
+
+    page.goto(f"http://localhost:{port}")
+
+    assert page.text_content(".reactive") == '1'
+    component.background = 'red'
+    time.sleep(0.1)
+    assert page.text_content(".reactive") == '1'
+    component.background = 'green'
+    time.sleep(0.1)
+    assert page.text_content(".reactive") == '1'
