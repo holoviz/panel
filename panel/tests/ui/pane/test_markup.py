233a7aa3d954f2be58a2783886a1136a1ab0f070
Philipp Rudiger <prudiger@anaconda.com>
2022-06-13 14:25:43 +0200

    Invalidate layout on HTML re-rerender (#3616)
    * Invalidate layout on HTML re-rerender
    
    * Add test

--- panel/tests/ui/pane/test_markup.py
+++ panel/tests/ui/pane/test_markup.py
@@ -14,13 +14,26 @@ def test_update_markdown_pane(page, port):
     serve(md, port=port, threaded=True, show=False)
 
     time.sleep(0.2)
-
     page.goto(f"http://localhost:{port}")
 
     assert page.text_content(".bk.markdown") == 'Initial'
-
     md.object = 'Updated'
-
     time.sleep(0.1)
-
     assert page.text_content(".bk.markdown") == 'Updated'
+
+
+def test_update_markdown_pane_resizes(page, port):
+    md = Markdown('Initial')
+
+    serve(md, port=port, threaded=True, show=False)
+
+    time.sleep(0.2)
+    page.goto(f"http://localhost:{port}")
+
+    assert page.locator(".bk.markdown").bounding_box()['height'] == 45
+    md.object = """
+    - Bullet
+    - Points
+    """
+    time.sleep(0.1)
+    assert page.locator(".bk.markdown").bounding_box()['height'] == 64
