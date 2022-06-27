e93d69870a51c41fbe4a74fbb935eefb7b8992a7
Philipp Rudiger <prudiger@anaconda.com>
2022-06-27 09:59:11 +0200

    Fix support for copying cells and creating new views in JupyterLab

--- panel/_templates/doc_nb_js.js
+++ panel/_templates/doc_nb_js.js
@@ -3,6 +3,15 @@
     var docs_json = {{ docs_json }};
     var render_items = {{ render_items }};
     root.Bokeh.embed.embed_items_notebook(docs_json, render_items);
+    for (const render_item of render_items) {
+      for (const root_id of render_item.root_ids) {
+	const id_el = document.getElementById(root_id)
+	if (id_el.children.length && (id_el.children[0].className === 'bk-root')) {
+	  const root_el = id_el.children[0]
+	  root_el.id = root_el.id + '-rendered'
+	}
+      }
+    }
   }
   if (root.Bokeh !== undefined && root.Bokeh.Panel !== undefined{% for reqs in requirements %} && ({% for req in reqs %}{% if loop.index0 > 0 %}||{% endif %} root['{{ req }}'] !== undefined{% endfor %}){% endfor %}{% if ipywidget %}&& (root.Bokeh.Models.registered_names().indexOf("ipywidgets_bokeh.widget.IPyWidget") > -1){% endif %}) {
     embed_document(root);
