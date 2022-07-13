94476537fe7c4e8aab74f8db08abae0894895495
thuydotm <thuydtm.csk9@gmail.com>
2022-07-13 16:06:25 +0700

    more tests

--- panel/tests/ui/layout/test_accordion.py
+++ panel/tests/ui/layout/test_accordion.py
@@ -129,3 +129,72 @@ def test_accordion_toggle(page, port, accordion_components):
     d2_object.click()
     assert is_collapsed(card_object=d1_object, card_content=d1.text)
     assert is_expanded(card_object=d2_object, card_content=d2.text)
+
+
+def test_accordion_append(page, port, accordion_components):
+    accordion = Accordion()
+    serve(accordion, port=port, threaded=True, show=False)
+    time.sleep(0.2)
+    page.goto(f"http://localhost:{port}")
+
+    accordion_elements = page.locator('.bk.accordion')
+    # empty accordion
+    expect(accordion_elements).to_have_count(0)
+
+    d1, d2 = accordion_components
+
+    # add new element d1
+    accordion.append(d1)
+    expect(accordion_elements).to_have_count(1)
+    # collapsed by default
+    d1_object = accordion_elements.nth(0)
+    assert is_collapsed(card_object=d1_object, card_content=d1.text)
+
+    # add new element d2
+    accordion.append(d2)
+    expect(accordion_elements).to_have_count(2)
+    d2_object = accordion_elements.nth(1)
+    # both cards are collapsed
+    assert is_collapsed(card_object=d1_object, card_content=d1.text)
+    assert is_collapsed(card_object=d2_object, card_content=d2.text)
+
+
+def test_accordion_extend(page, port, accordion_components):
+    d1, d2 = accordion_components
+    accordion = Accordion(d1, d2)
+    serve(accordion, port=port, threaded=True, show=False)
+    time.sleep(0.2)
+    page.goto(f"http://localhost:{port}")
+
+    accordion_elements = page.locator('.bk.accordion')
+    expect(accordion_elements).to_have_count(len(accordion_components))
+
+    d3 = Div(name='Div 3', text='Text 3', css_classes=['class_div3'])
+    additional_list = [d3]
+    # add new list of elements to the accordion
+    accordion.extend(additional_list)
+    expect(accordion_elements).to_have_count(len(accordion_components) + len(additional_list))
+
+    # collapsed by default
+    d1_object = accordion_elements.nth(0)
+    d2_object = accordion_elements.nth(1)
+    d3_object = accordion_elements.nth(2)
+    assert is_collapsed(card_object=d1_object, card_content=d1.text)
+    assert is_collapsed(card_object=d2_object, card_content=d2.text)
+    assert is_collapsed(card_object=d3_object, card_content=d3.text)
+
+
+def test_accordion_clear(page, port, accordion_components):
+    d1, d2 = accordion_components
+    accordion = Accordion(d1, d2)
+    serve(accordion, port=port, threaded=True, show=False)
+    time.sleep(0.2)
+    page.goto(f"http://localhost:{port}")
+
+    accordion_elements = page.locator('.bk.accordion')
+    expect(accordion_elements).to_have_count(len(accordion_components))
+
+    # clear all contents of the accordion
+    accordion.clear()
+    # empty accordion
+    expect(accordion_elements).to_have_count(0)
