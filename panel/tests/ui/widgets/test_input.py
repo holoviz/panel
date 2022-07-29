0168d1325c685a0dd8e489238214e10e452da200
thuydotm <thuydtm.csk9@gmail.com>
2022-07-29 13:11:06 +0700

    more tests

--- panel/tests/ui/widgets/test_input.py
+++ panel/tests/ui/widgets/test_input.py
@@ -13,7 +13,97 @@ except ImportError:
 pytestmark = pytest.mark.ui
 
 
-def test_datetimepicker_default(page, port):
+@pytest.fixture
+def weekdays_as_str():
+    weekdays_str = '''
+        Sun
+        Mon
+        Tue
+        Wed
+        Thu
+        Fri
+        Sat
+    '''
+    return weekdays_str
+
+
+@pytest.fixture
+def months_as_str():
+    months_str = '''
+        January
+        February
+        March
+        April
+        May
+        June
+        July
+        August
+        September
+        October
+        November
+        December
+    '''
+    return months_str
+
+
+@pytest.fixture
+def months_mapper():
+    mapper = {
+        0: 'January',
+        1: 'February',
+        2: 'March',
+        3: 'April',
+        4: 'May',
+        5: 'June',
+        6: 'July',
+        7: 'August',
+        8: 'September',
+        9: 'October',
+        10: 'November',
+        11: 'December',
+    }
+    return mapper
+
+
+def valid_prev_time(old_value, new_value, max_value, amount=1):
+    current_value = int(old_value)
+    prev_value = int(new_value)
+    if current_value == 0:
+        assert prev_value == max_value - amount
+    else:
+        assert prev_value == current_value - amount
+    return True
+
+
+def valid_next_time(old_value, new_value, max_value, amount=1):
+    current_value = int(old_value)
+    next_value = int(new_value)
+    if current_value == max_value - amount:
+        assert next_value == 0
+    else:
+        assert next_value == current_value + amount
+    return True
+
+
+def valid_prev_month(old_month, new_month, old_year, new_year):
+    assert valid_prev_time(old_month, new_month, max_value=12, amount=1)
+    if int(old_month) == 0:
+        assert int(new_year) == int(old_year) - 1
+    else:
+        assert int(new_year) == int(old_year)
+    return True
+
+
+def valid_next_month(old_month, new_month, old_year, new_year):
+    assert valid_next_time(old_month, new_month, max_value=12, amount=1)
+    if int(old_month) == 11:
+        assert int(new_year) == int(old_year) + 1
+    else:
+        assert int(new_year) == int(old_year)
+    return True
+
+
+def test_datetimepicker_default(page, port, weekdays_as_str, months_as_str):
     datetime_picker_widget = DatetimePicker(name='Datetime Picker')
     serve(datetime_picker_widget, port=port, threaded=True, show=False)
     time.sleep(0.2)
@@ -27,4 +117,145 @@ def test_datetimepicker_default(page, port):
     # no value set at initialization, this locator has an empty string
     expect(datetime_value).to_have_text('', use_inner_text=True)
 
-    datetime_value.click()
+    # click to show the datetime picker
+    datetime_value.dblclick()
+    # ensure the datetime picker is shown
+    datetime_value_active = page.locator('.flatpickr-input.active')
+    expect(datetime_value_active).to_have_count(1)
+
+    # month and year
+    months_container = page.locator('.flatpickr-calendar .flatpickr-months')
+    expect(months_container).to_have_count(1)
+    expect(months_container).to_contain_text(months_as_str, use_inner_text=True)
+
+    prev_month_button = page.locator('.flatpickr-prev-month')
+    next_month_button = page.locator('.flatpickr-next-month')
+    month_dropdown = page.locator('.flatpickr-current-month select.flatpickr-monthDropdown-months')
+    year_input = page.locator('.flatpickr-current-month .numInput.cur-year')
+    year_up = page.locator('.flatpickr-current-month .arrowUp')
+    year_down = page.locator('.flatpickr-current-month .arrowDown')
+    expect(prev_month_button).to_have_count(1)
+    expect(next_month_button).to_have_count(1)
+    expect(month_dropdown).to_have_count(1)
+    expect(prev_month_button).to_have_count(1)
+    expect(year_input).to_have_count(1)
+    expect(year_up).to_have_count(1)
+    expect(year_down).to_have_count(1)
+
+    # change to previous month
+    year_before_click = year_input.input_value()
+    month_before_click = month_dropdown.input_value()
+    # click to select prev month
+    prev_month_button.click()
+    month_after_click = month_dropdown.input_value()
+    year_after_click = year_input.input_value()
+    assert valid_prev_month(
+        month_before_click, month_after_click, year_before_click, year_after_click
+    )
+
+    # change to next month
+    year_before_click = year_input.input_value()
+    month_before_click = month_dropdown.input_value()
+    # click to select next month
+    next_month_button.click()
+    month_after_click = month_dropdown.input_value()
+    year_after_click = year_input.input_value()
+    assert valid_next_month(
+        month_before_click, month_after_click, year_before_click, year_after_click
+    )
+
+    # TODO: check selected option for month dropdown
+    # month_dropdown.click()
+    # month_dropdown.select_option(index=0)
+
+    # change to previous year
+    year_before_click = year_input.input_value()
+    # click to select prev year
+    year_down.click()
+    year_after_click = year_input.input_value()
+    assert int(year_before_click) == int(year_after_click) + 1
+
+    # change to next year
+    year_before_click = year_input.input_value()
+    # click to select prev year
+    year_up.click()
+    year_after_click = year_input.input_value()
+    assert int(year_before_click) == int(year_after_click) - 1
+
+    # weekdays
+    weekdays_container = page.locator('.flatpickr-calendar .flatpickr-weekdays')
+    expect(weekdays_container).to_have_count(1)
+    expect(weekdays_container).to_contain_text(weekdays_as_str, use_inner_text=True)
+
+    # days
+    days_container = page.locator('.flatpickr-calendar .flatpickr-days')
+    expect(days_container).to_have_count(1)
+    # only 1 date circled as current date
+    current_date = page.locator('.flatpickr-calendar .flatpickr-days .flatpickr-day.today')
+    expect(current_date).to_have_count(1)
+
+    # time value
+    time_container = page.locator('.flatpickr-calendar .flatpickr-time')
+    expect(time_container).to_have_count(1)
+
+    time_inputs = page.locator('.flatpickr-calendar .flatpickr-time .numInputWrapper')
+    time_up_buttons = page.locator('.flatpickr-calendar .flatpickr-time .arrowUp')
+    time_down_buttons = page.locator('.flatpickr-calendar .flatpickr-time .arrowDown')
+    # 3 inputs and up/down buttons for hour, min, and sec
+    expect(time_inputs).to_have_count(3)
+    expect(time_up_buttons).to_have_count(3)
+    expect(time_down_buttons).to_have_count(3)
+    # 2 separators `:` hour:min:sec
+    time_separators = page.locator('.flatpickr-calendar .flatpickr-time .flatpickr-time-separator')
+    expect(time_separators).to_have_count(2)
+
+    hour_input = page.locator('.flatpickr-calendar .flatpickr-time .numInput.flatpickr-hour')
+    expect(hour_input).to_have_count(1)
+    hour_up = time_up_buttons.nth(0)
+    hour_down = time_down_buttons.nth(0)
+
+    # click down arrow button to decrease hour
+    hour_before_click = hour_input.input_value()
+    hour_down.click()
+    hour_after_click = hour_input.input_value()
+    assert valid_prev_time(hour_before_click, hour_after_click, max_value=24, amount=1)
+
+    # click up arrow button to increase hour
+    hour_before_click = hour_input.input_value()
+    hour_up.click()
+    hour_after_click = hour_input.input_value()
+    assert valid_next_time(hour_before_click, hour_after_click, max_value=24, amount=1)
+
+    min_input = page.locator('.flatpickr-calendar .flatpickr-time .numInput.flatpickr-minute')
+    expect(min_input).to_have_count(1)
+    min_up = time_up_buttons.nth(1)
+    min_down = time_down_buttons.nth(1)
+
+    # click down arrow button to decrease minute
+    min_before_click = min_input.input_value()
+    min_down.click()
+    min_after_click = min_input.input_value()
+    assert valid_prev_time(min_before_click, min_after_click, max_value=60, amount=5)
+
+    # click up arrow button to increase minutes
+    min_before_click = min_input.input_value()
+    min_up.click()
+    min_after_click = min_input.input_value()
+    assert valid_next_time(min_before_click, min_after_click, max_value=60, amount=5)
+
+    sec_input = page.locator('.flatpickr-calendar .flatpickr-time .numInput.flatpickr-second')
+    expect(sec_input).to_have_count(1)
+    sec_up = time_up_buttons.nth(2)
+    sec_down = time_down_buttons.nth(2)
+
+    # click down arrow button to decrease second
+    sec_before_click = sec_input.input_value()
+    sec_down.click()
+    sec_after_click = sec_input.input_value()
+    assert valid_prev_time(sec_before_click, sec_after_click, max_value=60, amount=5)
+
+    # click up arrow button to increase second
+    sec_before_click = sec_input.input_value()
+    sec_up.click()
+    sec_after_click = sec_input.input_value()
+    assert valid_next_time(sec_before_click, sec_after_click, max_value=60, amount=5)
