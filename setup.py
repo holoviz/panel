5beaa65d90aa49e2414d4f561ba1004e8f575204
Simon Høxbro <simon.hansen@me.com>
2022-08-12 22:20:19 +0200

    Try to install bokeh 3.0 first

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ except Exception:
 ########## dependencies ##########
 
 install_requires = [
-    'bokeh',
+    'bokeh ==3.0.0dev11',
     'param >=1.12.0',
     'pyviz_comms >=0.7.4',
     'markdown',
