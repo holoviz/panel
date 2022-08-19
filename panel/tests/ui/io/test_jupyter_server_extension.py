1f5b97a8a0a3c3b4a6b33a6cd41b61316f2eb138
Philipp Rudiger <prudiger@anaconda.com>
2022-08-19 21:16:22 +0200

    Increase timeout

--- panel/tests/ui/io/test_jupyter_server_extension.py
+++ panel/tests/ui/io/test_jupyter_server_extension.py
@@ -9,7 +9,7 @@ not_windows = pytest.mark.skipif(sys.platform=='win32', reason="Does not work on
 
 @not_windows
 def test_jupyter_server(page, jupyter_server):
-    page.goto(f"{jupyter_server}/panel-preview/render/app.py")
+    page.goto(f"{jupyter_server}/panel-preview/render/app.py", timeout=60000)
 
     assert page.text_content('.bk.string') == '0'
 
@@ -25,7 +25,7 @@ def test_jupyter_server(page, jupyter_server):
 
 @not_windows
 def test_jupyter_server_kernel_error(page, jupyter_server):
-    page.goto(f"{jupyter_server}/panel-preview/render/app.py?kernel=blah")
+    page.goto(f"{jupyter_server}/panel-preview/render/app.py?kernel=blah", timeout=60000)
 
     assert page.text_content('#subtitle') == "Kernel error: No such kernel 'blah'"
 
