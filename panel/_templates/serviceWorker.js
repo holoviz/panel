ffa99fdb499ce4d801f5cf3ebceddbabce93bfbf
Philipp Rudiger <prudiger@anaconda.com>
2022-09-18 21:54:09 +0200

    Ensure pyodide app cache is invalidated (#3860)
    * Improve caching policy for converted apps
    
    * Build converted apps in correct place

--- panel/_templates/serviceWorker.js
+++ panel/_templates/serviceWorker.js
@@ -1,23 +1,33 @@
-const cacheName = '{{ app }}';
+const appName = 'Panel-Pyodide-App-{{ uuid }}';
 
 const preCacheFiles = [{{ pre_cache }}];
 
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
   e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
+    const cache = await caches.open(appName);
     console.log('[Service Worker] Caching ');
     await cache.addAll(preCacheFiles);
   })());
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.forEach((cache, cacheName) => {
+      if (cacheName.startsWith('Panel-Pyodide-App') && cacheName !== appName) {
+        return caches.delete(cacheName);
+      }
+    })
+  );
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith((async () => {
-    const r = await caches.match(e.request);
+    const cache = await caches.open(appName);
+    let response = await cache.match(e.request);
     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) { return r; }
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
+    if (response) { return response; }
+    response = await fetch(e.request);
     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
     cache.put(e.request, response.clone());
     return response;
