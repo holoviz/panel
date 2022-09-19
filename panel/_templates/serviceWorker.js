f3bef99e2cc9c82508ce256daf66143da133324b
Philipp Rudiger <prudiger@anaconda.com>
2022-09-19 17:41:53 +0200

    Make service worker caching more robust

--- panel/_templates/serviceWorker.js
+++ panel/_templates/serviceWorker.js
@@ -5,10 +5,17 @@ const preCacheFiles = [{{ pre_cache }}];
 
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
+  self.skipWaiting();
   e.waitUntil((async () => {
     const cache = await caches.open(appCacheName);
     console.log('[Service Worker] Caching ');
-    await cache.addAll(preCacheFiles);
+    preCacheFiles.forEach(async (cacheFile) => {
+      const request = new Request(cacheFile);
+      const response = await fetch(request);
+      if (response.ok || response.type == 'opaque') {
+        cache.put(request, response);
+      }
+    })
   })());
 });
 
@@ -18,7 +25,7 @@ self.addEventListener('activate', (event) => {
     const cacheNames = await caches.keys();
     for (const cacheName of cacheNames) {
       if (cacheName.startsWith(appName) && cacheName !== appCacheName) {
-	console.log(`[Service Worker] Delete old cache ${cacheName}`);
+        console.log(`[Service Worker] Delete old cache ${cacheName}`);
         caches.delete(cacheName);
       }
     }
@@ -27,12 +34,20 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') {
+    return
+  }
   e.respondWith((async () => {
     const cache = await caches.open(appCacheName);
     let response = await cache.match(e.request);
     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (response) { return response; }
+    if (response) {
+      return response;
+    }
     response = await fetch(e.request);
+    if (!response.ok && !(response.type == 'opaque')) {
+      throw Error('[Service Worker] Fetching resource failed with response: ' + response.status);
+    }
     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
     cache.put(e.request, response.clone());
     return response;
