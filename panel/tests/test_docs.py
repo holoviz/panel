10a3ced495aaa6c1eb292d9cd05b2f9cc4d35b80
Simon Høxbro <simon.hansen@me.com>
2022-09-23 14:54:20 +0200

    Added testing of markdown docs

--- panel/tests/test_docs.py
+++ panel/tests/test_docs.py
@@ -2,9 +2,11 @@
 These tests verify that all of the panes, layouts, and widgets defined by panel are
 represented in the reference gallery.
 """
+import ast
 import os
 
 from inspect import isclass
+from pathlib import Path
 
 import pytest
 
@@ -52,3 +54,25 @@ def test_panes_are_in_reference_gallery():
 
     panes = set(filter(is_panel_pane, dir(pn.pane)))
     assert panes - exceptions - docs == set()
+
+
+def test_markdown_codeblocks():
+    import commonmark
+    md_parser = commonmark.Parser()
+
+    path = Path(__file__).parents[2] / "docs"
+
+    for file in sorted(path.rglob("*.md")):
+        print(file)
+        md_ast = md_parser.parse(file.read_text())
+        lines = ""
+        for n, _ in md_ast.walker():
+            if n.t == "code_block" and n.info is not None:
+                if 'pyodide' in n.info.lower() or 'python' in n.info.lower():
+                    if ">>>" not in n.literal:
+                        lines += n.literal
+        if lines:
+            try:
+                ast.parse(lines)
+            except SyntaxError as e:
+                raise SyntaxError(file) from e
