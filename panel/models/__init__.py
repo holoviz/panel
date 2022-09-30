66e237ffab340cc41f04f03ae130f6d18dbfd5fc
Philipp Rudiger <prudiger@anaconda.com>
2022-09-30 17:12:13 +0200

    Remove custom Tabs model from index

--- panel/models/__init__.py
+++ panel/models/__init__.py
@@ -13,7 +13,6 @@ from .location import Location  # noqa
 from .markup import HTML, JSON  # noqa
 from .reactive_html import ReactiveHTML  # noqa
 from .state import State  # noqa
-from .tabs import Tabs  # noqa
 from .trend import TrendIndicator  # noqa
 from .widgets import (  # noqa
     Audio, CustomSelect, FileDownload, Player, Progress, SingleSelect, Video,
