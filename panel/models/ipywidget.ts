8768106c467fa50f1cb4d1ccadab25be2a4db0b5
Philipp Rudiger <prudiger@anaconda.com>
2022-10-02 11:52:41 +0200

    Simplify rendering of ipywidget (#3937)

--- panel/models/ipywidget.ts
+++ panel/models/ipywidget.ts
@@ -9,47 +9,45 @@ export class IPyWidgetView extends PanelHTMLBoxView {
   model: IPyWidget
   private rendered: boolean = false
   private ipyview: any
+  private manager: any
 
-  render(): void {
-    super.render()
-    this._render().then(() => {
-      this.rendered = true
-      this.invalidate_layout()
-      this.notify_finished()
-    })
-  }
-
-  has_finished(): boolean {
-    return this.rendered && super.has_finished()
-  }
-
-  async _render(): Promise<void> {
-    const {spec, state} = this.model.bundle
+  override async lazy_initialize(): Promise<void> {
+    await super.lazy_initialize()
     let manager: any
     if ((Jupyter != null) && (Jupyter.notebook != null))
       manager = Jupyter.notebook.kernel.widget_manager
     else if ((window as any).PyViz.widget_manager != null)
       manager = (window as any).PyViz.widget_manager
-    if (!manager) {
-      console.log("Panel IPyWidget model could not find a WidgetManager")
+    else {
+      console.warn("Panel IPyWidget model could not find a WidgetManager")
       return
     }
-
-    if (this.ipyview == null) {
-      const models = await manager.set_state(state)
-      const model = models.find((item: any) => item.model_id == spec.model_id)
-      if (model != null) {
-        const view = await manager.create_view(model, {el: this.el})
-        this.ipyview = view
-        if (view.children_views) {
-          for (const child of view.children_views.views)
-            await child
-        }
-        this.el.appendChild(view.el)
-        view.trigger('displayed', view)
+    this.manager = manager
+    const {spec, state} = this.model.bundle
+    const models = await manager.set_state(state)
+    const model = models.find((item: any) => item.model_id == spec.model_id)
+    if (model != null) {
+      const view = await this.manager.create_view(model, {el: this.el})
+      this.ipyview = view
+      if (view.children_views) {
+        for (const child of view.children_views.views)
+          await child
       }
-    } else
+    }
+  }
+
+  render(): void {
+    super.render()
+    if (this.ipyview != null) {
       this.el.appendChild(this.ipyview.el)
+      if (!this.rendered)
+	this.ipyview.trigger('displayed', this.ipyview)
+    }
+    this.rendered = true
+  }
+
+  has_finished(): boolean {
+    return this.rendered && super.has_finished()
   }
 }
 
