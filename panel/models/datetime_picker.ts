9c87527f68c6dc823c06d86534ac51002ff2e66c
Simon Høxbro <simon.hansen@me.com>
2022-10-13 10:12:42 +0200

    Don't set empty value

--- panel/models/datetime_picker.ts
+++ panel/models/datetime_picker.ts
@@ -89,6 +89,8 @@ export class DatetimePickerView extends InputWidgetView {
   protected _on_close(_selected_dates: Date[], date_string: string, _instance: flatpickr.Instance): void {
     if (this.model.mode == "range" && !date_string.includes("to"))
       return
+    if (date_string == "")
+      return
     this.model.value = date_string
     this.change_input()
   }
