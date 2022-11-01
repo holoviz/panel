b2e65521394beab216ee22470d490c7cb852fc16
Simon Høxbro <simon.hansen@me.com>
2022-11-01 13:00:08 +0100

    Added workaround for conftest.module_cleanup

--- panel/tests/conftest.py
+++ panel/tests/conftest.py
@@ -18,7 +18,6 @@ import pytest
 
 from bokeh.client import pull_session
 from bokeh.document import Document
-from bokeh.model import Model
 from pyviz_comms import Comm
 
 from panel import config, serve
@@ -292,11 +291,15 @@ def module_cleanup():
     """
     Cleanup Panel extensions after each test.
     """
+    from bokeh.core.has_props import _default_resolver
     to_reset = list(panel_extension._imports.values())
-    Model.model_class_reverse_map = {
-        name: model for name, model in Model.model_class_reverse_map.items()
+
+    _default_resolver._known_models = {
+        name: model for name, model in _default_resolver._known_models.items()
         if not any(model.__module__.startswith(tr) for tr in to_reset)
     }
+
+    # maybe use default_resolver.clear_extensions()?
 
 
 @pytest.fixture(autouse=True)
