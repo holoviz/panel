7abc236acbc8da251f8e1cce18793204e96d1248
maximlt <mliquet@anaconda.com>
2022-11-02 08:22:05 +0100

    move pip deploy deps to dodo.py

--- dodo.py
+++ dodo.py
@@ -10,7 +10,7 @@ def task_pip_on_conda():
     """Experimental: provide pip build env via conda"""
     return {'actions':[
         # some ecosystem=pip build tools must be installed with conda when using conda...
-        'conda install -y pip twine wheel',
+        'conda install -y pip twine wheel "rfc3986>=1.4.0"',
         # ..and some are only available via conda-forge
         'conda install -y -c conda-forge tox "virtualenv<=20.4.7"',
     ]}
