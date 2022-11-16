f820edd7727649684c920f91bb9ce94ec620e44b
Philipp Rudiger <prudiger@anaconda.com>
2022-11-16 11:28:13 +0100

    Copy assets

--- scripts/generate_panelite_content.py
+++ scripts/generate_panelite_content.py
@@ -2,23 +2,36 @@
 Helper script to convert and copy example notebooks into JupyterLite build.
 """
 import pathlib
+import shutil
 
 import nbformat
 
 PANEL_BASE = pathlib.Path(__file__).parent.parent
 EXAMPLES_DIR = PANEL_BASE / 'examples'
+
+# Add piplite command to notebooks
 DEPENDENCIES = [repr(d) for d in ['panel', 'pyodide-http', 'altair', 'hvplot', 'matplotlib', 'plotly', 'pydeck', 'scikit-learn']]
 
-nbs = list(EXAMPLES_DIR.glob('*/*/*.ipynb')) + list(EXAMPLES_DIR.glob('*/*.ipynb'))
+nbs = list(EXAMPLES_DIR.glob('*/*/*.ipynb')) + list(EXAMPLES_DIR.glob('*/*.*'))
 install = nbformat.v4.new_code_cell(source=f"import piplite\nawait piplite.install([{', '.join(DEPENDENCIES)}])")
 del install['id']
 
 for nb in nbs:
     nbpath = pathlib.Path(nb)
-    with open(nb) as fin:
-        nb = nbformat.read(fin, 4)
-        nb['cells'].insert(0, install)
     out = (PANEL_BASE / 'lite/files') / nbpath.relative_to(EXAMPLES_DIR)
     out.parent.mkdir(parents=True, exist_ok=True)
-    with open(out, 'w') as fout:
-        nbformat.write(nb, fout)
+    if nb.suffix == '.ipynb':
+        with open(nb) as fin:
+            nb = nbformat.read(fin, 4)
+            nb['cells'].insert(0, install)
+        with open(out, 'w') as fout:
+            nbformat.write(nb, fout)
+    elif not nb.is_dir():
+        shutil.copyfile(nb, out)
+
+# Copy assets
+shutil.copytree(
+    EXAMPLES_DIR / 'assets',
+    PANEL_BASE / 'lite' / 'files' / 'assets',
+    dirs_exist_ok=True
+)
