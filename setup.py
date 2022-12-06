c61369a8c44bfa67443bd67985106174ddfca3cc
maximlt <mliquet@anaconda.com>
2022-12-06 11:30:57 +0100

    add 3.11 classifier

--- setup.py
+++ setup.py
@@ -260,6 +260,7 @@ setup_args = dict(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
