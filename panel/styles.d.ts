761c7a5ddb885c3922cee115057493c393458f1c
Mateusz Paprocki <mattpap@gmail.com>
2022-12-15 16:49:32 +0100

    Add a declaration file for CSS modules

---
+++ panel/styles.d.ts
@@ -0,0 +1,36 @@
+declare module "styles/alerts.css" {
+  export default ""
+}
+declare module "styles/booleanstatus.css" {
+  export default ""
+}
+declare module "styles/card.css" {
+  export default ""
+}
+declare module "styles/dataframe.css" {
+  export default ""
+}
+declare module "styles/debugger.css" {
+  export default ""
+}
+declare module "styles/json.css" {
+  export default ""
+}
+declare module "styles/loading.css" {
+  export default ""
+}
+declare module "styles/loadingspinner.css" {
+  export default ""
+}
+declare module "styles/markdown.css" {
+  export default ""
+}
+declare module "styles/progress.css" {
+  export default ""
+}
+declare module "styles/variables.css" {
+  export default ""
+}
+declare module "styles/widgets.css" {
+  export default ""
+}
