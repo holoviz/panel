5578f9889ae7ab49c9c07bec426d38dbf81d3aed
Philipp Rudiger <prudiger@anaconda.com>
2023-01-19 19:05:44 +0100

    Skip on_load callbacks in liveness check (#4302)

--- panel/io/liveness.py
+++ panel/io/liveness.py
@@ -1,7 +1,6 @@
 from tornado import web
 
 from .document import _cleanup_doc
-from .state import set_curdoc, state
 
 
 class LivenessHandler(web.RequestHandler):
@@ -21,8 +20,6 @@ class LivenessHandler(web.RequestHandler):
         app = self.applications[endpoint]
         try:
             doc = app.create_document()
-            with set_curdoc(doc):
-                state._on_load(None)
             _cleanup_doc(doc)
             self.write({endpoint: True})
         except Exception as e:
