6c89ef0054fa5ebac73796c8e2035656aefe677b
Marc Skov Madsen <masma@orsted.com>
2023-02-04 15:39:13 +0000

    add piplite.install proof of concept

--- scripts/generate_panelite_content.py
+++ scripts/generate_panelite_content.py
@@ -14,34 +14,57 @@ PANEL_BASE = pathlib.Path(__file__).parent.parent
 EXAMPLES_DIR = PANEL_BASE / 'examples'
 
 # Add piplite command to notebooks
-DEPENDENCIES = [repr(d) for d in ['panel', 'pyodide-http', 'altair', 'hvplot', 'matplotlib', 'plotly', 'pydeck', 'scikit-learn']]
+DEPENDENCIES = ['panel', 'pyodide-http', 'altair', 'hvplot', 'matplotlib', 'plotly', 'pydeck', 'scikit-learn']
+DEPENDENCY_MAP = {
+    "reference/global/Notifications.ipynb": ["panel"],
+    "reference/indicators/BooleanStatus.ipynb": ["panel"],
+    "reference/indicators/Dial.ipynb": ["panel"],
+    "reference/indicators/Gauge.ipynb": ["panel"],
+    "reference/indicators/LinearGauge.ipynb": ["panel"],
+    "reference/indicators/LoadingSpinner.ipynb": ["panel"],
+    "reference/indicators/Number.ipynb": ["panel"]
+}
 
-nbs = list(EXAMPLES_DIR.glob('*/*/*.ipynb')) + list(EXAMPLES_DIR.glob('*/*.*'))
-install = nbformat.v4.new_code_cell(source=f"import piplite\nawait piplite.install([{', '.join(DEPENDENCIES)}])")
-del install['id']
+def _get_dependencies(nbpath: pathlib.Path):
+    key = str(nbpath).split("examples/")[-1]
+    dependencies = DEPENDENCY_MAP.get(key, DEPENDENCIES)
+    dependencies = [repr(d) for d in dependencies]
+    return dependencies
 
-for nb in nbs:
-    nbpath = pathlib.Path(nb)
-    out = (PANEL_BASE / 'lite/files') / nbpath.relative_to(EXAMPLES_DIR)
-    out.parent.mkdir(parents=True, exist_ok=True)
-    if nb.suffix == '.ipynb':
-        with open(nb, encoding='utf-8') as fin:
-            nb = nbformat.read(fin, 4)
-            nb['cells'].insert(0, install)
-        with open(out, 'w', encoding='utf-8') as fout:
-            nbformat.write(nb, fout)
-    elif not nb.is_dir():
-        shutil.copyfile(nb, out)
+def _to_source(dependencies):
+    return f"import piplite\nawait piplite.install([{', '.join(dependencies)}])"
 
-# Copy assets
-shutil.copytree(
-    EXAMPLES_DIR / 'assets',
-    PANEL_BASE / 'lite' / 'files' / 'assets',
-    dirs_exist_ok=True
-)
+def _get_install_code_cell(nbpath: pathlib.Path):
+    dependencies = _get_dependencies(nbpath)
+    source = _to_source(dependencies)
+    install = nbformat.v4.new_code_cell(source=source)
+    del install['id']
+    return install
 
-# Download sampledata
-def download():
+def copy_examples():
+    nbs = list(EXAMPLES_DIR.glob('*/*/*.ipynb')) + list(EXAMPLES_DIR.glob('*/*.*'))
+    for nb in nbs:
+        nbpath = pathlib.Path(nb)
+        out = (PANEL_BASE / 'lite/files') / nbpath.relative_to(EXAMPLES_DIR)
+        out.parent.mkdir(parents=True, exist_ok=True)
+        if nb.suffix == '.ipynb':
+            with open(nb, encoding='utf-8') as fin:
+                nb = nbformat.read(fin, 4)
+                install = _get_install_code_cell(nbpath)
+                nb['cells'].insert(0, install)
+            with open(out, 'w', encoding='utf-8') as fout:
+                nbformat.write(nb, fout)
+        elif not nb.is_dir():
+            shutil.copyfile(nb, out)
+
+def copy_assets():
+    shutil.copytree(
+        EXAMPLES_DIR / 'assets',
+        PANEL_BASE / 'lite' / 'files' / 'assets',
+        dirs_exist_ok=True
+    )
+
+def download_sample_data():
     """
     Download larger data sets for various Bokeh examples.
     """
@@ -71,4 +94,7 @@ def download():
                 print(f"Re-fetching {filename!r} (checksum mismatch)")
         _download_file(s3, filename, data_dir, progress=False)
 
-download()
+if __name__=="__main__":
+    copy_examples()
+    copy_assets()
+    download_sample_data()
