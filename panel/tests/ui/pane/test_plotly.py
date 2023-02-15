424cb0ab166f9259ad7a1109502de1835befa109
Simon Høxbro Hansen <simon.hansen@me.com>
2023-02-15 11:25:33 +0100

    Add flaky to plotly hover data

--- panel/tests/ui/pane/test_plotly.py
+++ panel/tests/ui/pane/test_plotly.py
@@ -128,6 +128,7 @@ def test_plotly_3d_plot(page, port, plotly_3d_plot):
 
 
 @plotly_available
+@pytest.mark.flaky(max_runs=3)
 def test_plotly_hover_data(page, port, plotly_2d_plot):
     serve(plotly_2d_plot, port=port, threaded=True, show=False)
 
