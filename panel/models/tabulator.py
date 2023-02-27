37c28781f074d0150f664219376416151bacd41c
Philipp Rudiger <prudiger@anaconda.com>
2023-02-27 18:06:10 +0100

    Fix lint

--- panel/models/tabulator.py
+++ panel/models/tabulator.py
@@ -12,7 +12,7 @@ from bokeh.models import ColumnDataSource, LayoutDOM
 from bokeh.models.widgets.tables import TableColumn
 
 from ..config import config
-from ..io.resources import JS_VERSION, bundled_files
+from ..io.resources import bundled_files
 from ..util import classproperty
 from .layout import HTMLBox
 
