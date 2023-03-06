8c36cbbbbd4674523e0d26648a4d45117fea9f31
Philipp Rudiger <prudiger@anaconda.com>
2023-03-06 23:23:17 +0100

    Replace usage of deprecated make_current (#4506)
    * Replace usage of deprecated make_current
    
    * Set make_current=False in IOLoop constructor

--- panel/io/server.py
+++ panel/io/server.py
@@ -705,7 +705,7 @@ def serve(
         location=location, admin=admin
     ))
     if threaded:
-        kwargs['loop'] = loop = IOLoop() if loop is None else loop
+        kwargs['loop'] = loop = IOLoop(make_current=False) if loop is None else loop
         server = StoppableThread(
             target=get_server, io_loop=loop, args=(panels,), kwargs=kwargs
         )
@@ -919,7 +919,7 @@ def get_server(
 
     opts = dict(kwargs)
     if loop:
-        loop.make_current()
+        asyncio.set_event_loop(loop.asyncio_loop)
         opts['io_loop'] = loop
     elif opts.get('num_procs', 1) == 1:
         opts['io_loop'] = IOLoop.current()
