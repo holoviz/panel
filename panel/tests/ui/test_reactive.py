a1a876e7c1cae5e97c8b57782a638bf89d058e17
Philipp Rudiger <prudiger@anaconda.com>
2023-03-07 00:01:11 +0100

    Update preact and htm (#4509)

--- panel/tests/ui/test_reactive.py
+++ panel/tests/ui/test_reactive.py
@@ -13,9 +13,34 @@ class ReactiveComponent(ReactiveHTML):
 
     count = param.Integer(default=0)
 
-    _template = "<div id='reactive' class='reactive'></div>"
+    _template = """
+    <div id="reactive" class="reactive" onclick="${script('click')}"></div>
+    """
 
-    _scripts = {'render': 'data.count += 1; reactive.innerText = `${data.count}`;'}
+    _scripts = {
+        'render': 'data.count += 1; reactive.innerText = `${data.count}`;',
+        'click': 'data.count += 1; reactive.innerText = `${data.count}`;'
+    }
+
+
+def test_reactive_html_click_js_event(page, port):
+    component = ReactiveComponent()
+
+    serve(component, port=port, threaded=True, show=False)
+
+    time.sleep(0.2)
+
+    page.goto(f"http://localhost:{port}")
+
+    assert page.text_content(".reactive") == '1'
+
+    page.locator(".reactive").click()
+
+    assert page.text_content(".reactive") == '2'
+
+    time.sleep(0.2)
+
+    assert component.count == 2
 
 
 def test_reactive_html_set_loading_no_rerender(page, port):
