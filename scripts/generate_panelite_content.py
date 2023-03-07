f0c64ae753548d2e405397718a9eaddf1d1049b0
Marc Skov Madsen <marc.skov.madsen@gmail.com>
2023-03-07 11:40:28 +0100

    Improve piplite.install time for Panelite notebooks (#4388)

--- scripts/generate_panelite_content.py
+++ scripts/generate_panelite_content.py
@@ -14,34 +14,79 @@ PANEL_BASE = pathlib.Path(__file__).parent.parent
 EXAMPLES_DIR = PANEL_BASE / 'examples'
 
 # Add piplite command to notebooks
-DEPENDENCIES = [repr(d) for d in ['panel', 'pyodide-http', 'altair', 'hvplot', 'matplotlib', 'plotly', 'pydeck', 'scikit-learn']]
+DEFAULT_DEPENDENCIES = ['panel', 'pyodide-http', 'altair', 'hvplot', 'matplotlib', 'plotly', 'pydeck', 'scikit-learn']
+with open(PANEL_BASE/"scripts"/"panelite_dependencies.json", "r", encoding="utf8") as file:
+    DEPENDENCIES = json.load(file)
+DEPENDENCY_NOT_IMPORTABLE = [
+    "streamz", # https://github.com/python-streamz/streamz/issues/467,
+    "vtk", # https://gitlab.kitware.com/vtk/vtk/-/issues/18806
+]
+NOTEBOOK_ISSUES = {
+    "Getting_Started.ipynb": ["https://github.com/holoviz/panel/issues/4416"],
+    "reference/panes/DataFrame.ipynb": ["https://github.com/python-streamz/streamz/issues/467"],
+    "reference/panes/HoloViews.ipynb": ["https://github.com/holoviz/panel/issues/4393"],
+    "reference/panes/IPyWidget.ipynb": ["https://github.com/holoviz/panel/issues/4394", "https://github.com/widgetti/ipyvolume/issues/427"],
+    "reference/panes/Matplotlib.ipynb": ["https://github.com/holoviz/panel/issues/4394"],
+    "reference/panes/Param.ipynb": ["https://github.com/holoviz/panel/issues/4393"],
+    "reference/panes/Reacton.ipynb": ["https://github.com/holoviz/panel/issues/4394"],
+    "reference/panes/Str.ipynb": ["https://github.com/holoviz/panel/issues/4396"],
+    "reference/panes/Streamz.ipynb": ["https://github.com/python-streamz/streamz/issues/467"],
+    "reference/panes/VTK.ipynb": ["https://gitlab.kitware.com/vtk/vtk/-/issues/18806"],
+    "reference/panes/VTKVolume.ipynb": ["https://gitlab.kitware.com/vtk/vtk/-/issues/18806"],
+    "reference/widgets/CrossSelector.ipynb": ["https://github.com/holoviz/panel/issues/4398"],
+    "reference/widgets/Debugger.ipynb": ["https://github.com/holoviz/panel/issues/4399"],
+    "reference/widgets/EditableIntSlider.ipynb": ["https://github.com/holoviz/panel/issues/4400"],
+    "reference/widgets/FileDownload.ipynb": ["https://github.com/holoviz/panel/issues/4401"],
+    "reference/widgets/IntRangeSlider.ipynb": ["https://github.com/holoviz/panel/issues/4402"],
+    "reference/widgets/MultiChoice.ipynb": ["https://github.com/holoviz/panel/issues/4403"],
+    "reference/widgets/RangeSlider.ipynb": ["https://github.com/holoviz/panel/issues/4402"],
+    "reference/widgets/SpeechToText.ipynb": ["https://github.com/holoviz/panel/issues/4404"],
+    "reference/widgets/Terminal.ipynb": ["https://github.com/holoviz/panel/issues/4407"],
+    "gallery/components/VuePdbInput.ipynb": ["https://github.com/holoviz/panel/issues/4417"],
+}
 
-nbs = list(EXAMPLES_DIR.glob('*/*/*.ipynb')) + list(EXAMPLES_DIR.glob('*/*.*'))
-install = nbformat.v4.new_code_cell(source=f"import piplite\nawait piplite.install([{', '.join(DEPENDENCIES)}])")
-del install['id']
+def _get_dependencies(nbpath: pathlib.Path):
+    key = str(nbpath).split("examples/")[-1]
+    dependencies = DEPENDENCIES.get(key, DEFAULT_DEPENDENCIES)
+    dependencies = [repr(d) for d in dependencies if not d in DEPENDENCY_NOT_IMPORTABLE]
+    return dependencies
 
-for nb in nbs:
-    nbpath = pathlib.Path(nb)
-    out = (PANEL_BASE / 'lite/files') / nbpath.relative_to(EXAMPLES_DIR)
-    out.parent.mkdir(parents=True, exist_ok=True)
-    if nb.suffix == '.ipynb':
-        with open(nb, encoding='utf-8') as fin:
-            nb = nbformat.read(fin, 4)
-            nb['cells'].insert(0, install)
-        with open(out, 'w', encoding='utf-8') as fout:
-            nbformat.write(nb, fout)
-    elif not nb.is_dir():
-        shutil.copyfile(nb, out)
 
-# Copy assets
-shutil.copytree(
-    EXAMPLES_DIR / 'assets',
-    PANEL_BASE / 'lite' / 'files' / 'assets',
-    dirs_exist_ok=True
-)
+def _to_source(dependencies):
+    return f"import piplite\nawait piplite.install([{', '.join(dependencies)}])"
 
-# Download sampledata
-def download():
+
+def _get_install_code_cell(nbpath: pathlib.Path):
+    dependencies = _get_dependencies(nbpath)
+    source = _to_source(dependencies)
+    install = nbformat.v4.new_code_cell(source=source)
+    del install['id']
+    return install
+
+def copy_examples():
+    nbs = list(EXAMPLES_DIR.glob('*/*/*.ipynb')) + list(EXAMPLES_DIR.glob('*/*.*'))
+    for nb in nbs:
+        nbpath = pathlib.Path(nb)
+        out = (PANEL_BASE / 'lite/files') / nbpath.relative_to(EXAMPLES_DIR)
+        out.parent.mkdir(parents=True, exist_ok=True)
+        if nb.suffix == '.ipynb':
+            with open(nb, encoding='utf-8') as fin:
+                nb = nbformat.read(fin, 4)
+                install = _get_install_code_cell(nbpath)
+                nb['cells'].insert(0, install)
+            with open(out, 'w', encoding='utf-8') as fout:
+                nbformat.write(nb, fout)
+        elif not nb.is_dir():
+            shutil.copyfile(nb, out)
+
+def copy_assets():
+    shutil.copytree(
+        EXAMPLES_DIR / 'assets',
+        PANEL_BASE / 'lite' / 'files' / 'assets',
+        dirs_exist_ok=True
+    )
+
+def download_sample_data():
     """
     Download larger data sets for various Bokeh examples.
     """
@@ -71,4 +116,7 @@ def download():
                 print(f"Re-fetching {filename!r} (checksum mismatch)")
         _download_file(s3, filename, data_dir, progress=False)
 
-download()
+if __name__=="__main__":
+    copy_examples()
+    copy_assets()
+    download_sample_data()
