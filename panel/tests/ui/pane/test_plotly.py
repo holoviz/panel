9f25c1a3bb8b285f6620c7bf1fc0aed00778eb0d
Philipp Rudiger <prudiger@anaconda.com>
2023-03-13 23:17:52 +0100

    Only run one test workflow per branch/PR concurrently (#4529)
    * Only run one action per branch/PR concurrently
    
    * Make plotly ui tests more robust

--- panel/tests/ui/pane/test_plotly.py
+++ panel/tests/ui/pane/test_plotly.py
@@ -132,11 +132,12 @@ def test_plotly_3d_plot(page, port, plotly_3d_plot):
 def test_plotly_hover_data(page, port, plotly_2d_plot):
     serve(plotly_2d_plot, port=port, threaded=True, show=False)
 
-    time.sleep(0.2)
+    time.sleep(0.5)
 
     page.goto(f"http://localhost:{port}")
 
-    page.wait_for_timeout(200)
+    plotly_plot = page.locator('.js-plotly-plot .plot-container.plotly')
+    expect(plotly_plot).to_have_count(1)
 
     # Select and hover on first point
     point = page.locator(':nth-match(.js-plotly-plot .plot-container.plotly path.point, 1)')
@@ -171,7 +172,8 @@ def test_plotly_click_data(page, port, plotly_2d_plot):
 
     page.goto(f"http://localhost:{port}")
 
-    time.sleep(0.2)
+    plotly_plot = page.locator('.js-plotly-plot .plot-container.plotly')
+    expect(plotly_plot).to_have_count(1)
 
     # Select and click on first point
     point = page.locator(':nth-match(.js-plotly-plot .plot-container.plotly path.point, 1)')
