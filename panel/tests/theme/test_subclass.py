543ce18e69fa7feea76d929999604f890df4a06e
Philipp Rudiger <prudiger@anaconda.com>
2023-03-19 22:28:23 +0100

    Fix test

--- panel/tests/theme/test_subclass.py
+++ panel/tests/theme/test_subclass.py
@@ -42,7 +42,7 @@ def test_subclassed_inheritance_server(server_document):
     assert isinstance(s1, ImportedStyleSheet)
     assert s1.url == f'{CDN_DIST}bundled/bootstrap5/css/bootstrap.min.css'
     assert isinstance(s2, ImportedStyleSheet)
-    assert s2.url == f'{CDN_DIST}/dist/bundled/theme/default.css'
+    assert s2.url == f'{CDN_DIST}bundled/theme/default.css'
     assert isinstance(s3, ImportedStyleSheet)
     assert s3.url == f'{CDN_DIST}bundled/theme/bootstrap_default.css'
     assert isinstance(s4, ImportedStyleSheet)
