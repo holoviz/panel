9e49750527440c4992b707cefc24bcb7b02b9afd
Philipp Rudiger <prudiger@anaconda.com>
2023-03-19 22:44:38 +0100

    Fix test on windows

--- panel/tests/io/test_resources.py
+++ panel/tests/io/test_resources.py
@@ -1,3 +1,5 @@
+import os
+
 from panel.io.resources import PANEL_DIR, resolve_custom_path
 from panel.widgets import Button
 
@@ -12,7 +14,7 @@ def test_resolve_custom_path_relative_level_up_input():
     assert resolve_custom_path(Button, '../reactive.py') == (PANEL_DIR / 'reactive.py')
 
 def test_resolve_custom_path_relative_input_level_up_relative_to():
-    assert str(resolve_custom_path(Button, '../reactive.py', relative=True)) == '../reactive.py'
+    assert str(resolve_custom_path(Button, '../reactive.py', relative=True)) == f'..{os.path.sep}reactive.py'
 
 def test_resolve_custom_path_abs_input():
     assert resolve_custom_path(Button, (PANEL_DIR / 'widgets' / 'button.py')) == (PANEL_DIR / 'widgets' / 'button.py')
