bc87b3487b09535eddba21aa89b6fca5a3b8651f
Philipp Rudiger <prudiger@anaconda.com>
2023-04-10 12:29:30 +0200

    Fix tickers

--- panel/models/vtk/vtkaxes.ts
+++ panel/models/vtk/vtkaxes.ts
@@ -73,19 +73,19 @@ export class VTKAxes extends Model {
   }
 
   get xlabels(): string[] {
-    return this.xticker.labels
+    return this.xticker?.labels
       ? this.xticker.labels
       : this.xticks.map((elem) => elem.toFixed(this.digits))
   }
 
   get ylabels(): string[] {
-    return this.yticker.labels
+    return this.yticker?.labels
       ? this.yticker.labels
       : this.yticks.map((elem) => elem.toFixed(this.digits))
   }
 
   get zlabels(): string[] {
-    return this.zticker.labels
+    return this.zticker?.labels
       ? this.zticker.labels
       : this.zticks.map((elem) => elem.toFixed(this.digits))
   }
