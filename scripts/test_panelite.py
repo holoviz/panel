f51464e7e8a309382b5e6bab0d47804ea4ead74a
Marc Skov Madsen <masma@orsted.dk>
2023-04-15 10:07:54 +0200

    wip

---
+++ scripts/test_panelite.py
@@ -0,0 +1,15 @@
+"""
+Before running this script make sure you have
+
+```bash
+git pull
+pip install -e . -U
+python setup.py develop
+```
+
+Then run the script to create/ update the notebooks for Panelite
+
+```bash
+python scripts/generate_panelite_content.py
+```
+"""
