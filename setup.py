c93cabcd77670bb8fa52509423825a73bb222955
Philipp Rudiger <prudiger@anaconda.com>
2023-04-24 23:34:54 +0200

    Markdown style enhancements

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ install_requires = [
     'pyviz_comms >=0.7.4',
     'xyzservices >=2021.09.1', # Bokeh dependency, but pyodide 23.0.0 does not always pick it up
     'markdown',
-    'markdown_it',
+    'markdown-it-py',
     'linkify-it-py'
     'mdit_py_plugins',
     'requests',
