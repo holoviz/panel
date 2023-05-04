703cb58dc750816b2cb01eda1c83042dafd7898c
Philipp Rudiger <prudiger@anaconda.com>
2023-05-04 11:31:26 +0200

    Remove print

--- panel/template/base.py
+++ panel/template/base.py
@@ -677,7 +677,6 @@ class BasicTemplate(BaseTemplate, ResourceComponent):
                     tmpl_name = scls.__name__.lower()
 
             css_file = os.path.basename(css)
-            print(tmpl_name, css_file)
             if (BUNDLE_DIR / tmpl_name / css_file).is_file():
                 css_files[f'base_{css_file}'] = dist_path + f'bundled/{tmpl_name}/{css_file}'
             elif isurl(css):
