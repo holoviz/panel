902c7acb327f54a7c7c5aeaf855046b365aca886
Philipp Rudiger <prudiger@anaconda.com>
2023-05-07 16:10:36 +0200

    Add support for button icons

--- panel/widgets/button.py
+++ panel/widgets/button.py
@@ -15,7 +15,9 @@ from bokeh.events import ButtonClick, MenuItemClick
 from bokeh.models import (
     Button as _BkButton, Dropdown as _BkDropdown, Toggle as _BkToggle,
 )
+from bokeh.models.ui import SVGIcon, TablerIcon
 
+from ..io.resources import CDN_DIST
 from ..links import Callback
 from .base import Widget
 
@@ -28,7 +30,7 @@ if TYPE_CHECKING:
 
 
 BUTTON_TYPES: List[str] = ['default', 'primary', 'success', 'warning', 'danger', 'light']
-
+BUTTON_STYLES: List[str] = ['solid', 'outline']
 
 class _ButtonBase(Widget):
 
@@ -37,12 +39,49 @@ class _ButtonBase(Widget):
         (blue), 'success' (green), 'info' (yellow), 'light' (light),
         or 'danger' (red).""")
 
-    _rename: ClassVar[Mapping[str, str | None]] = {'name': 'label'}
+    button_style = param.ObjectSelector(default='solid', objects=BUTTON_STYLES, doc="""
+        A button style to switch between 'solid', 'outline'.""")
+
+    _rename: ClassVar[Mapping[str, str | None]] = {'name': 'label', 'button_style': None}
+
+    _stylesheets: ClassVar[List[str]] = [f'{CDN_DIST}css/button.css']
 
     __abstract = True
 
+    def _process_param_change(self, params):
+        if 'button_style' in params or 'css_classes' in params:
+            params['css_classes'] = params.get('css_classes', self.css_classes) + [
+                params.pop('button_style', self.button_style)
+            ]
+        return super()._process_param_change(params)
 
-class _ClickButton(_ButtonBase):
+
+class IconMixin(Widget):
+
+    icon = param.String(default='', doc="""
+        An icon to render to the left of the button label. Either an SVG or an
+        icon name which is loaded from https://tabler-icons.io/.""")
+
+    icon_size = param.String(default='1em', doc="""
+        Size of the icon as a string, e.g. 12px or 1em.""")
+
+    _rename: ClassVar[Mapping[str, str | None]] = {'icon_size': None}
+
+    __abstract = True
+
+    def _process_param_change(self, params):
+        icon_size = params.pop('icon_size', self.icon_size)
+        if params.get('icon') is not None:
+            icon = params['icon']
+            if icon.lstrip().startswith('<svg'):
+                icon_model = SVGIcon(svg=icon, size=icon_size)
+            else:
+                icon_model = TablerIcon(icon_name=icon, size=icon_size)
+            params['icon'] = icon_model
+        return super()._process_param_change(params)
+
+
+class _ClickButton(_ButtonBase, IconMixin):
 
     __abstract = True
 
@@ -204,7 +243,7 @@ class Button(_ClickButton):
         return self.param.watch(callback, 'clicks', onlychanged=False)
 
 
-class Toggle(_ButtonBase):
+class Toggle(_ButtonBase, IconMixin):
     """The `Toggle` widget allows toggling a single condition between `True`/`False` states.
 
     This widget is interchangeable with the `Checkbox` widget.
