3ccb119a58e9717cc8630649356c47d814d565f9
Philipp Rudiger <prudiger@anaconda.com>
2023-05-08 13:32:35 +0200

    Template fixes

--- panel/template/vanilla/__init__.py
+++ panel/template/vanilla/__init__.py
@@ -22,7 +22,3 @@ class VanillaTemplate(BasicTemplate):
     _css = pathlib.Path(__file__).parent / 'vanilla.css'
 
     _template = pathlib.Path(__file__).parent / 'vanilla.html'
-
-    def _apply_root(self, name, model, tags):
-        if 'main' in tags:
-            model.margin = (10, 15, 10, 10)
