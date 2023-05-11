adba73763eb2bbc3bd641e55208c2261846012b4
Philipp Rudiger <prudiger@anaconda.com>
2023-05-11 16:44:27 +0200

    Stub out ServerSession methods for warmup (#4823)

--- panel/io/session.py
+++ panel/io/session.py
@@ -2,13 +2,35 @@ from __future__ import annotations
 
 import logging
 
+from typing import TYPE_CHECKING
+
 from bokeh.document import Document
 from bokeh.server.contexts import BokehSessionContext, _RequestProxy
 from bokeh.server.session import ServerSession
 from bokeh.settings import settings
 from bokeh.util.token import generate_jwt_token, generate_session_id
 
+if TYPE_CHECKING:
+    from bokeh.document.events import DocumentPatchedEvent
+    from bokeh.server.callbacks import SessionCallback
+
+
 log = logging.getLogger(__name__)
+
+class ServerSessionStub(ServerSession):
+    """
+    Stubs out ServerSession methods since the session is only used for
+    warming up the cache.
+    """
+
+    def _document_patched(self, event: DocumentPatchedEvent) -> None:
+        return
+
+    def _session_callback_added(self, event: SessionCallback):
+        return
+
+    def _session_callback_removed(self, event):
+        return
 
 def generate_session(application):
     secret_key = settings.secret_key_bytes()
@@ -39,6 +61,6 @@ def generate_session(application):
     # to schedule them on the event loop
     callbacks = doc.callbacks._session_callbacks
     doc.callbacks._session_callbacks = set()
-    session = ServerSession(session_id, doc, io_loop=None, token=token)
+    session = ServerSessionStub(session_id, doc, io_loop=None, token=token)
     doc.callbacks._session_callbacks = callbacks
     return session
