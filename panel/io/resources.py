e6f9fd6ed70262872675a0c6d92a1d6a93c0bc8d
Philipp Rudiger <prudiger@anaconda.com>
2023-05-25 16:31:26 +0200

    Fix global loading spinner

--- panel/io/resources.py
+++ panel/io/resources.py
@@ -175,7 +175,7 @@ def loading_css():
         svg = f.read().replace('\n', '').format(color=config.loading_color)
     b64 = b64encode(svg.encode('utf-8')).decode('utf-8')
     return textwrap.dedent(f"""
-    :host(.{LOADING_INDICATOR_CSS_CLASS}.pn-{config.loading_spinner}):before, .pn-loading.{config.loading_spinner}:before {{
+    :host(.{LOADING_INDICATOR_CSS_CLASS}.pn-{config.loading_spinner}):before, .pn-loading.pn-{config.loading_spinner}:before {{
       background-image: url("data:image/svg+xml;base64,{b64}");
       background-size: auto calc(min(50%, {config.loading_max_height}px));
     }}""")
