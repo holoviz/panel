2cf99067119303e5290ebcd90b2c41c987a757a0
Philipp Rudiger <prudiger@anaconda.com>
2023-05-31 08:45:10 +0200

    Apply suggestions from code review

--- panel/theme/base.py
+++ panel/theme/base.py
@@ -185,7 +185,8 @@ class Design(param.Parameterized, ResourceComponent):
         from ..io.resources import (
             CDN_DIST, component_resource_path, resolve_custom_path,
         )
-        modifiers, child_modifiers = cls._resolve_modifiers(type(viewable), type(theme))
+        theme_type  = type(theme) if isinstance(theme, Theme) else theme 
+        modifiers, child_modifiers = cls._resolve_modifiers(type(viewable), them_type)
         modifiers = dict(modifiers)
         if 'stylesheets' in modifiers:
             if isolated:
