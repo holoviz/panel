62f53da6cac51a6e28b94068cdd02bdb19600961
Philipp Rudiger <prudiger@anaconda.com>
2023-06-05 19:17:24 +0200

    Pin markdown-it-py max version

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ install_requires = [
     'pyviz_comms >=0.7.4',
     'xyzservices >=2021.09.1', # Bokeh dependency, but pyodide 23.0.0 does not always pick it up
     'markdown',
-    'markdown-it-py',
+    'markdown-it-py<3',
     'linkify-it-py',
     'mdit-py-plugins',
     'requests',
