1fcf19163dc86942062ae34e8b860cba4e819eb7
Philipp Rudiger <prudiger@anaconda.com>
2023-06-21 16:23:29 +0200

    Remove markdown-it-py pins

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ install_requires = [
     'pyviz_comms >=0.7.4',
     'xyzservices >=2021.09.1', # Bokeh dependency, but pyodide 23.0.0 does not always pick it up
     'markdown',
-    'markdown-it-py<3',
+    'markdown-it-py',
     'linkify-it-py',
     'mdit-py-plugins',
     'requests',
