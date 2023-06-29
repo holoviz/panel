30fe9c58f4f07e416c92e762aaf9df2f71383120
Andrew Huang <ahuang@anaconda.com>
2023-06-29 11:15:53 -0400

    Add recursive search for chatlog

--- panel/widgets/chatbox.py
+++ panel/widgets/chatbox.py
@@ -357,9 +357,42 @@ class ChatBox(CompositeWidget):
         self._scroll_button.on_click(self._trigger_scroll_callback)
         self._hidden_scroll_trigger.jscallback(
             value="""
-                const outerColumn = document.querySelector(".bk-Column");
-                const column = outerColumn.shadowRoot.querySelector(".bk-Column");
+                function $$$(selector, rootNode=document.body) {
+                    const arr = []
 
+                    const traverser = node => {
+                        // 1. decline all nodes that are not elements
+                        if(node.nodeType !== Node.ELEMENT_NODE) {
+                            return
+                        }
+
+                        // 2. add the node to the array, if it matches the selector
+                        if(node.matches(selector)) {
+                            arr.push(node)
+                        }
+
+                        // 3. loop through the children
+                        const children = node.children
+                        if (children.length) {
+                            for(const child of children) {
+                                traverser(child)
+                            }
+                        }
+
+                        // 4. check for shadow DOM, and loop through it's children
+                        const shadowRoot = node.shadowRoot
+                        if (shadowRoot) {
+                            const shadowChildren = shadowRoot.children
+                            for(const shadowChild of shadowChildren) {
+                                traverser(shadowChild)
+                            }
+                        }
+                    }
+
+                    traverser(rootNode)
+                    return arr
+                }
+                const column = $$$(".chat-log")[0];
                 if (ascending) {
                     column.scrollTop = column.scrollHeight;
                 } else {
