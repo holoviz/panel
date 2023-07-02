83d5753253d129498908f9751de5d098651afdcd
Philipp Rudiger <prudiger@anaconda.com>
2023-07-02 13:08:14 +0200

    Add support for LoadingSpinner label (#5194)

--- panel/widgets/indicators.py
+++ panel/widgets/indicators.py
@@ -152,16 +152,17 @@ class LoadingSpinner(BooleanIndicator):
         'primary', 'secondary', 'success', 'info', 'danger', 'warning',
         'light', 'dark'])
 
-    height = param.Integer(default=125, doc="""
-        height of the circle.""")
-
-    width = param.Integer(default=125, doc="""
-        Width of the circle.""")
+    size = param.Integer(default=125, doc="""
+        Size of the spinner in pixels.""")
 
     value = param.Boolean(default=False, doc="""
         Whether the indicator is active or not.""")
 
-    _source_transforms: ClassVar[Mapping[str, str | None]] = {'value': None, 'color': None, 'bgcolor': None}
+    _rename = {'name': 'text'}
+
+    _source_transforms: ClassVar[Mapping[str, str | None]] = {
+        'value': None, 'color': None, 'bgcolor': None, 'size': None
+    }
 
     _stylesheets: ClassVar[List[str]] = [f'{CDN_DIST}css/loadingspinner.css']
 
@@ -169,9 +170,20 @@ class LoadingSpinner(BooleanIndicator):
 
     def _process_param_change(self, msg):
         msg = super()._process_param_change(msg)
+        if 'text' in msg:
+            msg['text'] = escape(f'<span><b>{msg["text"]}</b></span>')
         value = msg.pop('value', None)
         color = msg.pop('color', None)
         bgcolor = msg.pop('bgcolor', None)
+        if msg.get('sizing_mode') == 'fixed':
+            msg['sizing_mode'] = None
+        if 'size' in msg or 'height' in msg:
+            if 'width' in msg and msg['width'] == msg.get('height'):
+                del msg['width']
+            size = int(min(msg.pop('height', float('inf')), msg.pop('size', 125)))
+            msg['stylesheets'] = ([f':host {{ --loading-spinner-size: {size}px; }}'] +
+                                  msg.get('stylesheets', []))
+            msg['min_width'] = msg['min_height'] = size
         if value is None and not (color or bgcolor):
             return msg
         color_cls = f'{self.color}-{self.bgcolor}'
@@ -1062,7 +1074,7 @@ class Trend(SyncableData, Indicator):
     _manual_params: ClassVar[List[str]] = ['data']
 
     _rename: ClassVar[Mapping[str, str | None]] = {
-        'data': None, 'name': 'name', 'selection': None
+        'data': None, 'name': 'title', 'selection': None
     }
 
     _widget_type: ClassVar[Type[Model]] = _BkTrendIndicator
