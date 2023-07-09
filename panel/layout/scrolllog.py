bc6f44f81b689c43058e02d6d59b8d37579c53c4
Andrew Huang <ahuang@anaconda.com>
2023-07-09 14:49:24 -0400

    Attempt to convert to Typescript

--- panel/layout/scrolllog.py
+++ panel/layout/scrolllog.py
@@ -1,99 +1,28 @@
 from __future__ import annotations
 
-from typing import ClassVar, List
+from typing import (
+    TYPE_CHECKING, ClassVar, List, Type,
+)
 
-import param
+from ..io.resources import CDN_DIST
+from ..layout.base import Column
+from ..models import ScrollLog as BkScrollLog
 
-from ..layout.base import ListLike
-from ..reactive import ReactiveHTML
+if TYPE_CHECKING:
+    from bokeh.model import Model
 
 
-class ScrollLog(ListLike, ReactiveHTML):
-    objects = param.List(
-        default=[],
-        doc="""
-        The list of child objects that make up the layout.""",
-        precedence=-1,
-    )
-
-    height = param.Integer(
-        default=250,
-        doc="""
-        The height of the scrollable area in pixels.""",
-    )
-
-    _stylesheets: ClassVar[List[str]] = [
-        """
-        .scroll-log {
-            overflow-y: scroll;
-        }
-
-        .scroll-down-arrow {
-            /* For location */
-            position: absolute;
-            bottom: 0%;
-            left: 50%;
-            transform: translate(0%, 0%);
-            /* For icon */
-            cursor: pointer;
-            visibility: hidden;
-            font-size: 18px;
-            border-radius: 50%;
-            background-color: rgba(0, 0, 0, 0.25);
-            color: white;
-            width: 36px;
-            height: 36px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            /* For animation */
-            opacity: 0;
-            transition: visibility 0s, opacity 0.2s ease-in-out;
-        }
-
-        .visible {
-            visibility: visible;
-            opacity: 1;
-        }
+class ScrollLog(Column):
     """
-    ]
+    A `ScrollLog` layout allows arranging multiple panel objects in a
+    scrollable, vertical container with a header bar.
 
-    _template = """
-        <div id="scrollLog" class="scroll-log" style="height: {{ height }}px; min-width: 75px">
-            <div id="scrollDownArrow" class="scroll-down-arrow">⬇</div>
-            {% for obj in objects %}
-            <div class="scroll-log-item" data-id="{{ id(obj) }}">
-                <div id="content" class="scroll-log-content">${obj}</div>
-            </div>
-            {% endfor %}
-        </div>
+    Reference: https://panel.holoviz.org/reference/layouts/ScrollLog.html
     """
 
-    _scripts = {
-        "render": """
-            scrollDownArrow.addEventListener("click", () => {
-                self.scroll_to_bottom();
-            });
+    _bokeh_model: ClassVar[Type[Model]] = BkScrollLog
 
-            var scrollThreshold = 20;
-            scrollLog.addEventListener("scroll", () => {
-                var scrollDistanceFromBottom = (
-                    scrollLog.scrollHeight - scrollLog.scrollTop - scrollLog.clientHeight
-                );
-                if (scrollDistanceFromBottom < scrollThreshold) {
-                    scrollDownArrow.classList.remove("visible");
-                } else {
-                    scrollDownArrow.classList.add("visible");
-                }
-            });
-        """,
-        "after_layout": """
-            self.scroll_to_bottom();
-        """,
-        "scroll_to_bottom": """
-            scrollLog.scrollTop = scrollLog.scrollHeight;
-        """
-    }
+    _stylesheets: ClassVar[List[str]] = [f"{CDN_DIST}/layout/scrolllog.css"]
 
     def __init__(self, *objects, **params):
         if "sizing_mode" not in params:
---
+++ panel/models/scroll_log.ts
@@ -0,0 +1,67 @@
+import { Column, ColumnView } from "@bokehjs/models/layouts/column";
+import * as p from "@bokehjs/core/properties";
+
+export class ScrollLogView extends ColumnView {
+  model: ScrollLog;
+  scroll_down_arrow: HTMLElement;
+
+  connect_signals(): void {
+    super.connect_signals();
+
+    const { autoscroll } = this.model.properties;
+
+    if (autoscroll) {
+      this.on_change(this.model.properties.children, () => {
+        this.scroll_to_bottom();
+      });
+    }
+
+    this.el.addEventListener("scroll", () => {
+      const scrollThreshold = 20;
+      const scrollDistanceFromBottom =
+        this.el.scrollHeight - this.el.scrollTop - this.el.clientHeight;
+
+      if (scrollDistanceFromBottom < scrollThreshold) {
+        this.scroll_down_arrow.classList.remove("visible");
+      } else {
+        this.scroll_down_arrow.classList.add("visible");
+      }
+    });
+
+    this.scroll_down_arrow.addEventListener("click", () => {
+      this.scroll_to_bottom();
+    });
+  }
+
+  scroll_to_bottom(): void {
+    this.el.scrollTop = this.el.scrollHeight;
+  }
+}
+
+export namespace ScrollLog {
+  export type Attrs = p.AttrsOf<Props>;
+
+  export type Props = Column.Props & {
+    autoscroll: p.Property<boolean>;
+  };
+}
+
+export interface ScrollLog extends ScrollLog.Attrs {}
+
+export class ScrollLog extends Column {
+  properties: ScrollLog.Props;
+
+  constructor(attrs?: Partial<ScrollLog.Attrs>) {
+    super(attrs);
+  }
+
+  static __module__ = "panel.models.layout";
+
+  static {
+    this.prototype.default_view = ScrollLogView;
+
+    this.define<ScrollLog.Props>(({ Boolean }) => ({
+      autoscroll: [Boolean, true],
+    }));
+  }
+}
