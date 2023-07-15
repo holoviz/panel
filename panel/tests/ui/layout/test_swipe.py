64fb33c2dd4b79094715fc62f4e37f8e4e3ac9ac
Andrew Huang <ahuang@anaconda.com>
2023-07-14 22:40:07 -0700

    Undo swipe

--- panel/tests/ui/layout/test_swipe.py
+++ panel/tests/ui/layout/test_swipe.py
@@ -3,56 +3,121 @@ import time
 import pytest
 
 from panel.io.server import serve
-from panel.layout import Fade, Spacer
+from panel.layout import Spacer, Swipe
 
 pytestmark = pytest.mark.ui
 
-def test_fade_layout(page, port):
+def test_swipe_fixed_width(page, port):
+
     before = Spacer(styles={'background': "red"}, height=400, width=800)
     after = Spacer(styles={'background': "green"}, height=400, width=800)
-    fade = Fade(before, after)
+    swipe = Swipe(
+        before,
+        after,
+    )
 
-    serve(fade, port=port, threaded=True, show=False)
+    serve(swipe, port=port, threaded=True, show=False)
 
     time.sleep(0.5)
 
     page.goto(f"http://localhost:{port}")
 
-    container = page.locator("#container")
+    bbox = page.locator(".swipe-container").bounding_box()
 
-    assert container.evaluate("el => el.style.position") == "relative"
-    assert container.evaluate("el => el.style.width") == "800px"
-    assert container.evaluate("el => el.style.height") == "400px"
+    assert bbox['width'] == 800
+    assert bbox['height'] == 400
 
-    before_el = page.locator("#before-inner")
-    assert before_el.evaluate("el => el.style.background") == "red"
-    before_bbox = before_el.bounding_box()
-    assert before_bbox["width"] == 800
-    assert before_bbox["height"] == 400
+    before_el = page.locator('.swipe-container .outer').first.locator('.inner div')
+    assert before_el.evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('background-color')""") == 'rgb(255, 0, 0)'
+    bbox_before = before_el.bounding_box()
+    assert bbox_before['width'] == 800
+    assert bbox_before['height'] == 400
 
-    after_el = page.locator("#after-inner")
-    assert after_el.evaluate("el => el.style.background") == "green"
-    after_bbox = after_el.bounding_box()
-    assert after_bbox["width"] == 800
-    assert after_bbox["height"] == 400
+    after_el = page.locator('.swipe-container .outer').nth(1).locator('.inner div')
+    assert after_el.evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('background-color')""") == 'rgb(0, 128, 0)'
+    bbox_after = after_el.bounding_box()
+    assert bbox_after['width'] == 800
+    assert bbox_after['height'] == 400
 
-    slider = page.locator("#slider")
+    slider = page.locator('.swipe-container .slider')
     slider_bbox = slider.bounding_box()
-    assert slider_bbox["width"] == fade.slider_width
-    assert slider_bbox["background"] == fade.slider_color
+    assert slider_bbox['x'] == (bbox['width']/2. + 3)
+    print(slider_bbox['x'] + (bbox['width']/4.))
+    slider.drag_to(slider, target_position={'x': (bbox['width']/4.), 'y': 0}, force=True)
 
-    slider.drag_to(slider, target_position={"x": 100, "y": 0}, force=True)
-
-    assert after_el.evaluate("el => el.style.opacity") == "1"
-    assert slider.evaluate("el => el.style.marginLeft") == f"calc({fade.value}% - {fade.slider_width / 2}px)"
-
-    time.sleep(0.2)
-
-    assert fade.value == 100
-
-    fade.value = 50
+    assert page.locator('.swipe-container .outer').nth(0).evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('clip-path')""") == 'polygon(0% 0%, calc(75% + 5px) 0%, calc(75% + 5px) 100%, 0% 100%)'
+    assert page.locator('.swipe-container .outer').nth(1).evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('clip-path')""") == 'polygon(calc(75% + 5px) 0%, 100% 0%, 100% 100%, calc(75% + 5px) 100%)'
 
     time.sleep(0.2)
 
-    assert after_el.evaluate("el => el.style.opacity") == "0.5"
-    assert slider.evaluate("el => el.style.marginLeft") == f"calc({fade.value}% - {fade.slider_width / 2}px)"
+    assert swipe.value == 75
+
+    swipe.value = 25
+
+    time.sleep(0.2)
+
+    assert page.locator('.swipe-container .outer').nth(0).evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('clip-path')""") == 'polygon(0% 0%, calc(25% + 5px) 0%, calc(25% + 5px) 100%, 0% 100%)'
+    assert page.locator('.swipe-container .outer').nth(1).evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('clip-path')""") == 'polygon(calc(25% + 5px) 0%, 100% 0%, 100% 100%, calc(25% + 5px) 100%)'
+
+
+def test_swipe_stretch_width(page, port):
+    before = Spacer(styles={'background': "red"}, height=400, sizing_mode='stretch_width')
+    after = Spacer(styles={'background': "green"}, height=400, sizing_mode='stretch_width')
+    swipe = Swipe(
+        before,
+        after,
+        sizing_mode='stretch_width'
+    )
+
+    serve(swipe, port=port, threaded=True, show=False)
+
+    time.sleep(0.5)
+
+    page.goto(f"http://localhost:{port}")
+
+    bbox = page.locator(".swipe-container").bounding_box()
+    assert bbox['height'] == 400
+
+    before_el = page.locator('.swipe-container .outer').first.locator('.inner div')
+    assert before_el.evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('background-color')""") == 'rgb(255, 0, 0)'
+    bbox_before = before_el.bounding_box()
+    assert bbox_before['width'] == bbox['width']
+    assert bbox_before['height'] == 400
+
+    after_el = page.locator('.swipe-container .outer').nth(1).locator('.inner div')
+    assert after_el.evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('background-color')""") == 'rgb(0, 128, 0)'
+    bbox_after = after_el.bounding_box()
+    assert bbox_after['width'] == bbox['width']
+    assert bbox_after['height'] == 400
+
+    slider = page.locator('.swipe-container .slider')
+    slider_bbox = slider.bounding_box()
+    assert slider_bbox['x'] == (bbox['width']/2. + 3)
+    print(slider_bbox['x'] + (bbox['width']/4.))
+    slider.drag_to(slider, target_position={'x': (bbox['width']/4.), 'y': 0}, force=True)
+
+    assert page.locator('.swipe-container .outer').nth(0).evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('clip-path')""") == 'polygon(0% 0%, calc(75% + 5px) 0%, calc(75% + 5px) 100%, 0% 100%)'
+    assert page.locator('.swipe-container .outer').nth(1).evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('clip-path')""") == 'polygon(calc(75% + 5px) 0%, 100% 0%, 100% 100%, calc(75% + 5px) 100%)'
+
+    time.sleep(0.2)
+
+    assert swipe.value == 75
+
+    swipe.value = 25
+
+    time.sleep(0.2)
+
+    assert page.locator('.swipe-container .outer').nth(0).evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('clip-path')""") == 'polygon(0% 0%, calc(25% + 5px) 0%, calc(25% + 5px) 100%, 0% 100%)'
+    assert page.locator('.swipe-container .outer').nth(1).evaluate("""(element) =>
+        window.getComputedStyle(element).getPropertyValue('clip-path')""") == 'polygon(calc(25% + 5px) 0%, 100% 0%, 100% 100%, calc(25% + 5px) 100%)'
