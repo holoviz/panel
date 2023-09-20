db40dd73294529fbe5f6b08d727aabe2bcee8ab6
Andrew Huang <ahuang@anaconda.com>
2023-09-20 12:29:39 -0700

    Upload draft

---
+++ doc/tutorials/11_Deep_Linking/basic_widget.py
@@ -0,0 +1,8 @@
+import panel as pn
+
+slider = pn.widgets.FloatSlider(name='Slider', start=0, end=10)
+
+if pn.state.location:
+    pn.state.location.sync(slider, {'value': 'slider_value'})
+
+slider.servable()
