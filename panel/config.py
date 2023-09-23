4a8d6200e17408cfb82df16c98e2f769173c442c
Philipp Rudiger <prudiger@anaconda.com>
2023-09-23 19:41:54 +0200

    Fix reactive._render_mimebundle_

--- panel/config.py
+++ panel/config.py
@@ -21,6 +21,9 @@ from bokeh.core.has_props import _default_resolver
 from bokeh.document import Document
 from bokeh.model import Model
 from bokeh.settings import settings as bk_settings
+from param.depends import (
+    register_display_accessor, unregister_display_accessor,
+)
 from pyviz_comms import (
     JupyterCommManager as _JupyterCommManager, extension as _pyviz_extension,
 )
@@ -773,20 +776,17 @@ class panel_extension(_pyviz_extension):
         except Exception:
             return
 
-        from param.depends import (
-            register_display_accessor, unregister_display_accessor,
-        )
+        from .io.notebook import load_notebook, mime_renderer
 
-        from .pane import panel
         try:
             unregister_display_accessor('_ipython_display_')
         except KeyError:
             pass
+
         try:
-            register_display_accessor('_repr_mimebundle_', lambda obj: panel(obj)._repr_mimebundle_)
+            register_display_accessor('_repr_mimebundle_', mime_renderer)
         except Exception:
             pass
-        from .io.notebook import load_notebook
 
         self._detect_comms(params)
 
