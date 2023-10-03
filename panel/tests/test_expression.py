c143f340ac6cec920a0bdb842503f51961e24604
Philipp Rudiger <prudiger@anaconda.com>
2023-10-03 15:27:41 +0200

    Update rx tests

--- panel/tests/test_expression.py
+++ panel/tests/test_expression.py
@@ -52,18 +52,18 @@ def test_reactive_widget_order():
 
 def test_reactive_dataframe_method_chain(dataframe):
     dfi = rx(dataframe).groupby('str')[['float']].mean().reset_index()
-    pd.testing.assert_frame_equal(dfi.rx.resolve(), dataframe.groupby('str')[['float']].mean().reset_index())
+    pd.testing.assert_frame_equal(dfi.rx.value, dataframe.groupby('str')[['float']].mean().reset_index())
 
 def test_reactive_dataframe_attribute_chain(dataframe):
-    array = rx(dataframe).str.values.rx.resolve()
+    array = rx(dataframe).str.values.rx.value
     np.testing.assert_array_equal(array, dataframe.str.values)
 
 def test_reactive_dataframe_param_value_method_chain(dataframe):
     P = Parameters(string='str')
     dfi = rx(dataframe).groupby(P.param.string)[['float']].mean().reset_index()
-    pd.testing.assert_frame_equal(dfi.rx.resolve(), dataframe.groupby('str')[['float']].mean().reset_index())
+    pd.testing.assert_frame_equal(dfi.rx.value, dataframe.groupby('str')[['float']].mean().reset_index())
     P.string = 'int'
-    pd.testing.assert_frame_equal(dfi.rx.resolve(), dataframe.groupby('int')[['float']].mean().reset_index())
+    pd.testing.assert_frame_equal(dfi.rx.value, dataframe.groupby('int')[['float']].mean().reset_index())
 
 def test_reactive_layout_default_with_widgets():
     w = IntSlider(value=2, start=1, end=5)
