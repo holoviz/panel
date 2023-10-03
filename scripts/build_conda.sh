5e15b369a673f1f0f02fe3edd2b3acb7ca93bbe4
Simon Høxbro Hansen <simon.hansen@me.com>
2023-10-03 18:20:39 +0200

    Try to not use pyctdev for building conda package
    [skip ci]

---
+++ scripts/build_conda.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+git status
+
+SETUPTOOLS_ENABLE_FEATURES="legacy-editable" python -m build .
+
+export VERSION="$(echo "$(ls dist/*.whl)" | cut -d- -f2)"
+conda build conda.recipe/ --no-test --no-anaconda-upload --no-verify
