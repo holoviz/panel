d129b03622c560ac97fbca14a9be61305117617e
Simon Høxbro Hansen <simon.hansen@me.com>
2023-10-04 08:17:01 +0200

    Update name and confirm build is not dirty
    [skip ci]

--- scripts/build_conda.sh
+++ scripts/build_conda.sh
@@ -6,5 +6,7 @@ git status
 
 SETUPTOOLS_ENABLE_FEATURES="legacy-editable" python -m build .
 
+git diff --exit-code
+
 export VERSION="$(echo "$(ls dist/*.whl)" | cut -d- -f2)"
 conda build conda.recipe/ --no-test --no-anaconda-upload --no-verify
