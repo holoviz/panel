dfbcb65c3dd6894d4415399e0681348e04c1a765
Andrew <15331990+ahuang11@users.noreply.github.com>
2023-10-10 14:39:49 -0700

    Update panel/models/column.ts

--- panel/models/column.ts
+++ panel/models/column.ts
@@ -49,7 +49,6 @@ export class ColumnView extends BkColumnView {
   toggle_scroll_button(): void {
     const threshold = this.model.scroll_button_threshold
     const exceeds_threshold = this.distance_from_latest >= threshold
-    // if scroll_down_button_el is not null
     if (this.scroll_down_button_el) {
       this.scroll_down_button_el.classList.toggle(
         "visible", threshold !== 0 && exceeds_threshold
