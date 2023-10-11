b5d7327038b8a22419439d661022bd4f93875a3c
Philipp Rudiger <prudiger@anaconda.com>
2023-10-11 17:59:03 +0200

    Small value_throttled fix

--- panel/links.py
+++ panel/links.py
@@ -707,8 +707,7 @@ class JSLinkCallbackGenerator(JSCallbackGenerator):
                     value = msg[tgt_spec]
             else:
                 value = getattr(src_model, src_spec)
-            if value and hasattr(tgt_model, tgt_spec):
-                if tgt_spec != 'value_throttled':
+            if value and tgt_spec != 'value_throttled' and hasattr(tgt_model, tgt_spec):
                     setattr(tgt_model, tgt_spec, value)
         if tgt_model is None and not link.code:
             raise ValueError('Model could not be resolved on target '
