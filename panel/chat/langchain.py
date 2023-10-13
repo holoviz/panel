09eb706040e71e0f0555fac2021d9de3a69cb77e
Andrew Huang <ahuang@anaconda.com>
2023-10-13 16:02:57 -0700

    Touch up langchain and add docs

--- panel/chat/langchain.py
+++ panel/chat/langchain.py
@@ -19,8 +19,10 @@ from panel.chat import ChatFeed, ChatInterface
 
 class PanelCallbackHandler(BaseCallbackHandler):
     """
-    Inherits from Langchain's `BaseCallbackHandler` to allow streaming
-    to a Panel `ChatFeed` or `ChatInterface`.
+    The Langchain `PanelCallbackHandler` itself is not a widget or pane, but is useful for rendering
+    and streaming output from Langchain Tools, Agents, and Chains as `ChatEntry` objects.
+
+    Reference: https://panel.holoviz.org/reference/chat/PanelCallbackHandler.html
 
     :Example:
 
@@ -30,6 +32,7 @@ class PanelCallbackHandler(BaseCallbackHandler):
     >>> chain = ConversationChain(llm=llm, memory=memory)
 
     """
+
     def __init__(
         self,
         instance: ChatFeed | ChatInterface,
@@ -45,21 +48,25 @@ class PanelCallbackHandler(BaseCallbackHandler):
         self._active_user = user
         self._active_avatar = avatar
         self._disabled_state = self.instance.disabled
+        self._is_streaming = None
 
         self._input_user = user  # original user
         self._input_avatar = avatar
 
-    def _revert_settings(self):
-        self._active_user = self._input_user
-        self._active_avatar = self._input_avatar
-        self._entry = None
+    def _update_active(self, avatar: str, label: str):
+        """
+        Prevent duplicate labels from being appended to the same user.
+        """
+        # not a typo; Langchain passes a string :/
+        if label == "None":
+            return
 
-    def _append_label(self, label: str):
         if f"- {label}" not in self._active_user:
             self._active_user = f"{self._active_user} - {label}"
 
     def on_llm_start(self, serialized: Dict[str, Any], *args, **kwargs):
         model = kwargs.get("invocation_params", {}).get("model_name", "")
+        self._is_streaming = serialized["kwargs"].get("streaming")
         entries = self.instance.value
         if entries[-1].user != self._active_user:
             self._entry = None
@@ -77,37 +84,43 @@ class PanelCallbackHandler(BaseCallbackHandler):
         return super().on_llm_new_token(token, **kwargs)
 
     def on_llm_end(self, response: LLMResult, *args, **kwargs):
+        if (
+            not self._is_streaming
+        ):  # on_llm_new_token does not get called if not streaming
+            self._entry = self.instance.stream(
+                response.generations[0][0].text,
+                user=self._active_user,
+                avatar=self._active_avatar,
+                entry=self._entry,
+            )
+        if self._active_user != self._input_user:
+            self._active_user = self._input_user
+            self._active_avatar = self._input_avatar
+            self._entry = None
         return super().on_llm_end(response, *args, **kwargs)
 
     def on_llm_error(self, error: Union[Exception, KeyboardInterrupt], *args, **kwargs):
         return super().on_llm_error(error, *args, **kwargs)
 
     def on_agent_action(self, action: AgentAction, *args, **kwargs: Any) -> Any:
-        if action.tool != "None":
-            self._active_avatar = "🕵️"
-            self._append_label(action.tool)
+        self._update_active("🛠️", action.tool)
         return super().on_agent_action(action, *args, **kwargs)
 
     def on_agent_finish(self, finish: AgentFinish, *args, **kwargs: Any) -> Any:
-        self._revert_settings()
         return super().on_agent_finish(finish, *args, **kwargs)
 
     def on_tool_start(
         self, serialized: Dict[str, Any], input_str: str, *args, **kwargs
     ):
-        if serialized["name"] != "None":
-            self._active_avatar = "🛠️"
-            self._append_label(serialized["name"])
+        self._update_active("🛠️", serialized["name"])
         return super().on_tool_start(serialized, input_str, *args, **kwargs)
 
     def on_tool_end(self, output, *args, **kwargs):
-        self._revert_settings()
         return super().on_tool_end(output, *args, **kwargs)
 
     def on_tool_error(
         self, error: Union[Exception, KeyboardInterrupt], *args, **kwargs
     ):
-        self._revert_settings()
         return super().on_tool_error(error, *args, **kwargs)
 
     def on_chain_start(
@@ -121,16 +134,15 @@ class PanelCallbackHandler(BaseCallbackHandler):
         return super().on_chain_end(outputs, *args, **kwargs)
 
     def on_retriever_error(
-        self,
-        error: Union[Exception, KeyboardInterrupt],
-        **kwargs: Any,
+        self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
     ) -> Any:
         """Run when Retriever errors."""
         return super().on_retriever_error(error, **kwargs)
 
-    def on_retriever_end(
-        self,
-        **kwargs: Any,
-    ) -> Any:
+    def on_retriever_end(self, **kwargs: Any) -> Any:
         """Run when Retriever ends running."""
         return super().on_retriever_end(**kwargs)
+
+    def on_text(self, text: str, **kwargs: Any):
+        """Run when text is received."""
+        return super().on_text(text, **kwargs)
