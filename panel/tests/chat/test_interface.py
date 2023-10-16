86988deaf3d7db43284f3205c090abcdec7ce682
Andrew Huang <ahuang@anaconda.com>
2023-10-16 09:54:55 -0700

    Remove dup

--- panel/tests/chat/test_interface.py
+++ panel/tests/chat/test_interface.py
@@ -151,11 +151,6 @@ class TestChatInterface:
         chat_interface.width = 800
         assert not chat_interface.show_button_name
 
-    def test_show_button_name_set(self, chat_interface):
-        chat_interface.show_button_name = False
-        chat_interface.width = 800
-        assert not chat_interface.show_button_name
-
 class TestChatInterfaceWidgetsSizingMode:
     def test_none(self):
         chat_interface = ChatInterface()
