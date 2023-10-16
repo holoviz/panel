806615627e65cba60ae2497b546fd57a3370500c
Simon Høxbro Hansen <simon.hansen@me.com>
2023-10-16 10:08:52 +0200

    Add examples/conftest.py

---
+++ examples/conftest.py
@@ -0,0 +1,11 @@
+collect_ignore_glob = [
+    "apps/",
+    "developer_guide/",
+    "homepage.ipynb",
+    "*VTK*",
+    "*vtk*",
+    "*Vega.ipynb",
+    "*DeckGL*",
+    "*deckgl*",
+    "reference/widgets/Terminal.ipynb",
+ ]
