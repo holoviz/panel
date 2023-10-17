8ba8dea426c08a7b6200ed90b5b3f5f74203788e
Philipp Rudiger <prudiger@anaconda.com>
2023-10-17 16:30:44 +0200

    Use expect API for indicator tests

--- panel/tests/ui/widgets/test_indicators.py
+++ panel/tests/ui/widgets/test_indicators.py
@@ -23,31 +23,31 @@ def test_plaintext_tooltip(page, value):
 
     icon = page.locator(".bk-icon")
     expect(icon).to_have_count(1)
-    tooltip = page.locator(".bk-Tooltip")
+    tooltip = page.locator(".bk-tooltip-content")
     expect(tooltip).to_have_count(0)
 
     # Hovering over the icon should show the tooltip
     page.hover(".bk-icon")
-    tooltip = page.locator(".bk-Tooltip")
+    tooltip = page.locator(".bk-tooltip-content")
     expect(tooltip).to_have_count(1)
-    assert tooltip.locator("div").first.text_content().strip() == "Test"
+    expect(tooltip).to_have_text("Test")
 
     # Removing hover should hide the tooltip
     page.hover("body")
-    tooltip = page.locator(".bk-Tooltip")
+    tooltip = page.locator(".bk-tooltip-content")
     expect(tooltip).to_have_count(0)
 
     # Clicking the icon should show the tooltip
     page.click(".bk-icon")
-    tooltip = page.locator(".bk-Tooltip")
+    tooltip = page.locator(".bk-tooltip-content")
     expect(tooltip).to_have_count(1)
 
     # Removing the hover should keep the tooltip
     page.hover("body")
-    tooltip = page.locator(".bk-Tooltip")
+    tooltip = page.locator(".bk-tooltip-content")
     expect(tooltip).to_have_count(1)
 
     # Clicking should remove the tooltip
     page.click("body")
-    tooltip = page.locator(".bk-Tooltip")
+    tooltip = page.locator(".bk-tooltip-content")
     expect(tooltip).to_have_count(0)
