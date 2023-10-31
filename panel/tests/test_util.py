44b720476b38d954d19c89f2d363cf75bc4488b9
Philipp Rudiger <prudiger@anaconda.com>
2023-10-31 10:09:27 +0100

    Bump panel.js to 1.3.1-a.2

--- panel/tests/test_util.py
+++ panel/tests/test_util.py
@@ -9,6 +9,7 @@ from bokeh.models import Div
 from panel.depends import bind
 from panel.io.notebook import render_mimebundle
 from panel.pane import PaneBase
+from panel.tests.util import mpl_available
 from panel.util import (
     abbreviated_repr, extract_dependencies, get_method_owner, parse_query,
     styler_update,
@@ -118,6 +119,7 @@ def test_parse_query_singe_quoted():
     assert expected_results == results
 
 
+@mpl_available
 def test_styler_update(dataframe):
     styler = dataframe.style.background_gradient('Reds')
     new_df = dataframe.iloc[:, :2]
