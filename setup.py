7d14ddede916a6aeaa894cf898a0acb091dd9017
Simon Høxbro Hansen <simon.hansen@me.com>
2023-11-08 12:49:18 +0100

    Try pinning param

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ except Exception:
 
 install_requires = [
     'bokeh >=3.2.0,<3.4.0',
-    'param >=2.0.0,<3.0',
+    'param ==2.0.0',
     'pyviz_comms >=2.0.0',
     'xyzservices >=2021.09.1', # Bokeh dependency, but pyodide 23.0.0 does not always pick it up
     'markdown',
@@ -229,7 +229,7 @@ extras_require['all_pip'] = sorted(set(extras_require['all']) - set(_conda_only)
 # non-python dependencies). Note that setup_requires isn't used
 # because it doesn't work well with pip.
 extras_require['build'] = [
-    'param >=2.0.0',
+    'param ==2.0.0',
     'setuptools >=42',
     'requests',
     'packaging',
