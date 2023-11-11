9dd28cbb7835e3a763d42a2d648ae1db2cd6e5e7
MarcSkovMadsen <marc.skov.madsen@gmail.com>
2023-11-11 05:02:14 +0000

    speed up pane clone

--- panel/pane/base.py
+++ panel/pane/base.py
@@ -369,6 +369,10 @@ class PaneBase(Reactive):
         """
         return None
 
+    def _should_inherit(self, p, v):
+        _p = self.param[p]
+        return not _p.readonly and v is not _p.default and (v is not None or _p.allow_None)
+
     def clone(self: T, object: Optional[Any] = None, **params) -> T:
         """
         Makes a copy of the Pane sharing the same parameters.
@@ -383,9 +387,7 @@ class PaneBase(Reactive):
         Cloned Pane object
         """
         inherited = {
-            p: v for p, v in self.param.values().items()
-            if not self.param[p].readonly and v is not self.param[p].default
-            and not (v is None and not self.param[p].allow_None)
+            p: v for p, v in self.param.values().items() if self._should_inherit(p, v)
         }
         params = dict(inherited, **params)
         old_object = params.pop('object', None)
