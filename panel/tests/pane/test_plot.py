fea796fde8d79ebaae05f68091a917ad7ea24ff0
MarcSkovMadsen <marc.skov.madsen@gmail.com>
2023-12-04 05:34:00 +0000

    fix failing test

--- panel/tests/pane/test_plot.py
+++ panel/tests/pane/test_plot.py
@@ -62,7 +62,7 @@ def test_matplotlib_pane(document, comm):
 
 @mpl_available
 def test_matplotlib_pane_svg_render(document, comm):
-    pane = pn.pane.Matplotlib(mpl_figure(), format='svg')
+    pane = pn.pane.Matplotlib(mpl_figure(), format='svg', encode=True)
     model = pane.get_root(document, comm=comm)
     assert model.text.startswith('&lt;img src=&quot;data:image/svg+xml;base64,')
 
