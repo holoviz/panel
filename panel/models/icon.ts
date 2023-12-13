bd8b4481c465a9698e7d204fd7f2b07cb68c934f
Andrew Huang <ahuang@anaconda.com>
2023-12-13 15:54:49 -0800

    Two spaces + add toggle

--- panel/models/icon.ts
+++ panel/models/icon.ts
@@ -1,71 +1,77 @@
-import { TablerIcon, TablerIconView } from "@bokehjs/models/ui/icons/tabler_icon"
-import { Control, ControlView } from '@bokehjs/models/widgets/control'
-import type {IterViews} from '@bokehjs/core/build_views'
+import { TablerIcon, TablerIconView } from "@bokehjs/models/ui/icons/tabler_icon";
+import { Control, ControlView } from '@bokehjs/models/widgets/control';
+import type { IterViews } from '@bokehjs/core/build_views';
 import * as p from "@bokehjs/core/properties";
-import {build_view} from '@bokehjs/core/build_views'
+import { build_view } from '@bokehjs/core/build_views';
 
 export class ToggleIconView extends ControlView {
-    model: ToggleIcon;
-    icon_view: TablerIconView;
+  model: ToggleIcon;
+  icon_view: TablerIconView;
 
-    public *controls() { }
+  public *controls() { }
 
-    override remove(): void {
-        this.icon_view?.remove()
-        super.remove()
-    }
+  override remove(): void {
+    this.icon_view?.remove();
+    super.remove();
+  }
 
-    override async lazy_initialize(): Promise<void> {
-        await super.lazy_initialize()
+  override async lazy_initialize(): Promise<void> {
+    await super.lazy_initialize();
 
-        const icon_model = new TablerIcon({ icon_name: this.model.icon_name })
-        this.icon_view = await build_view(icon_model, { parent: this })
-    }
+    const icon_model = new TablerIcon({ icon_name: this.model.icon_name });
+    this.icon_view = await build_view(icon_model, { parent: this });
 
-    override *children(): IterViews {
-        yield* super.children()
-        yield this.icon_view
-    }
+    this.icon_view.el.addEventListener('click', () => this.change_input());
+  }
 
-    change_input(): void {}
+  override *children(): IterViews {
+    yield* super.children();
+    yield this.icon_view;
+  }
 
-    connect_signals(): void {
-        super.connect_signals();
-    }
+  change_input(): void {
+    this.model.value = !this.model.value;
+    const icon_name = this.model.value ? `${this.model.icon_name}-filled` : this.model.icon_name;
+    this.icon_view.model.icon_name = icon_name;
+  }
 
-    render(): void {
-        super.render()
+  connect_signals(): void {
+    super.connect_signals();
+  }
 
-        this.icon_view.render()
-        this.shadow_el.appendChild(this.icon_view.el)
-    }
+  render(): void {
+    super.render();
+
+    this.icon_view.render();
+    this.shadow_el.appendChild(this.icon_view.el);
+  }
 }
 
 export namespace ToggleIcon {
-    export type Attrs = p.AttrsOf<Props>;
-    export type Props = Control.Props & {
-        icon_name: p.Property<string>;
-        value: p.Property<boolean>;
-    };
+  export type Attrs = p.AttrsOf<Props>;
+  export type Props = Control.Props & {
+    icon_name: p.Property<string>;
+    value: p.Property<boolean>;
+  };
 }
 
 export interface ToggleIcon extends ToggleIcon.Attrs { }
 
 export class ToggleIcon extends Control {
-    properties: ToggleIcon.Props;
-    declare __view_type__: ToggleIconView
-    static __module__ = "panel.models.icon";
+  properties: ToggleIcon.Props;
+  declare __view_type__: ToggleIconView
+  static __module__ = "panel.models.icon";
 
-    constructor(attrs?: Partial<ToggleIcon.Attrs>) {
-        super(attrs);
-    }
+  constructor(attrs?: Partial<ToggleIcon.Attrs>) {
+    super(attrs);
+  }
 
-    static {
-        this.prototype.default_view = ToggleIconView;
+  static {
+    this.prototype.default_view = ToggleIconView;
 
-        this.define<ToggleIcon.Props>(({ String, Boolean }) => ({
-            icon_name: [String, "heart"],
-            value: [Boolean, false],
-        }));
-    }
+    this.define<ToggleIcon.Props>(({ String, Boolean }) => ({
+      icon_name: [String, "heart"],
+      value: [Boolean, false],
+    }));
+  }
 }
