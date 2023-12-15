88b676be9dfb0fc98b7a1a21d9e7a99ead958904
Andrew <15331990+ahuang11@users.noreply.github.com>
2023-12-15 02:24:30 -0800

    Add toggle icon (#6034)

---
+++ panel/widgets/icon.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import ClassVar, List, Mapping
+
+import param
+
+from ..io.resources import CDN_DIST
+from ..models import ToggleIcon as _PnToggleIcon
+from .base import Widget
+
+
+class ToggleIcon(Widget):
+
+    active_icon = param.String(default='', doc="""
+        The name of the icon to display when toggled from
+        tabler-icons.io](https://tabler-icons.io)/""")
+
+    icon = param.String(default='heart', doc="""
+        The name of the icon to display from
+        [tabler-icons.io](https://tabler-icons.io)/""")
+
+    value = param.Boolean(default=False, doc="""
+        Whether the icon is toggled on or off.""")
+
+    _widget_type = _PnToggleIcon
+
+    _rename: ClassVar[Mapping[str, str | None]] = {'name': 'name', 'button_style': None}
+
+    _stylesheets: ClassVar[List[str]] = [f'{CDN_DIST}css/icon.css']
+
+    def __init__(self, **params):
+        super().__init__(**params)
+        if not self.icon:
+            raise ValueError('The icon parameter must not be empty.')
