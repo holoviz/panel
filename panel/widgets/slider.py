f8cbcb80f9245708a2f8dcb10e56d80bf2761603
Simon Høxbro Hansen <simon.hansen@me.com>
2024-02-23 10:58:58 +0100

    Update panel/widgets/slider.py

--- panel/widgets/slider.py
+++ panel/widgets/slider.py
@@ -774,8 +774,8 @@ class DatetimeRangeSlider(DateRangeSlider):
     ... )
     """
 
-    step = param.Number(default=600_000, doc="""
-        The step size in ms. Default is 10 min.""")
+    step = param.Number(default=60_000, doc="""
+        The step size in ms. Default is 1 min.""")
 
     _property_conversion = staticmethod(value_as_datetime)
 
