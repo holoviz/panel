0ecffaac240b1847a3ada10cb46a4f75c3c3ba7f
Philipp Rudiger <prudiger@anaconda.com>
2024-03-15 16:01:57 +0100

    Update test

--- panel/tests/test_interact.py
+++ panel/tests/test_interact.py
@@ -233,7 +233,7 @@ def test_interact_replaces_model(document, comm):
     assert new_pane._models[column.ref['id']][0] is new_div
 
     interact_pane._cleanup(column)
-    assert len(interact_pane._internal_callbacks) == 6
+    assert len(interact_pane._internal_callbacks) == 5
     # Note one of the callbacks is Viewable._set_background
     # the counter should be reduced when this function is removed.
 
