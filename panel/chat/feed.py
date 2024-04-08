73896c75963c8a010333d2be91ea7cfecbcde9aa
shwetabh-23 <shwetabhbiswas2305@gmail.com>
2024-04-08 16:19:26 +0530

    added sentence 384 for assertion

--- panel/chat/feed.py
+++ panel/chat/feed.py
@@ -34,6 +34,7 @@ if TYPE_CHECKING:
     from pyviz_comms import Comm
 
 from typing import BinaryIO
+
 PLACEHOLDER_SVG = """
     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-loader-3" width="35" height="35" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
@@ -380,6 +381,9 @@ class ChatFeed(ListPanel):
 
         user = self.callback_user
         avatar = self.callback_avatar
+
+        assert avatar == "👨"
+
         if isinstance(value, dict):
             user = value.get("user", user)
             avatar = value.get("avatar")
