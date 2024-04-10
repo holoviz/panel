1127b7d00dbc3b139a2ca2fa8bcb7859e5e100fa
Andrew Huang <ahuang@anaconda.com>
2024-04-10 13:26:59 -0700

    Add test

--- panel/tests/ui/chat/test_chat_interface_ui.py
+++ panel/tests/ui/chat/test_chat_interface_ui.py
@@ -16,3 +16,24 @@ def test_chat_interface_help(page):
     message = page.locator("p")
     message_text = message.inner_text()
     assert message_text == "This is a test help text"
+
+
+def test_chat_interface_custom_js(page):
+    chat_interface = ChatInterface()
+    chat_interface.button_properties={
+        "help": {
+            "icon": "help",
+            "js_on_click": "console.log(`Typed: '${chat_input.value}'`)",
+            "js_args": {"chat_input": chat_interface.active_widget},
+        },
+    }
+    serve_component(page, chat_interface)
+
+    chat_input = page.locator(".bk-input")
+    chat_input.fill("Hello")
+
+    with page.expect_console_message() as msg_info:
+        page.locator("button", has_text="help").click()
+        msg = msg_info.value
+
+    assert msg.args[0].json_value() == "Typed: 'Hello'"
