6f2996ed2397ce297d0eadc217489cea7dacf49c
Simon Høxbro Hansen <simon.hansen@me.com>
2024-04-20 20:50:26 +0200

    Update validate js version

--- scripts/hatch_build.py
+++ scripts/hatch_build.py
@@ -46,15 +46,16 @@ def clean_js_version(version):
 
 
 def validate_js_version(version):
-    if "post" not in version:
-        with open("./panel/package.json") as f:
-            package_json = json.load(f)
-        js_version = package_json["version"]
-        version = version.split("+")[0]
-        if any(dev in version for dev in ("a", "b", "rc")) and "-" not in js_version:
-            raise ValueError(f"panel.js dev versions ({js_version}) must " "must separate dev suffix with a dash, e.g. " "v1.0.0rc1 should be v1.0.0-rc.1.")
-        if version != "None" and version != clean_js_version(js_version):
-            raise ValueError(f"panel.js version ({js_version}) does not match " f"panel version ({version}). Cannot build release.")
+    # TODO: Double check the logic in this function
+    version = version.split(".dev")[0]
+    with open("./panel/package.json") as f:
+        package_json = json.load(f)
+    js_version = package_json["version"]
+    version = version.split("+")[0]
+    if any(dev in version for dev in ("a", "b", "rc")) and "-" not in js_version:
+        raise ValueError(f"panel.js dev versions ({js_version}) must separate dev suffix with a dash, e.g. v1.0.0rc1 should be v1.0.0-rc.1.")
+    if version != "None" and version != clean_js_version(js_version):
+        raise ValueError(f"panel.js version ({js_version}) does not match panel version ({version}). Cannot build release.")
 
 
 class BuildHook(BuildHookInterface):
