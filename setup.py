e88c80a0e2fa4573cfdcf6cb3c0d69c6fedae6df
Philipp Rudiger <prudiger@anaconda.com>
2024-04-22 09:41:09 +0200

    Fix resolution

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ except Exception:
 
 install_requires = [
     'bokeh >=3.4.0,<3.5.0',
-    'param >=2.0.0,<3.0',
+    'param >=2.1.0,<3.0',
     'pyviz_comms >=2.0.0',
     'xyzservices >=2021.09.1', # Bokeh dependency, but pyodide 23.0.0 does not always pick it up
     'markdown',
