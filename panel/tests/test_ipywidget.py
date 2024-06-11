17a39e301aafcd0d7d1b344a040e1c0e8e14a02c
MarcSkovMadsen <marc.skov.madsen@gmail.com>
2024-06-11 15:38:46 +0000

    update design spec

--- panel/tests/test_ipywidget.py
+++ panel/tests/test_ipywidget.py
@@ -5,6 +5,7 @@ The purpose is to enable Panel users to easily use Ipywidgets using familiar API
 """
 
 import param
+import pytest
 
 from traitlets import (
     Float, HasTraits, Int, Unicode,
@@ -16,22 +17,30 @@ from panel.ipywidget import to_rx, to_viewer
 
 
 class ExampleTraitlets(HasTraits):
+    """Test Traitlets"""
+
     name = Unicode("Default Name").tag(description="A string trait")
     age = Int(0).tag(description="An integer trait")
     height = Float(0.0).tag(description="A float trait")
 
 
-def test_to_viewer():
-    widget = ExampleTraitlets(name="A", age=1, height=1.1)
+@pytest.fixture
+def widget():
+    return ExampleTraitlets(name="A", age=1, height=1.1)
 
+
+def test_to_viewer(widget):
     viewer = to_viewer(widget)
 
-    assert isinstance(viewer, pn.viewable.Viewer)
     assert {"name", "age", "height"} == set(viewer.param.params)
 
     assert viewer.name == widget.name
     assert viewer.age == widget.age
-    assert viewer.height == widget.height
+    assert viewer.height == widget
+
+
+def test_to_viewer_is_synced(widget):
+    viewer = to_viewer(widget)
 
     # widget synced to widget
     widget.name = "B"
@@ -49,14 +58,27 @@ def test_to_viewer():
     assert viewer.age == widget.age
     assert viewer.height == widget.height
 
+
+def test_to_viewer_is_viewer(widget):
+    viewer = to_viewer(widget)
+
+    assert isinstance(viewer, pn.viewable.Viewer)
     component = viewer.__panel__()
     assert isinstance(component, pn.pane.IPyWidget)
     assert component.object == widget
 
 
-def test_to_viewer_parameter_list():
-    widget = ExampleTraitlets(name="A", age=1, height=1.1)
+def test_to_viewer_is_layoutable(widget):
+    viewer = to_viewer(widget)
+    component = viewer.__panel__()
 
+    assert isinstance(viewer, pn.viewable.Layoutable)
+    assert viewer.sizing_mode == component.sizing_mode == "fixed"
+    viewer.sizing_mode = "stretch_width"
+    assert viewer.sizing_mode == component.sizing_mode
+
+
+def test_to_viewer_parameter_list(widget):
     viewer = to_viewer(widget, parameters=["name", "age"])
 
     assert isinstance(viewer, pn.viewable.Viewer)
@@ -77,15 +99,29 @@ def test_to_viewer_parameter_list():
     assert viewer.name == widget.name
     assert viewer.age == widget.age
 
-def test_to_viewer_kwargs():
-    widget = ExampleTraitlets(name="A", age=1, height=1.1)
 
+def test_to_viewer_kwargs(widget):
     viewer = to_viewer(widget, parameters=["name", "age"], sizing_mode="stretch_width")
-    assert viewer.__panel__().sizing_mode=="stretch_width"
+    assert viewer.__panel__().sizing_mode == "stretch_width"
 
-def test_to_rx():
-    widget = ExampleTraitlets(name="A", age=1, height=1.1)
 
+def test_to_viewer_bases(widget):
+
+    class ExampleParameterized(param.Parameterized):
+        name = param.String("default", doc="A string parameter")
+        age = param.Integer(0, bounds=(0, 10))
+        not_trait = param.Parameter(1)
+
+    viewer = to_viewer(widget, bases=ExampleParameterized)
+    assert isinstance(viewer, ExampleParameterized)
+    assert {"name", "age", "height", "not_trait"} <= set(viewer.param.params)
+
+    assert viewer.name == widget.name
+    assert viewer.age == widget.age
+    assert viewer.height == widget.height
+
+
+def test_to_rx(widget):
     name, age, height = to_rx(widget)
     assert isinstance(name, param.rx)
     assert isinstance(age, param.rx)
@@ -112,9 +148,7 @@ def test_to_rx():
     assert height.rx.value == widget.height
 
 
-def test_to_rx_parameter_list():
-    widget = ExampleTraitlets(name="A", age=1, height=1.1)
-
+def test_to_rx_parameter_list(widget):
     name, age = to_rx(widget, parameters=["name", "age"])
     assert isinstance(name, param.rx)
     assert isinstance(age, param.rx)
