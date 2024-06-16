57ee7be1374cb0ace4fc00645423160689a5dfbb
Simon Høxbro Hansen <simon.hansen@me.com>
2024-06-16 17:06:36 +0200

    Simplify conda-build script

--- scripts/conda/build.sh
+++ scripts/conda/build.sh
@@ -4,15 +4,11 @@ set -euxo pipefail
 
 PACKAGE="panel"
 
-for file in dist/*.whl dist/*.tar.bz2; do
-    if [ -e "$file" ]; then
-        echo "dist folder already contains $(basename "$file"). Please delete it before running this script."
-        exit 1
-    fi
-done
-
 git diff --exit-code
 python -m build -w .
+
+VERSION=$(python -c "import $PACKAGE; print($PACKAGE._version.__version__)")
+export VERSION
 
 BK_CHANNEL=$(python -c "
 import bokeh
@@ -24,8 +20,6 @@ else:
     print('bokeh')
 ")
 
-VERSION=$(find dist -name "*.whl" -exec basename {} \; | cut -d- -f2)
-export VERSION
 conda build scripts/conda/recipe --no-anaconda-upload --no-verify -c $BK_CHANNEL
 
 mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.tar.bz2" dist
