873cfb8f160b1b63dde5689ca22e4bb5168fe6c3
Andrew Huang <ahuang@anaconda.com>
2024-08-06 14:35:42 -0700

    pre-commit

--- panel/models/time_picker.ts
+++ panel/models/time_picker.ts
@@ -1,8 +1,6 @@
-import { TimePicker as BkTimePicker, TimePickerView as BkTimePickerView } from "@bokehjs/models/widgets/time_picker"
+import {TimePicker as BkTimePicker, TimePickerView as BkTimePickerView} from "@bokehjs/models/widgets/time_picker"
 import type * as p from "@bokehjs/core/properties"
 import type flatpickr from "flatpickr"
-
-
 
 export class TimePickerView extends BkTimePickerView {
   declare model: TimePicker
@@ -21,17 +19,16 @@ export class TimePickerView extends BkTimePickerView {
   protected override get flatpickr_options(): flatpickr.Options.Options {
     // on init
     const options = super.flatpickr_options
-    if (options.defaultDate != null)
-      options.defaultDate = this._offset_time(options.defaultDate as string)
+    if (options.defaultDate != null) { options.defaultDate = this._offset_time(options.defaultDate as string) }
     return options
   }
 
   override connect_signals(): void {
     super.connect_signals()
 
-    const { value } = this.model.properties
+    const {value} = this.model.properties
     this.connect(value.change, () => {
-      const { value } = this.model
+      const {value} = this.model
       if (value != null && typeof value === "number") {
         // we need to handle it when programmatically changed thru Python, e.g.
         // time_picker.value = "4:08" or time_picker.value = "datetime.time(4, 8)"
