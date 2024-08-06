a6e2df79985dec141ad0d2669d1ed94720642339
Andrew Huang <ahuang@anaconda.com>
2024-08-06 14:33:36 -0700

    cover all cases

--- panel/models/time_picker.ts
+++ panel/models/time_picker.ts
@@ -1,52 +1,70 @@
-import {TimePicker as BkTimePicker, TimePickerView as BkTimePickerView} from "@bokehjs/models/widgets/time_picker"
+import { TimePicker as BkTimePicker, TimePickerView as BkTimePickerView } from "@bokehjs/models/widgets/time_picker"
 import type * as p from "@bokehjs/core/properties"
+import type flatpickr from "flatpickr"
 
 
 
 export class TimePickerView extends BkTimePickerView {
-    declare model: TimePicker
+  declare model: TimePicker
 
-    // Override the render method or any method where time rendering is done
-    override render(): void {
-        // Fix time zone difference; add back 8 hours
-        const date = this.model.value ? new Date(this.model.value) : new Date();
-        date.setHours(date.getHours() + 8);
-
-        console.log(date);
-
-        // get type of date
-        console.log(this.model.value)
-        console.log("Type of Date:", typeof this.model.value);
-
-        console.log("Converted Date:", date.toISOString());
-        console.log("Converted Time:", date.getTime());
-        this.model.value = 72000000
-
-        super.render();
-    }
+  private _offset_time(value: string | number): number {
+    const baseDate = new Date(value)
+    const timeZoneOffset = baseDate.getTimezoneOffset() * 60 * 1000
+    return baseDate.getTime() + timeZoneOffset
   }
 
-  export namespace TimePicker {
-    export type Attrs = p.AttrsOf<Props>
-    export type Props = BkTimePicker.Props & {
-    }
+  private _setDate(date: string | number): void {
+    date = this._offset_time(date)
+    this.picker.setDate(date)
   }
 
-  export interface TimePicker extends TimePicker.Attrs { }
-
-  export class TimePicker extends BkTimePicker {
-    declare properties: TimePicker.Props
-
-    constructor(attrs?: Partial<TimePicker.Attrs>) {
-      super(attrs)
-    }
-
-    static override __module__ = "panel.models.time_picker"
-
-    static {
-      this.prototype.default_view = TimePickerView
-
-      this.define<TimePicker.Props>(({}) => ({
-      }))
-    }
+  protected override get flatpickr_options(): flatpickr.Options.Options {
+    // on init
+    const options = super.flatpickr_options
+    if (options.defaultDate != null)
+      options.defaultDate = this._offset_time(options.defaultDate as string)
+    return options
   }
+
+  override connect_signals(): void {
+    super.connect_signals()
+
+    const { value } = this.model.properties
+    this.connect(value.change, () => {
+      const { value } = this.model
+      if (value != null && typeof value === "number") {
+        // we need to handle it when programmatically changed thru Python, e.g.
+        // time_picker.value = "4:08" or time_picker.value = "datetime.time(4, 8)"
+        // else, when changed in the UI, e.g. by typing in the input field
+        // no special handling is needed
+        this._setDate(value)
+      }
+    })
+  }
+
+}
+
+export namespace TimePicker {
+  export type Attrs = p.AttrsOf<Props>
+  export type Props = BkTimePicker.Props & {
+  }
+}
+
+export interface TimePicker extends TimePicker.Attrs { }
+
+export class TimePicker extends BkTimePicker {
+  declare properties: TimePicker.Props
+
+  constructor(attrs?: Partial<TimePicker.Attrs>) {
+    super(attrs)
+  }
+
+  static override __module__ = "panel.models.time_picker"
+
+  static {
+    this.prototype.default_view = TimePickerView
+
+    this.define<TimePicker.Props>(({ }) => ({
+    }))
+  }
+}
