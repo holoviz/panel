43f19c07f8e522cfd21b78741c994a9cf12518e8
Andrew <15331990+ahuang11@users.noreply.github.com>
2024-08-20 09:27:26 -0700

    Add on_keyup and value_input for code editor (#6919)

---
+++ panel/tests/ui/widgets/test_codeeditor.py
@@ -0,0 +1,84 @@
+import sys
+
+import pytest
+
+pytest.importorskip("playwright")
+
+from playwright.sync_api import expect
+
+from panel.tests.util import serve_component, wait_until
+from panel.widgets import CodeEditor
+
+pytestmark = pytest.mark.ui
+
+
+def test_code_editor_on_keyup(page):
+
+    editor = CodeEditor(value="print('Hello World!')", on_keyup=True)
+    serve_component(page, editor)
+    ace_input = page.locator(".ace_content")
+    expect(ace_input).to_have_count(1)
+    ace_input.click()
+
+    page.keyboard.press("Enter")
+    page.keyboard.type('print("Hello Panel!")')
+
+    expect(page.locator(".ace_content")).to_have_text("print('Hello World!')\nprint(\"Hello Panel!\")", use_inner_text=True)
+    wait_until(lambda: editor.value_input == "print('Hello World!')\nprint(\"Hello Panel!\")")
+    assert editor.value == "print('Hello World!')\nprint(\"Hello Panel!\")"
+
+    # clear the editor
+    editor.value = ""
+    expect(page.locator(".ace_content")).to_have_text("", use_inner_text=True)
+    assert editor.value == ""
+    assert editor.value_input == ""
+
+    # enter Hello UI
+    ace_input.click()
+    page.keyboard.type('print("Hello UI!")')
+    expect(page.locator(".ace_content")).to_have_text("print(\"Hello UI!\")", use_inner_text=True)
+    assert editor.value == "print(\"Hello UI!\")"
+
+
+def test_code_editor_not_on_keyup(page):
+
+    editor = CodeEditor(value="print('Hello World!')", on_keyup=False)
+    serve_component(page, editor)
+    ace_input = page.locator(".ace_content")
+    expect(ace_input).to_have_count(1)
+    ace_input.click()
+
+    page.keyboard.press("Enter")
+    page.keyboard.type('print("Hello Panel!")')
+
+    expect(page.locator(".ace_content")).to_have_text("print('Hello World!')\nprint(\"Hello Panel!\")", use_inner_text=True)
+    wait_until(lambda: editor.value_input == "print('Hello World!')\nprint(\"Hello Panel!\")")
+    assert editor.value == "print('Hello World!')"
+
+    # page click outside the editor; sync the value
+    page.locator("body").click()
+    assert editor.value_input == "print('Hello World!')\nprint(\"Hello Panel!\")"
+    wait_until(lambda: editor.value == "print('Hello World!')\nprint(\"Hello Panel!\")")
+
+    # clear the editor
+    editor.value = ""
+    expect(page.locator(".ace_content")).to_have_text("", use_inner_text=True)
+    assert editor.value == ""
+    assert editor.value_input == ""
+
+    # enter Hello UI
+    ace_input.click()
+    page.keyboard.type('print("Hello UI!")')
+    expect(page.locator(".ace_content")).to_have_text("print(\"Hello UI!\")", use_inner_text=True)
+    assert editor.value == ""
+
+    # If windows: Ctrl+Enter to trigger value else if mac, Command+Enter
+    if sys.platform == "win32":
+        page.keyboard.down("Control")
+        page.keyboard.press("Enter")
+        page.keyboard.up("Control")
+    else:
+        page.keyboard.down("Meta")
+        page.keyboard.press("Enter")
+        page.keyboard.up("Meta")
+    wait_until(lambda: editor.value == "print(\"Hello UI!\")")
