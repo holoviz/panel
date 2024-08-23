31e1fa3291e3fa3446cf623c0bc59b839a339ed2
Simon Høxbro Hansen <simon.hansen@me.com>
2024-08-23 15:32:46 +0200

    Update panel/tests/conftest.py

--- panel/tests/conftest.py
+++ panel/tests/conftest.py
@@ -50,7 +50,7 @@ if os.name != 'nt':
 
 
 for e in os.environ:
-    if e.startswith(('BOKEH_', "PANEL_")) and e not in ("PANEL_LOG_LEVEL"):
+    if e.startswith(('BOKEH_', "PANEL_")) and e not in ("PANEL_LOG_LEVEL", ):
         os.environ.pop(e, None)
 
 try:
