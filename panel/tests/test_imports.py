911c0449b4499505ee863937cdaad90414ba58ba
Simon Høxbro Hansen <simon.hansen@me.com>
2024-08-30 18:28:26 +0200

    Add test

--- panel/tests/test_imports.py
+++ panel/tests/test_imports.py
@@ -19,3 +19,34 @@ def test_no_blocklist_imports():
     output = check_output([sys.executable, '-c', dedent(check)])
 
     assert output == b""
+
+
+def test_limited_panel_imports():
+    check = """\
+    import sys
+    import panel
+
+    found = sorted([k for k in sys.modules if k.startswith("panel")])
+
+    expected = [
+        "panel",
+        "panel.__version",
+        "panel._version",
+        "panel.config",
+        "panel.depends",
+        "panel.io",
+        "panel.io.logging",
+        "panel.io.state",
+        "panel.util",
+        "panel.util.checks",
+        "panel.util.parameters",
+    ]
+
+    if found != expected:
+        mods = set(found) - set(expected)
+        print(", ".join(mods), end="")
+    """
+
+    output = check_output([sys.executable, '-c', dedent(check)])
+
+    assert output == b""
