7c00952c13ca9f55b590f0053cca0b6822a76932
Simon Høxbro Hansen <simon.hansen@me.com>
2024-08-30 15:25:18 +0200

    Don't set curdoc during init of _config

--- panel/config.py
+++ panel/config.py
@@ -40,6 +40,7 @@ _LOCAL_DEV_VERSION = (
 #---------------------------------------------------------------------
 
 _PATH = os.path.abspath(os.path.dirname(__file__))
+_config_initialized = False
 
 def validate_config(config, parameter, value):
     """
@@ -435,7 +436,7 @@ class _config(_base_config):
         ensure that even on first access mutable parameters do not
         end up being modified.
         """
-        if attr in ('_param__private', '_globals', '_parameter_set', '__class__', 'param'):
+        if not _config_initialized or attr in ('_param__private', '_globals', '_parameter_set', '__class__', 'param'):
             return super().__getattribute__(attr)
 
         from .io.state import state
@@ -621,6 +622,7 @@ else:
 _config._parameter_set = set(_params)
 config = _config(**{k: None if p.allow_None else getattr(_config, k)
                     for k, p in _params.items() if k != 'name'})
+_config_initialized = True
 
 class panel_extension(_pyviz_extension):
     """
