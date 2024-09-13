bf80ba6a97560d7dacfd8b5755d1e2f852b2cffd
Philipp Rudiger <prudiger@anaconda.com>
2024-09-13 18:28:32 +0200

    Do not load es-module-shim as module (#7270)

--- panel/models/esm.py
+++ panel/models/esm.py
@@ -46,13 +46,13 @@ class ReactiveESM(HTMLBox):
 
     importmap = bp.Dict(bp.String, bp.Dict(bp.String, bp.String))
 
-    __javascript_modules_raw__ = [
+    __javascript_raw__ = [
         f"{config.npm_cdn}/es-module-shims@^1.10.0/dist/es-module-shims.min.js"
     ]
 
     @classproperty
-    def __javascript_modules__(cls):
-        return bundled_files(cls, 'javascript_modules')
+    def __javascript__(cls):
+        return bundled_files(cls)
 
 
 class ReactComponent(ReactiveESM):
