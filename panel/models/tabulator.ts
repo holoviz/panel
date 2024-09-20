3194fb50b94a90e12ca5da5bce3abeca9ec631d1
Philipp Rudiger <prudiger@anaconda.com>
2024-09-20 14:10:37 +0200

    Do not set to building until it starts

--- panel/models/tabulator.ts
+++ panel/models/tabulator.ts
@@ -550,7 +550,6 @@ export class DataTabulatorView extends HTMLBoxView {
     }
     super.render()
     this._initializing = true
-    this._building = true
     const container = div({style: {display: "contents"}})
     const el = div({style: {width: "100%", height: "100%", visibility: "hidden"}})
     this.container = el
