c509a44543860a6e08849805cad23675837cd6e7
Andrew Huang <ahuang@anaconda.com>
2024-10-18 16:17:37 -0700

    add docstrings, allow triggering views, bugfixes

--- panel/models/fullcalendar.js
+++ panel/models/fullcalendar.js
@@ -7,16 +7,14 @@ export function render({model, el}) {
       buttonIcons: model.button_icons,
       buttonText: model.button_text,
       contentHeight: model.content_height,
-      dateAlignment: model.date_alignment,
       dateIncrement: model.date_increment,
       events: model.value,
       expandRows: model.expand_rows,
       footerToolbar: model.footer_toolbar,
       handleWindowResize: model.handle_window_resize,
       headerToolbar: model.header_toolbar,
-      initialDate: model.initial_date,
-      initialView: model.initial_view,
-      multiMonthMaxColumns: model.multi_month_max_columns,
+      initialView: model.current_view,
+      navLinks: model.nav_links,
       nowIndicator: model.now_indicator,
       plugins,
       showNonCurrentDates: model.show_non_current_dates,
@@ -29,7 +27,29 @@ export function render({model, el}) {
       datesSet(info) {
         model.send_msg({current_date: calendar.getDate().toISOString()})
       },
+      viewClassNames(info) {
+        model.send_msg({current_view: info.view.type})
+      },
+      navLinkDayClick(date, jsEvent) {
+        calendar.changeView("timeGridDay", date)
+      },
+      navLinkWeekClick(weekStart, jsEvent) {
+      },
     })
+
+    // there's initialDate, but if it's set, there's buggy behavior on re-renders
+    if (model.current_date) {
+      calendar.gotoDate(model.current_date)
+    }
+
+    // these cannot be set in the constructor if null
+    if (model.dateAlignment) {
+      calendar.setOption("dateAlignment", model.dateAlignment)
+    }
+
+    if (model.current_view == "multiMonth") {
+      calendar.setOption("multiMonthMaxColumns", model.multi_month_max_columns)
+    }
 
     if (model.aspect_ratio) {
       calendar.setOption("aspectRatio", model.aspect_ratio)
@@ -50,10 +70,10 @@ export function render({model, el}) {
         calendar.gotoDate(event.date)
       } else if (event.type === "incrementDate") {
         calendar.incrementDate(event.increment)
-      } else if (event.type === "updateSize") {
-        calendar.updateSize()
       } else if (event.type === "updateOption") {
         calendar.setOption(event.key, event.value)
+      } else if (event.type === "changeView") {
+        calendar.changeView(event.view, event.date)
       }
     })
     calendar.render()
@@ -61,7 +81,7 @@ export function render({model, el}) {
 
   const plugins = []
   function loadPluginIfNeeded(viewName, pluginName) {
-    if (model.initial_view.startsWith(viewName) ||
+    if (model.current_view.startsWith(viewName) ||
       (model.header_toolbar && Object.values(model.header_toolbar).some(v => v.includes(viewName))) ||
       (model.footer_toolbar && Object.values(model.footer_toolbar).some(v => v.includes(viewName)))) {
       return import(`@fullcalendar/${pluginName}`).then(plugin => {
--- panel/widgets/calendar.py
+++ panel/widgets/calendar.py
@@ -8,9 +8,30 @@ from panel.custom import JSComponent
 
 THIS_DIR = Path(__file__).parent
 MODELS_DIR = THIS_DIR.parent / "models"
+VIEW_DEFAULT_INCREMENTS = {
+    "dayGridMonth": {"days": 1},
+    "dayGridWeek": {"weeks": 1},
+    "dayGridDay": {"days": 1},
+    "timeGridWeek": {"weeks": 1},
+    "timeGridDay": {"days": 1},
+    "listWeek": {"weeks": 1},
+    "listMonth": {"months": 1},
+    "listYear": {"years": 1},
+    "multiMonthYear": {"years": 1},
+}
 
 
 class Calendar(JSComponent):
+    """
+    The Calendar widget is a wrapper around the FullCalendar library.
+    See https://fullcalendar.io/docs for more information on the parameters.
+
+    Reference: https://panel.holoviz.org/reference/widgets/Calendar.html
+
+    :Example:
+
+    >>> pn.widgets.Calendar(value=[{"date": "2024-10-01", "type": "event"}])
+    """
 
     aspect_ratio = param.Number(
         default=None, doc="Sets the width-to-height aspect ratio of the calendar."
@@ -34,12 +55,32 @@ class Calendar(JSComponent):
         default=None, doc="Sets the height of the view area of the calendar."
     )
 
-    current_date = param.Date(
-        default=None, doc="The current date of the calendar view."
+    # using String instead of Date because fullcalendar supports fuzzy dates
+    current_date = param.String(
+        default=None,
+        constant=True,
+        doc="The onload or current date of the calendar view. Use go_to_date() to change the date.",
+    )
+
+    current_view = param.Selector(
+        default="dayGridMonth",
+        objects=[
+            "dayGridMonth",
+            "dayGridWeek",
+            "dayGridDay",
+            "timeGridWeek",
+            "timeGridDay",
+            "listWeek",
+            "listMonth",
+            "listYear",
+            "multiMonthYear",
+        ],
+        constant=True,
+        doc="The onload or current view of the calendar. Use change_view() to change the view.",
     )
 
     date_alignment = param.String(
-        default="month", doc="Determines how certain views should be initially aligned."
+        default=None, doc="Determines how certain views should be initially aligned."
     )
 
     date_increment = param.String(
@@ -67,28 +108,7 @@ class Calendar(JSComponent):
             "center": "title",
             "right": "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
         },
-        doc="Defines the buttons and title at the top of the calendar.",
-    )
-
-    initial_date = param.Date(
-        default=None,
-        doc="The initial date the calendar should display when first loaded.",
-    )
-
-    initial_view = param.Selector(
-        default="dayGridMonth",
-        objects=[
-            "dayGridMonth",
-            "dayGridWeek",
-            "dayGridDay",
-            "timeGridWeek",
-            "timeGridDay",
-            "listWeek",
-            "listMonth",
-            "listYear",
-            "multiMonthYear",
-        ],
-        doc="The initial view when the calendar loads.",
+        doc="Defines the buttons and title at the top of the calendar."
     )
 
     multi_month_max_columns = param.Integer(
@@ -96,8 +116,13 @@ class Calendar(JSComponent):
         doc="Determines the maximum number of columns in the multi-month view.",
     )
 
+    nav_links = param.Boolean(
+        default=True,
+        doc="Turns various datetime text into clickable links that the user can use for navigation.",
+    )
+
     now_indicator = param.Boolean(
-        default=False, doc="Whether to display an indicator for the current time."
+        default=True, doc="Whether to display an indicator for the current time."
     )
 
     show_non_current_dates = param.Boolean(
@@ -171,6 +196,7 @@ class Calendar(JSComponent):
                 "handle_window_resize",
                 "header_toolbar",
                 "multi_month_max_columns",
+                "nav_links",
                 "now_indicator",
                 "show_non_current_dates",
                 "sticky_footer_scrollbar",
@@ -183,46 +209,114 @@ class Calendar(JSComponent):
             ],
         )
 
-    def next(self):
+    def click_next(self) -> None:
+        """
+        Click the next button through the calendar's UI.
+        """
         self._send_msg({"type": "next"})
 
-    def prev(self):
+    def click_prev(self) -> None:
+        """
+        Click the previous button through the calendar's UI.
+        """
         self._send_msg({"type": "prev"})
 
-    def prev_year(self):
+    def click_prev_year(self) -> None:
+        """
+        Click the previous year button through the calendar's UI.
+        """
         self._send_msg({"type": "prevYear"})
 
-    def next_year(self):
+    def click_next_year(self) -> None:
+        """
+        Click the next year button through the calendar's UI.
+        """
         self._send_msg({"type": "nextYear"})
 
-    def today(self):
+    def click_today(self) -> None:
+        """
+        Click the today button through the calendar's UI.
+        """
         self._send_msg({"type": "today"})
 
-    def go_to_date(self, date):
-        self._send_msg({"type": "gotoDate", "date": date.isoformat()})
+    def change_view(
+        self,
+        view: str,
+        date: str | datetime.datetime | datetime.date | int | None = None,
+    ) -> None:
+        """
+        Change the current view of the calendar, and optionally go to a specific date.
 
-    def increment_date(self, increment):
+        Args:
+            view: The view to change to.
+                Options: "dayGridMonth", "dayGridWeek", "dayGridDay", "timeGridWeek", "timeGridDay",
+                "listWeek", "listMonth", "listYear", "multiMonthYear".
+            date: The date to go to after changing the view; if None, the current date will be used.
+                Supports ISO 8601 date strings, datetime/date objects, and int in milliseconds.
+        """
+        self._send_msg({"type": "changeView", "view": view, "date": date})
+
+    def go_to_date(self, date: str | datetime.datetime | datetime.date | int) -> None:
+        """
+        Go to a specific date on the calendar.
+
+        Args:
+            date: The date to go to.
+                Supports ISO 8601 date strings, datetime/date objects, and int in milliseconds.
+        """
+        self._send_msg({"type": "gotoDate", "date": date})
+
+    def increment_date(
+        self, increment: str | datetime.timedelta | int | dict | None = None
+    ) -> None:
+        """
+        Increment the current date by a specific amount.
+
+        Args:
+            increment: The amount to increment the current date by.
+                Supports a string in the format hh:mm:ss.sss, hh:mm:sss or hh:mm, an int in milliseconds,
+                datetime.timedelta objects, or a dict with any of the following keys:
+                    year, years, month, months, day, days, minute, minutes, second,
+                    seconds, millisecond, milliseconds, ms.
+                If not provided, the date_increment parameter will be used.
+                If date_increment is not set, the default increment for the current view will be used:
+                    dayGridMonth: {"days": 1}
+                    dayGridWeek: {"weeks": 1}
+                    dayGridDay: {"days": 1}
+                    timeGridWeek: {"weeks": 1}
+                    timeGridDay: {"days": 1}
+                    listWeek: {"weeks": 1}
+                    listMonth: {"months": 1}
+                    listYear: {"years": 1}
+                    multiMonthYear: {"years": 1}
+        """
+
+        if increment is None and self.date_increment is None:
+            increment = VIEW_DEFAULT_INCREMENTS[self.current_view]
         self._send_msg({"type": "incrementDate", "increment": increment})
-
-    def update_size(self):
-        self._send_msg({"type": "updateSize"})
-
-    def _handle_msg(self, msg):
-        if "current_date" in msg:
-            self.current_date = datetime.datetime.strptime(
-                msg["current_date"], "%Y-%m-%dT%H:%M:%S.%fZ"
-            )
-        else:
-            raise NotImplementedError(f"Unhandled message: {msg}")
 
     def add_event(
         self,
-        start: str,
-        end: str | None = None,
+        start: str | datetime.datetime | datetime.date | int,
+        end: str | datetime.datetime | datetime.date | int | None = None,
         title: str = "(no title)",
         all_day: bool = False,
         **kwargs,
-    ):
+    ) -> None:
+        """
+        Add an event to the calendar.
+
+        Args:
+            start: The start date of the event.
+                Supports ISO 8601 date strings, datetime/date objects, and int in milliseconds.
+            end: The end date of the event.
+                Supports ISO 8601 date strings, datetime/date objects, and int in milliseconds.
+                If None, the event will be all-day.
+            title: The title of the event.
+            all_day: Whether the event is an all-day event.
+            **kwargs: Additional properties to set on the event
+        """
+
         event = {
             "start": start,
             "end": end,
@@ -232,13 +326,22 @@ class Calendar(JSComponent):
         }
         self.value.append(event)
 
+    def _handle_msg(self, msg):
+        if "current_date" in msg:
+            with param.edit_constant(self):
+                self.current_date = msg["current_date"]
+        elif "current_view" in msg:
+            with param.edit_constant(self):
+                self.current_view = msg["current_view"]
+        else:
+            raise NotImplementedError(f"Unhandled message: {msg}")
+
     def _update_option(self, event):
         def to_camel_case(string):
             return "".join(
                 word.capitalize() if i else word
                 for i, word in enumerate(string.split("_"))
             )
+
         key = to_camel_case(event.name)
-        self._send_msg(
-            {"type": "updateOption", "key": key, "value": event.new}
-        )
+        self._send_msg({"type": "updateOption", "key": key, "value": event.new})
