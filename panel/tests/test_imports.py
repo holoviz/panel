ce61860c14acd2e9d081ae3590df08a80843d643
Simon Høxbro Hansen <simon.hansen@me.com>
2024-11-11 11:32:46 +0100

    Update import test

--- panel/tests/test_imports.py
+++ panel/tests/test_imports.py
@@ -1,10 +1,8 @@
 import sys
 
-from importlib import import_module
 from subprocess import check_output
 from textwrap import dedent
 
-import pytest
 
 
 def test_no_blocklist_imports():
@@ -29,18 +27,7 @@ def test_limited_panel_imports():
     import sys
     import panel
 
-    found = {k for k in sys.modules if k.startswith("panel")}
-    expected = {
-        "panel",
-        "panel.__version",
-        "panel._version",
-        "panel.config",
-        "panel.depends",
-        "panel.util",
-        "panel.util.checks",
-        "panel.util.parameters",
-    }
-    mods = found - expected
+    mods = {k for k in sys.modules if k.startswith("panel.")}
     if mods:
         print(", ".join(mods), end="")
     """
@@ -48,27 +35,3 @@ def test_limited_panel_imports():
     output = check_output([sys.executable, '-c', dedent(check)])
 
     assert output == b""
-
-@pytest.mark.parametrize("mod_name", ["panel", "panel.io", "panel.pane", "panel.widgets"])
-def test_lazy_import(mod_name):
-    mod = import_module(mod_name)
-    assert mod.__dir__() == list(mod.__all__)
-
-    check = f"""\
-    import {mod_name} as mod
-    a, b, c = set(mod.__all__), set(mod._attrs), set(mod.__dict__)
-
-    # Check no overlap between b and c
-    if b & c:
-        print("Failed: b and c overlap: ", end="")
-        print(", ".join(b & c), end="")
-    # Check if all imports are either directly imported or in attrs
-    if a - (b | c):
-        print("Failed: a - (b | c): ", end="")
-        print(", ".join(a - (b | c)), end="")
-    """
-    output = check_output([sys.executable, '-c', dedent(check)])
-    assert output == b""
-
-    for n in mod._attrs:
-        assert getattr(mod, n)
