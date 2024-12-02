c9ada928716a4d8e963620cb7f633743cadd3e9f
Simon Høxbro Hansen <simon.hansen@me.com>
2024-12-02 12:03:25 +0100

    Use a symbol instead of SVG for close button

--- panel/models/modal.ts
+++ panel/models/modal.ts
@@ -6,13 +6,6 @@ import type {Attrs} from "@bokehjs/core/types"
 import {UIElementView} from "@bokehjs/models/ui/ui_element"
 import {LayoutDOMView} from "@bokehjs/models/layouts/layout_dom"
 
-const SVG = `
-<svg class="svg-icon" viewBox="0 0 20 20">
-<path
-  fill="currentcolor"
-  d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"
-></path>`
-
 type A11yDialogView = {
   on(event: string, listener: () => void): void
   show(): void
@@ -98,7 +91,7 @@ export class ModalView extends BkColumnView {
       class: "pnx-dialog-close",
       ariaLabel: "Close this dialog window",
     } as any)
-    this.close_button.innerHTML = SVG
+    this.close_button.innerHTML = "&#x2715"
     this.modal_children = div({id: "pnx_modal_object"})
 
     container.append(dialog)
