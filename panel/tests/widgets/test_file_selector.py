5d6147852ece293d9599e5fbb2e6c381050b6710
Philipp Rudiger <prudiger@anaconda.com>
2025-01-15 17:48:08 +0100

    Add tests

--- panel/tests/widgets/test_file_selector.py
+++ panel/tests/widgets/test_file_selector.py
@@ -1,13 +1,22 @@
 import os
 
-from importlib.util import find_spec
+from pathlib import Path
 
 import pytest
 
-from panel.models.widgets import DoubleClickEvent
-from panel.widgets import FileSelector
+try:
+    import s3fs
+except Exception:
+    s3fs = None
 
-S3FS_UNAVAILABLE = find_spec('s3fs') is None
+from panel.models.widgets import DoubleClickEvent
+from panel.widgets.file_selector import (
+    FileSelector, LocalFileProvider, RemoteFileProvider,
+)
+
+s3fs_available = pytest.mark.skipif(s3fs is None, reason='s3fs not available')
+
+FILE_PATH = Path(__file__)
 
 
 @pytest.fixture
@@ -24,6 +33,33 @@ def test_dir(tmp_path):
     b.write_text("")
 
     yield str(test_dir)
+
+
+def test_local_file_provider_is_dir():
+    provider = LocalFileProvider()
+    assert not provider.isdir(FILE_PATH)
+    assert provider.isdir(FILE_PATH.parent)
+
+def test_local_file_provider_ls():
+    provider = LocalFileProvider()
+    dirs, files = provider.ls(FILE_PATH.parent, '*test_file_selector*')
+    assert files == [str(FILE_PATH)]
+
+@s3fs_available
+def test_remote_file_provider_is_dir():
+    fs = s3fs.S3FileSystem(anon=True)
+    provider = RemoteFileProvider(fs=fs)
+    assert not provider.isdir('s3://datasets.holoviz.org/stocks/v1/stocks.csv')
+    assert provider.isdir('s3://datasets.holoviz.org/stocks/v1/')
+
+@s3fs_available
+def test_remote_file_provider_ls():
+    fs = s3fs.S3FileSystem(anon=True)
+    provider = RemoteFileProvider(fs=fs)
+    dirs, _ = provider.ls('s3://datasets.holoviz.org/stocks/')
+    assert dirs == ['s3://datasets.holoviz.org/stocks/v1/']
+    _, files = provider.ls('s3://datasets.holoviz.org/stocks/v1')
+    assert files == ['s3://datasets.holoviz.org/stocks/v1/stocks.csv']
 
 
 def test_file_selector_init(test_dir):
