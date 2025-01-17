b73c4f2c78f8cc0d8ec56d922d564ea672112110
Philipp Rudiger <prudiger@anaconda.com>
2025-01-17 19:12:00 +0100

    Make oauth-secret test optional

--- panel/tests/command/test_oauth_secret.py
+++ panel/tests/command/test_oauth_secret.py
@@ -1,7 +1,11 @@
+import importlib.util
 import subprocess
 import sys
 
+import pytest
 
+
+@pytest.mark.skipif(importlib.util.find_spec("cryptography"), reason='cryptography is not installed')
 def test_oauth_secret_command():
     cmd = [sys.executable, "-m", "panel", "oauth-secret"]
     p = subprocess.Popen(cmd, shell=False, text=True, stdout=subprocess.PIPE)
