5558ac0154de317866894122566168dae615d99d
Philipp Rudiger <prudiger@anaconda.com>
2025-01-17 19:16:12 +0100

    Fix condition

--- panel/tests/command/test_oauth_secret.py
+++ panel/tests/command/test_oauth_secret.py
@@ -5,7 +5,7 @@ import sys
 import pytest
 
 
-@pytest.mark.skipif(importlib.util.find_spec("cryptography"), reason='cryptography is not installed')
+@pytest.mark.skipif(not importlib.util.find_spec("cryptography"), reason='cryptography is not installed')
 def test_oauth_secret_command():
     cmd = [sys.executable, "-m", "panel", "oauth-secret"]
     p = subprocess.Popen(cmd, shell=False, text=True, stdout=subprocess.PIPE)
