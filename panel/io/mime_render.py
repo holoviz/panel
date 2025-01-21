36f4712236eb847b2265fdf87c63b35eec28cf4e
Philipp Rudiger <prudiger@anaconda.com>
2025-01-21 13:24:35 +0100

    Correctly identify skimage and PIL packages when converting (#7637)

--- panel/io/mime_render.py
+++ panel/io/mime_render.py
@@ -32,9 +32,10 @@ from typing import IO, Any
 _STDLIBS = sys.stdlib_module_names
 _PACKAGE_MAP = {
     'sklearn': 'scikit-learn',
+    'skimage': 'scikit-image',
     'transformers_js': 'transformers-js-py',
 }
-_IGNORED_PKGS = ['js', 'pyodide']
+_IGNORED_PKGS = ['js', 'pyodide', 'PIL']
 _PANDAS_AUTODETECT = ['bokeh.sampledata', 'as_frame']
 
 def find_requirements(code: str) -> list[str]:
