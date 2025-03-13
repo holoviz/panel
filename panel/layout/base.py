afa745cdcf7d6304ab09c1837a842f8bf64ff8f2
Alexey Sukharevich <sukharevich@kaggle.com>
2025-03-13 14:39:39 +0100

    perf: Avoid redundant list operations in `ListLike` (#7764)

--- panel/layout/base.py
+++ panel/layout/base.py
@@ -388,16 +388,12 @@ class ListLike(param.Parameterized):
     def __add__(self, other: Iterable[Any]) -> ListLike:
         if isinstance(other, ListLike):
             other = other.objects
-        else:
-            other = list(other)
-        return self.clone(*(self.objects+other))
+        return self.clone(*self.objects, *other)
 
     def __radd__(self, other: Iterable[Any]) -> ListLike:
         if isinstance(other, ListLike):
             other = other.objects
-        else:
-            other = list(other)
-        return self.clone(*(other+self.objects))
+        return self.clone(*other, *self.objects)
 
     def __contains__(self, obj: Viewable) -> bool:
         return obj in self.objects
@@ -643,24 +639,26 @@ class NamedListLike(param.Parameterized):
         return self
 
     def __add__(self, other: Iterable[Any]) -> NamedListLike:
+        added: Iterable
         if isinstance(other, NamedListLike):
-            added = list(zip(other._names, other.objects))
+            added = zip(other._names, other.objects)
         elif isinstance(other, ListLike):
             added = other.objects
         else:
-            added = list(other)
-        objects = list(zip(self._names, self.objects))
-        return self.clone(*(objects+added))
+            added = other
+        objects = zip(self._names, self.objects)
+        return self.clone(*objects, *added)
 
     def __radd__(self, other: Iterable[Any]) -> NamedListLike:
+        added: Iterable
         if isinstance(other, NamedListLike):
-            added = list(zip(other._names, other.objects))
+            added = zip(other._names, other.objects)
         elif isinstance(other, ListLike):
             added = other.objects
         else:
-            added = list(other)
-        objects = list(zip(self._names, self.objects))
-        return self.clone(*(added+objects))
+            added = other
+        objects = zip(self._names, self.objects)
+        return self.clone(*added, *objects)
 
     def __setitem__(self, index: int | slice, panes: Iterable[Any]) -> None:
         new_objects = list(self)
