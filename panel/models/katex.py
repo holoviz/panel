b48230fdf26cea2eb025c0c38672e0892032d073
Philipp Rudiger <prudiger@anaconda.com>
2025-09-08 13:01:03 +0200

    Bundle KaTeX

--- panel/models/katex.py
+++ panel/models/katex.py
@@ -4,6 +4,8 @@ Defines a custom KaTeX bokeh model to render text using KaTeX.
 from bokeh.models import Markup
 
 from ..config import config
+from ..io.resources import bundled_files
+from ..util import classproperty
 
 KATEX_VERSION = "0.16.22"
 
@@ -13,17 +15,27 @@ class KaTeX(Markup):
     A bokeh model that renders text using KaTeX.
     """
 
-    __css__ = [f"{config.npm_cdn}/katex@{KATEX_VERSION}/dist/katex.min.css"]
+    __css_raw__ = [f"{config.npm_cdn}/katex@{KATEX_VERSION}/dist/katex.min.css"]
 
-    __javascript__ = [
+    @classproperty
+    def __css__(cls):
+        return bundled_files(cls, 'css')
+
+    __javascript_raw__ = [
         f"{config.npm_cdn}/katex@{KATEX_VERSION}/dist/katex.min.js",
         f"{config.npm_cdn}/katex@{KATEX_VERSION}/dist/contrib/auto-render.min.js"
     ]
 
-    __js_skip__ = {
-        'katex': __javascript__[:1],
-        'renderMathInElement': __javascript__[1:]
-    }
+    @classproperty
+    def __javascript__(cls):
+        return bundled_files(cls)
+
+    @classproperty
+    def __js_skip__(cls):
+        return {
+            'katex': cls.__javascript__[:1],
+            'renderMathInElement': cls.__javascript__[1:]
+        }
 
     __js_require__ = {
         'paths': {
