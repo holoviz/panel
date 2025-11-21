41da1fb0b4710433c910d113207cbf9af3b9bf57
Daniel Althviz Moré <16781833+dalthviz@users.noreply.github.com>
2025-11-21 12:13:24 -0500

    child_view visible handling simplification inside render

--- panel/models/card.ts
+++ panel/models/card.ts
@@ -22,7 +22,6 @@ export class CardView extends ColumnView {
 
   button_el: HTMLButtonElement
   header_el: HTMLElement
-  initially_collapsed: boolean
 
   readonly collapsed_style = new DOM.InlineStyleSheet()
 
@@ -30,7 +29,6 @@ export class CardView extends ColumnView {
     super.connect_signals()
 
     const {active_header_background, collapsed, header_background, header_color, hide_header} = this.model.properties
-    this.initially_collapsed = this.model.collapsed
     this.on_change(collapsed, () => this._collapse())
     this.on_change([header_color, hide_header], () => this.render())
 
@@ -109,10 +107,7 @@ export class CardView extends ColumnView {
     }
 
     for (const child_view of this.child_views.slice(1)) {
-      if (this.initially_collapsed) {
-        child_view.model.visible = false
-        this.initially_collapsed = false
-      }
+      child_view.model.visible = !this.model.collapsed
       this.shadow_el.appendChild(child_view.el)
       child_view.render()
       child_view.r_after_render()
