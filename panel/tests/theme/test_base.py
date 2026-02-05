61d75e0cdf38295e776e4b1c6d54502a49913710
Philipp Rudiger <prudiger@anaconda.com>
2026-02-05 12:31:09 +0100

    Debug

--- panel/tests/theme/test_base.py
+++ panel/tests/theme/test_base.py
@@ -3,6 +3,7 @@ import pathlib
 from bokeh.models import ImportedStyleSheet
 
 from panel.io.resources import CDN_DIST
+from panel.io.state import state
 from panel.theme.base import BOKEH_DARK, Design, Inherit
 from panel.viewable import Viewable
 from panel.widgets import FloatSlider, IntSlider, TextInput
@@ -121,6 +122,8 @@ def test_design_params_url_inherited():
 def test_design_apply(document, comm):
     widget = TextInput()
     model = widget.get_root(document, comm=comm)
+
+    print(">>>", state.curdoc, getattr(state.curdoc, "session_context", None))  # noqa
 
     DesignTest().apply(widget, model)
 
