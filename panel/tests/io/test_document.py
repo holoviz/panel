905b44d912d3d6818240508582553aa5ad9f4d5c
Philipp Rudiger <prudiger@anaconda.com>
2026-06-01 13:06:10 +0200

    Fix hold in threaded mode (#8619)

--- panel/tests/io/test_document.py
+++ panel/tests/io/test_document.py
@@ -1,10 +1,14 @@
+from concurrent.futures import ThreadPoolExecutor
+
+import pytest
+
 from bokeh.document import Document
 
 import panel as pn
 
-from panel.io.document import _cleanup_doc, unlocked
+from panel.io.document import _cleanup_doc, hold, unlocked
 from panel.io.state import _state, set_curdoc, state
-from panel.tests.util import serve_and_request
+from panel.tests.util import serve_and_request, wait_until
 from panel.widgets import IntSlider
 
 
@@ -40,3 +44,26 @@ def test_document_hold():
             model.value = 3
 
     assert doc.callbacks._held_events
+
+
+@pytest.mark.xdist_group(name="server")
+def test_hold_does_not_get_stuck_with_threaded_callbacks(threads):
+    column = pn.FlexBox(*[pn.pane.Str('0') for _ in range(20)])
+    layout = pn.Column(column)
+
+    serve_and_request(layout)
+
+    doc = list(layout._documents.keys())[0]
+
+    def update_in_hold(i):
+        with set_curdoc(doc):
+            with hold(doc):
+                for obj in column:
+                    obj.object = str(i)
+
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = [executor.submit(update_in_hold, i) for i in range(10)]
+        for f in futures:
+            f.result()
+
+    wait_until(lambda: not doc.callbacks.hold_value, timeout=5000)
