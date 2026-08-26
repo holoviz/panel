9286b39111b61f7d9d4de87946a6d64481020098
Philipp Rudiger <prudiger@anaconda.com>
2026-08-26 13:01:43 +0200

    Simplify

--- panel/layout/grid.py
+++ panel/layout/grid.py
@@ -466,10 +466,10 @@ class GridSpec(Panel):
             deleted = dict([list(o)[0] for o in subgrid.flatten()])
         else:
             deleted = [list(subgrid)[0][0]]
-        new_objects = dict(self.objects)
-        for key in deleted:
-            del new_objects[key]
-        self.objects = new_objects
+        self.objects = {
+            k: v for k, v in self.objects.items()
+            if k not in deleted
+        }
 
     def __getitem__(self, index):
         if isinstance(index, tuple):
