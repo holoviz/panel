7a2bc51385818fe6994f02ef308512a1e41f3aa7
Philipp Rudiger <prudiger@anaconda.com>
2026-09-07 09:30:29 +0200

    Cross reference with panel-material-ui

--- doc/conf.py
+++ doc/conf.py
@@ -108,7 +108,15 @@ extensions = [
     'ui_reference',
     'nbsite.pyodide',
     'nbsite.analytics',
+    'material_reference',
 ]
+
+# Resolves the cross-links material_reference adds from a classic reference page
+# to its Material counterpart. The 'external+' form is required because Sphinx
+# disables implicit intersphinx resolution of :doc: references by default.
+intersphinx_mapping = {
+    'panel_material_ui': ('https://panel-material-ui.holoviz.org', None),
+}
 
 numpydoc_show_inherited_class_members = False
 numpydoc_class_members_toctree = False
